1c99ca52cb02c16be4be2537077d2cdaa76045cf
c9s <yoan@maicoin.com>
2020-07-11 15:00:35 +0800

    add color.go

---
+++ types/side.go
@@ -0,0 +1,24 @@
+package types
+
+// SideType define side type of order
+type SideType string
+
+const (
+	SideTypeBuy  = SideType("BUY")
+	SideTypeSell = SideType("SELL")
+)
+
+func (side SideType) Color() string {
+	if side == SideTypeBuy {
+		return Green
+	}
+	if side == SideTypeSell {
+		return Red
+	}
+
+	return "#f0f0f0"
+}
+
+func SideToColorName(side SideType) string {
+	return side.Color()
+}
