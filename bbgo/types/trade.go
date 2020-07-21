da75905aeb2e316492d066911c68875fa11da82b
c9s <yoan@maicoin.com>
2020-07-21 23:22:15 +0800

    insert trades into the mysql database

--- bbgo/types/trade.go
+++ bbgo/types/trade.go
@@ -6,16 +6,21 @@ import (
 )
 
 type Trade struct {
-	ID          int64
-	Price       float64
-	Volume      float64
-	Side        string
-	IsBuyer     bool
-	IsMaker     bool
-	Time        time.Time
-	Symbol      string
-	Fee         float64
-	FeeCurrency string
+	// GID is the global ID
+	GID int64 `json:"gid" db:"gid"`
+
+	// ID is the source trade ID
+	ID          int64     `json:"id" db:"id"`
+	Exchange    string    `json:"exchange" db:"exchange"`
+	Price       float64   `json:"price" db:"price"`
+	Volume      float64   `json:"volume" db:"volume"`
+	Symbol      string    `json:"symbol" db:"symbol"`
+	Side        string    `json:"side" db:"side"`
+	IsBuyer     bool      `json:"isBuyer" db:"is_buyer"`
+	IsMaker     bool      `json:"isMaker" db:"is_maker"`
+	Time        time.Time `json:"tradedAt" db:"traded_at"`
+	Fee         float64   `json:"fee" db:"fee"`
+	FeeCurrency string    `json:"feeCurrency" db:"fee_currency"`
 }
 
 func (trade Trade) SlackAttachment() slack.Attachment {
@@ -40,10 +45,10 @@ func (trade Trade) SlackAttachment() slack.Attachment {
 		// Pretext:       "",
 		// Text:          "",
 		Fields: []slack.AttachmentField{
-			{Title: "Symbol", Value: trade.Symbol, Short: true,},
-			{Title: "Side", Value: trade.Side, Short: true,},
-			{Title: "Price", Value: market.FormatPrice(trade.Price), Short: true,},
-			{Title: "Volume", Value: market.FormatVolume(trade.Volume), Short: true,},
+			{Title: "Symbol", Value: trade.Symbol, Short: true},
+			{Title: "Side", Value: trade.Side, Short: true},
+			{Title: "Price", Value: market.FormatPrice(trade.Price), Short: true},
+			{Title: "Volume", Value: market.FormatVolume(trade.Volume), Short: true},
 		},
 		// Footer:     tradingCtx.TradeStartTime.Format(time.RFC822),
 		// FooterIcon: "",
