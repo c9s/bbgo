1dc1ef3a4249e7ebfe99f06988c059edb4d8e4a9
c9s <yoan@maicoin.com>
2020-10-26 13:40:43 +0800

    configure notifier and make slack notification optional

--- pkg/bbgo/notifier.go
+++ pkg/bbgo/notifier.go
@@ -23,14 +23,22 @@ func (m *Notifiability) AddNotifier(notifier Notifier) {
 	m.notifiers = append(m.notifiers, notifier)
 }
 
-func (m *Notifiability) Notify(msg string, args ...interface{}) {
+func (m *Notifiability) Notify(msg string, args ...interface{}) (err error) {
 	for _, n := range m.notifiers {
-		n.Notify(msg, args...)
+		if err2 := n.Notify(msg, args...); err2 != nil {
+			err = err2
+		}
 	}
+
+	return err
 }
 
-func (m *Notifiability) NotifyTo(channel, msg string, args ...interface{}) {
+func (m *Notifiability) NotifyTo(channel, msg string, args ...interface{}) (err error) {
 	for _, n := range m.notifiers {
-		n.NotifyTo(channel, msg, args...)
+		if err2 := n.NotifyTo(channel, msg, args...); err2 != nil {
+			err = err2
+		}
 	}
+
+	return err
 }
