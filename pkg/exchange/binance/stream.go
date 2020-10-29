9c46ef17b24121f1b21cba592c062aa6d71f54fc
c9s <yoan@maicoin.com>
2020-10-29 21:10:01 +0800

    handle order update

--- pkg/exchange/binance/stream.go
+++ pkg/exchange/binance/stream.go
@@ -130,11 +130,21 @@ func NewStream(client *binance.Client) *Stream {
 
 	stream.OnExecutionReportEvent(func(e *ExecutionReportEvent) {
 		switch e.CurrentExecutionType {
+
+		case "NEW", "CANCELED", "REJECTED", "EXPIRED", "REPLACED":
+			order, err := e.Order()
+			if err != nil {
+				log.WithError(err).Error("order convert error")
+				return
+			}
+
+			stream.EmitOrderUpdate(*order)
+
 		case "TRADE":
 			trade, err := e.Trade()
 			if err != nil {
 				log.WithError(err).Error("trade convert error")
-				break
+				return
 			}
 
 			stream.EmitTradeUpdate(*trade)
