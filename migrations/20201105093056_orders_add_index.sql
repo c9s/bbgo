e0b26bddd0a6798dee2fbe70985491fc8623d6e8
c9s <yoan@maicoin.com>
2020-11-05 12:55:20 +0800

    make order id unique

--- migrations/20201105093056_orders_add_index.sql
+++ migrations/20201105093056_orders_add_index.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE INDEX orders_symbol ON orders (exchange, symbol);
-CREATE INDEX orders_id_symbol ON orders(exchange, order_id, symbol);
+CREATE UNIQUE INDEX orders_order_id ON orders (order_id, exchange);
 
 -- +goose Down
 DROP INDEX orders_symbol ON orders;
-DROP INDEX orders_id_symbol ON orders;
+DROP INDEX orders_order_id ON orders;
