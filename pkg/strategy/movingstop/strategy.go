9d7aa2fe22ff680e793e922b1e61cca650e1c4b0
c9s <yoan@maicoin.com>
2020-12-03 08:52:32 +0800

    add movingstop strategy

---
+++ pkg/strategy/movingstop/strategy.go
@@ -0,0 +1,182 @@
+package movingstop
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/c9s/bbgo/pkg/bbgo"
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+// The indicators (SMA and EWMA) that we want to use are returning float64 data.
+type Float64Indicator interface {
+	Last() float64
+}
+
+func init() {
+	// Register the pointer of the strategy struct,
+	// so that bbgo knows what struct to be used to unmarshal the configs (YAML or JSON)
+	// Note: built-in strategies need to imported manually in the bbgo cmd package.
+	bbgo.RegisterStrategy("movingstop", &Strategy{})
+}
+
+type Strategy struct {
+	SourceExchangeName string `json:"sourceExchange"`
+
+	TargetExchangeName string `json:"targetExchange"`
+
+	// These fields will be filled from the config file (it translates YAML to JSON)
+	Symbol string `json:"symbol"`
+
+	// Interval is the interval of the kline channel we want to subscribe,
+	// the kline event will trigger the strategy to check if we need to submit order.
+	Interval types.Interval `json:"interval"`
+
+	Quantity fixedpoint.Value `json:"quantity"`
+
+	OrderType string `json:"orderType"`
+
+	PriceRatio fixedpoint.Value `json:"priceRatio"`
+
+	// MovingAverageType is the moving average indicator type that we want to use,
+	// it could be SMA or EWMA
+	MovingAverageType string `json:"movingAverageType"`
+
+	// MovingAverageInterval is the interval of k-lines for the moving average indicator to calculate,
+	// it could be "1m", "5m", "1h" and so on.  note that, the moving averages are calculated from
+	// the k-line data we subscribed
+	MovingAverageInterval types.Interval `json:"movingAverageInterval"`
+
+	// MovingAverageWindow is the number of the window size of the moving average indicator.
+	// The number of k-lines in the window. generally used window sizes are 7, 25 and 99 in the TradingView.
+	MovingAverageWindow int `json:"movingAverageWindow"`
+
+	order types.Order
+}
+
+func (s *Strategy) Subscribe(sessions map[string]*bbgo.ExchangeSession) {
+	sourceSession := sessions[s.SourceExchangeName]
+	sourceSession.Subscribe(types.KLineChannel, s.Symbol, types.SubscribeOptions{Interval: s.Interval.String()})
+
+	// make sure we have the connection alive
+	targetSession := sessions[s.TargetExchangeName]
+	targetSession.Subscribe(types.KLineChannel, s.Symbol, types.SubscribeOptions{Interval: s.Interval.String()})
+}
+
+func (s *Strategy) clear(ctx context.Context, session *bbgo.ExchangeSession) {
+	if s.order.OrderID > 0 {
+		if err := session.Exchange.CancelOrders(ctx, s.order); err != nil {
+			log.WithError(err).Errorf("can not cancel movingstop order: %+v", s.order)
+		}
+
+		// clear out the existing order
+		s.order = types.Order{}
+	}
+}
+
+func (s *Strategy) place(ctx context.Context, orderExecutor *bbgo.ExchangeOrderExecutor, indicator Float64Indicator, closePrice float64) {
+	movingAveragePrice := indicator.Last()
+
+	// skip it if it's near zero because it's not loaded yet
+	if movingAveragePrice < 0.0001 {
+		return
+	}
+
+	// place stop limit order only when the closed price is greater than the moving average price
+	if closePrice <= movingAveragePrice {
+		return
+	}
+
+	var price float64 = 0.0
+	var orderType = types.OrderTypeStopMarket
+
+	switch strings.ToLower(s.OrderType) {
+	case "market":
+		orderType = types.OrderTypeStopMarket
+	case "limit":
+		orderType = types.OrderTypeStopLimit
+		price = movingAveragePrice
+		if s.PriceRatio > 0 {
+			price = price * s.PriceRatio.Float64()
+		}
+	}
+
+	retOrders, err := orderExecutor.SubmitOrders(ctx, types.SubmitOrder{
+		Symbol:    s.Symbol,
+		Side:      types.SideTypeSell,
+		Type:      orderType,
+		Price:     price,
+		StopPrice: movingAveragePrice,
+		Quantity:  s.Quantity.Float64(),
+	})
+	if err != nil {
+		log.WithError(err).Error("submit order error")
+	}
+
+	if len(retOrders) > 0 {
+		s.order = retOrders[0]
+	}
+}
+
+func (s *Strategy) handleOrderUpdate(order types.Order) {
+	if order.OrderID == s.order.OrderID {
+		s.order = order
+	}
+}
+
+func (s *Strategy) Run(ctx context.Context, _ bbgo.OrderExecutionRouter, sessions map[string]*bbgo.ExchangeSession) error {
+	// source session
+	sourceSession := sessions[s.SourceExchangeName]
+
+	// target exchange
+	session := sessions[s.TargetExchangeName]
+	orderExecutor := bbgo.ExchangeOrderExecutor{
+		Session: session,
+	}
+
+	var indicator Float64Indicator
+	var iw = types.IntervalWindow{Interval: s.MovingAverageInterval, Window: s.MovingAverageWindow}
+
+	var standardIndicatorSet, ok = sourceSession.StandardIndicatorSet(s.Symbol)
+	if !ok {
+		return fmt.Errorf("standardIndicatorSet is nil, symbol %s", s.Symbol)
+	}
+
+	switch strings.ToUpper(s.MovingAverageType) {
+	case "SMA":
+		indicator = standardIndicatorSet.SMA(iw)
+
+	case "EWMA", "EMA":
+		indicator = standardIndicatorSet.EWMA(iw)
+
+	default:
+		return fmt.Errorf("unsupported moving average type: %s", s.MovingAverageType)
+
+	}
+
+	session.Stream.OnOrderUpdate(s.handleOrderUpdate)
+
+	// session.Stream.OnKLineClosed
+	sourceSession.Stream.OnKLineClosed(func(kline types.KLine) {
+		// skip k-lines from other symbols
+		if kline.Symbol != s.Symbol {
+			return
+		}
+
+		if kline.Interval != s.Interval {
+			return
+		}
+
+		closePrice := kline.Close
+
+		// ok, it's our call, we need to cancel the stop limit order first
+		s.clear(ctx, session)
+
+		s.place(ctx, &orderExecutor, indicator, closePrice)
+	})
+	return nil
+}
