b188901ed003c1930bb001db9df02606b97a1e8c
c9s <yoan@maicoin.com>
2020-12-04 19:40:05 +0800

    fix ewma calculation

--- pkg/indicator/ewma.go
+++ pkg/indicator/ewma.go
@@ -27,28 +27,27 @@ func (inc *EWMA) calculateAndUpdate(kLines []types.KLine) {
 
 	var index = len(kLines) - 1
 	var lastK = kLines[index]
-
-	// see https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp
-	var multiplier = 2.0 / float64(inc.Window+1)
-
 	if inc.EndTime != zeroTime && lastK.EndTime.Before(inc.EndTime) {
 		return
 	}
 
-	inc.EndTime = kLines[index].EndTime
-
 	var recentK = kLines[index-(inc.Window-1) : index+1]
-	if len(inc.Values) > 0 {
-		var previousEWMA = inc.Values[len(inc.Values)-1]
-		var ewma = lastK.Close*multiplier + previousEWMA*(1-multiplier)
-		inc.Values.Push(ewma)
-		inc.EmitUpdate(ewma)
-	} else {
-		// The first EWMA is actually SMA
-		var sma = calculateSMA(recentK)
-		inc.Values.Push(sma)
-		inc.EmitUpdate(sma)
+	var multiplier = 2.0 / float64(inc.Window+1)
+	var val = calculateEWMA(recentK, multiplier)
+	// val = calculateSMA(recentK)
+	inc.Values.Push(val)
+	inc.EndTime = lastK.EndTime
+	inc.EmitUpdate(val)
+}
+
+// see https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp
+func calculateEWMA(kLines []types.KLine, multiplier float64) float64 {
+	var end = len(kLines) - 1
+	if end == 0 {
+		return kLines[0].Close
 	}
+
+	return kLines[end].Close*multiplier + (1-multiplier)*calculateEWMA(kLines[:end-1], multiplier)
 }
 
 type KLineWindowUpdater interface {
