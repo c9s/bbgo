a3ad987f76df7a2894ade15ccee971f33efaa385
c9s <yoan@maicoin.com>
2020-12-29 17:46:13 +0800

    add download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,7 +2,11 @@
 osf=$(uname | tr '[:upper:]' '[:lower:]')
 version=v1.5.0
 
-echo "Downloading bbgo"
+if [[ -n $1 ]] ; then
+    version=$1
+fi
+
 curl -L -o bbgo https://github.com/c9s/bbgo/releases/download/$version/bbgo-$osf
 chmod +x bbgo
-echo "Binary downloaded"
+
+echo "bbgo is downloaded at ./bbgo"
