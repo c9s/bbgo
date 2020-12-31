3c7f478ccee489790183274e90185db81e28ed68
c9s <yoan@maicoin.com>
2020-12-31 13:07:39 +0800

    rename movingstop to trailingstop

--- pkg/cmd/builtin.go
+++ pkg/cmd/builtin.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/c9s/bbgo/pkg/strategy/buyandhold"
 	_ "github.com/c9s/bbgo/pkg/strategy/flashcrash"
 	_ "github.com/c9s/bbgo/pkg/strategy/grid"
-	_ "github.com/c9s/bbgo/pkg/strategy/movingstop"
+	_ "github.com/c9s/bbgo/pkg/strategy/trailingstop"
 	_ "github.com/c9s/bbgo/pkg/strategy/pricealert"
 	_ "github.com/c9s/bbgo/pkg/strategy/swing"
 	_ "github.com/c9s/bbgo/pkg/strategy/xpuremaker"
