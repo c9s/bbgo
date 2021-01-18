463a9bf1b7eb840a083f2a407b8ab45db751e9b5
c9s <yoan@maicoin.com>
2021-01-18 16:56:01 +0800

    fix sql syntax

--- migrations/20210118163847_fix_unique_index.sql
+++ migrations/20210118163847_fix_unique_index.sql
@@ -3,7 +3,7 @@
 ALTER TABLE `trades` DROP INDEX `id`;
 -- +end
 -- +begin
-ALTER TABLE `trades` ADD INDEX UNIQUE `id` ON (`exchange`,`symbol`, `side`, `id`);
+ALTER TABLE `trades` ADD UNIQUE INDEX `id` (`exchange`,`symbol`, `side`, `id`);
 -- +end
 
 -- +down
@@ -11,5 +11,5 @@ ALTER TABLE `trades` ADD INDEX UNIQUE `id` ON (`exchange`,`symbol`, `side`, `id`
 ALTER TABLE `trades` DROP INDEX `id`;
 -- +end
 -- +begin
-ALTER TABLE `trades` ADD INDEX UNIQUE `id` ON (`id`);
+ALTER TABLE `trades` ADD UNIQUE INDEX `id` (`id`);
 -- +end
