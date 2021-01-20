34148948ab6df529f8b8c2efd5fc27a803be88a9
c9s <yoan@maicoin.com>
2021-01-20 16:08:14 +0800

    add position and its tests

---
+++ pkg/bbgo/position_test.go
@@ -0,0 +1,33 @@
+package bbgo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+func TestPosition(t *testing.T) {
+	trades := []types.Trade{
+		{
+			Side:     types.SideTypeBuy,
+			Price:    1000.0,
+			Quantity: 0.01,
+		},
+		{
+			Side:     types.SideTypeBuy,
+			Price:    2000.0,
+			Quantity: 0.03,
+		},
+	}
+
+	pos := Position{}
+	for _, trade := range trades {
+		pos.AddTrade(trade)
+	}
+
+	assert.Equal(t, fixedpoint.NewFromFloat(0.04), pos.Base)
+	assert.Equal(t, fixedpoint.NewFromFloat((1000.0*0.01+2000.0*0.03)/0.04), pos.AverageCost)
+}
