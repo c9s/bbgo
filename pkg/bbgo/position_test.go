169af638463b01708083eb344758feb5c5172012
c9s <yoan@maicoin.com>
2021-01-20 16:10:20 +0800

    add more position tests

--- pkg/bbgo/position_test.go
+++ pkg/bbgo/position_test.go
@@ -12,14 +12,16 @@ import (
 func TestPosition(t *testing.T) {
 	trades := []types.Trade{
 		{
-			Side:     types.SideTypeBuy,
-			Price:    1000.0,
-			Quantity: 0.01,
+			Side:          types.SideTypeBuy,
+			Price:         1000.0,
+			Quantity:      0.01,
+			QuoteQuantity: 1000.0 * 0.01,
 		},
 		{
-			Side:     types.SideTypeBuy,
-			Price:    2000.0,
-			Quantity: 0.03,
+			Side:          types.SideTypeBuy,
+			Price:         2000.0,
+			Quantity:      0.03,
+			QuoteQuantity: 2000.0 * 0.03,
 		},
 	}
 
@@ -28,6 +30,7 @@ func TestPosition(t *testing.T) {
 		pos.AddTrade(trade)
 	}
 
+	assert.Equal(t, fixedpoint.NewFromFloat(-70.0), pos.Quote)
 	assert.Equal(t, fixedpoint.NewFromFloat(0.04), pos.Base)
 	assert.Equal(t, fixedpoint.NewFromFloat((1000.0*0.01+2000.0*0.03)/0.04), pos.AverageCost)
 }
