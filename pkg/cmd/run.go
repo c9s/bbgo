568250f4bb5176ea497801f358771db329e3bb61
c9s <yoan@maicoin.com>
2021-01-21 14:42:14 +0800

    define api routes

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -13,6 +13,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 	"github.com/pquerna/otp"
 	log "github.com/sirupsen/logrus"
@@ -47,6 +48,55 @@ var RunCmd = &cobra.Command{
 	// SilenceUsage is an option to silence usage when an error occurs.
 	SilenceUsage: true,
 	RunE:         run,
+}
+
+func runServer(ctx context.Context, userConfig *bbgo.Config, environ *bbgo.Environment) error {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/trades", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/open-orders", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/closed-orders", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/loaded-symbols", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/pnl", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/market/:symbol/closed-orders", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/market/:symbol/open-orders", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/market/:symbol/trades", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sessions/:session/market/:symbol/pnl", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	return r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func runConfig(basectx context.Context, userConfig *bbgo.Config) error {
