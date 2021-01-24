9e7c155aa80ffd09c67041a782b3f97bea7f2a7c
c9s <yoan@maicoin.com>
2021-01-24 18:12:26 +0800

    add basic dashboard layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 import styles from '../styles/Home.module.css'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+
 export default function Home() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
+        <Grid container className={classes.root}>
+          <Grid item xs={12}>
+            <Grid container justify="center">
+              <Grid item>
+                 <Paper className={classes.paper}/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
+
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
