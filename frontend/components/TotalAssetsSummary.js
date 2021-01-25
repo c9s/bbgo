a1c907ea498f1a6fbfa1893129d9b8bd6f334777
c9s <yoan@maicoin.com>
2021-01-25 18:29:31 +0800

    add exchange session panel

---
+++ frontend/components/TotalAssetsSummary.js
@@ -0,0 +1,66 @@
+import {useEffect, useState} from "react";
+import {queryAssets} from "../api/bbgo";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+
+function aggregateAssetsBy(assets, field) {
+    let total = 0.0
+    for (let key in assets) {
+        if (assets[key]) {
+            let a = assets[key]
+            let value = a[field]
+            total += value
+        }
+    }
+
+    return total
+}
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginTop: 12,
+    },
+});
+
+export default function TotalAssetSummary() {
+    const classes = useStyles();
+    const [assets, setAssets] = useState({})
+
+    useEffect(() => {
+        queryAssets((assets) => {
+            setAssets(assets)
+        })
+    }, [])
+
+    return <Card className={classes.root} variant="outlined">
+        <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Total Account Balance
+            </Typography>
+            <Typography variant="h5" component="h2">
+                {Math.round(aggregateAssetsBy(assets, "inBTC") * 1e8) / 1e8} <span>BTC</span>
+            </Typography>
+
+            <Typography className={classes.pos} color="textSecondary">
+                Estimated Value
+            </Typography>
+
+            <Typography variant="h5" component="h3">
+                {Math.round(aggregateAssetsBy(assets, "inUSD") * 100) / 100} <span>USD</span>
+            </Typography>
+        </CardContent>
+    </Card>
+}
---
+++ frontend/src/utils.js
@@ -0,0 +1,28 @@
+
+export function currencyColor(currency) {
+    switch (currency) {
+        case "BTC":
+            return "#f69c3d"
+        case "ETH":
+            return "#497493"
+        case "MCO":
+            return "#032144"
+        case "OMG":
+            return "#2159ec"
+        case "LTC":
+            return "#949494"
+        case "USDT":
+            return "#2ea07b"
+        case "SAND":
+            return "#2E9AD0"
+        case "XRP":
+            return "#00AAE4"
+        case "BCH":
+            return "#8DC351"
+        case "MAX":
+            return "#2D4692"
+        case "TWD":
+            return "#4A7DED"
+
+    }
+}
