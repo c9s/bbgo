505e82d579105eeedb46c6ee659b35d12768088e
c9s <yoan@maicoin.com>
2021-01-29 12:55:11 +0800

    fix layout and margin

--- frontend/components/ExchangeSessionTabPanel.js
+++ frontend/components/ExchangeSessionTabPanel.js
@@ -1,18 +1,28 @@
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Link from "@material-ui/core/Link";
 import React, {useEffect, useState} from "react";
 import {querySessions} from '../api/bbgo'
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+    }
+}));
 
 export default function ExchangeSessionTabPanel() {
+    const classes = useStyles();
+
     const [tabIndex, setTabIndex] = React.useState(0);
     const handleTabClick = (event, newValue) => {
         setTabIndex(newValue);
     };
 
-    const [sessions, setSessions] = useState({})
+    const [sessions, setSessions] = useState([])
 
     useEffect(() => {
         querySessions((sessions) => {
@@ -20,22 +30,21 @@ export default function ExchangeSessionTabPanel() {
         })
     }, [])
 
-    return <Paper>
-        <Box m={4}>
-            <Tabs
-                value={tabIndex}
-                onChange={handleTabClick}
-                indicatorColor="primary"
-                textColor="primary"
-            >
-                <Tab label="Item One"/>
-                <Tab label="Item Two"/>
-                <Tab label="Item Three"/>
-            </Tabs>
-
-            <Link href="/about" color="secondary">
-                Go to the about page
-            </Link>
-        </Box>
+    return <Paper className={classes.paper}>
+        <Typography variant="h4" gutterBottom>
+            Sessions
+        </Typography>
+        <Tabs
+            value={tabIndex}
+            onChange={handleTabClick}
+            indicatorColor="primary"
+            textColor="primary"
+        >
+            {
+                sessions.map((session) => {
+                    return <Tab key={session.name} label={session.name}/>
+                })
+            }
+        </Tabs>
     </Paper>
 }
