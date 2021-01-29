7f9af0a51caf034cd75c8969d7a891e94af2ecd8
c9s <yoan@maicoin.com>
2021-01-29 18:28:23 +0800

    update migration package

---
+++ pkg/migrations/20210129182704_trade_price_quantity_index.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/c9s/rockhopper"
+)
+
+func init() {
+	rockhopper.AddMigration(upTradePriceQuantityIndex, downTradePriceQuantityIndex)
+}
+
+func upTradePriceQuantityIndex(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is applied.
+
+	_, err = tx.ExecContext(ctx, "CREATE INDEX trades_price_quantity ON trades (order_id,price,quantity);")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func downTradePriceQuantityIndex(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is rolled back.
+
+	_, err = tx.ExecContext(ctx, "DROP INDEX trades_price_quantity ON trades")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
