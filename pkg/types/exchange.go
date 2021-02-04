49373869f3a58b447615082c961d24ad967923b9
ycchen <ychi@users.noreply.github.com>
2021-02-04 22:48:35 +0100

    feat: ticker api for types.Exchange

--- pkg/types/exchange.go
+++ pkg/types/exchange.go
@@ -45,6 +45,8 @@ type Exchange interface {
 
 	QueryAccountBalances(ctx context.Context) (BalanceMap, error)
 
+	QueryTicker(ctx context.Context, symbol string) (Ticker, error)
+
 	QueryKLines(ctx context.Context, symbol string, interval Interval, options KLineQueryOptions) ([]KLine, error)
 
 	QueryTrades(ctx context.Context, symbol string, options *TradeQueryOptions) ([]Trade, error)
@@ -68,4 +70,3 @@ type TradeQueryOptions struct {
 	Limit       int64
 	LastTradeID int64
 }
-
