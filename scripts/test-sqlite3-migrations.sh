01ea0885dcffbd0a60fedf25ec1620fb9c04f1c8
c9s <yoan@maicoin.com>
2021-02-05 14:33:12 +0800

    scripts: use force flag to remove bbgo.sqlite3

--- scripts/test-sqlite3-migrations.sh
+++ scripts/test-sqlite3-migrations.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -v bbgo.sqlite3 && rockhopper --config rockhopper_sqlite.yaml up && rockhopper --config rockhopper_sqlite.yaml down --to 1
+rm -fv bbgo.sqlite3 && rockhopper --config rockhopper_sqlite.yaml up && rockhopper --config rockhopper_sqlite.yaml down --to 1
