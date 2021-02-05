f1bce94d4d01297470852b931577749327396c4a
c9s <yoan@maicoin.com>
2021-02-05 14:20:07 +0800

    add script for testing sqlite3 migration

--- migrations/sqlite3/20200819054742_trade_index.sql
+++ migrations/sqlite3/20200819054742_trade_index.sql
@@ -4,6 +4,6 @@ CREATE INDEX trades_symbol_fee_currency ON trades(symbol, fee_currency, traded_a
 CREATE INDEX trades_traded_at_symbol ON trades(traded_at, symbol);
 
 -- +down
-DROP INDEX trades_symbol ON trades;
-DROP INDEX trades_symbol_fee_currency ON trades;
-DROP INDEX trades_traded_at_symbol ON trades;
+DROP INDEX trades_symbol;
+DROP INDEX trades_symbol_fee_currency;
+DROP INDEX trades_traded_at_symbol;
