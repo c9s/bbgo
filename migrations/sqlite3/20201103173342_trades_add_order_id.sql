c4fa0a4e6735c29db1a001520dcc314ce39c3bee
c9s <yoan@maicoin.com>
2021-02-06 09:15:03 +0800

    add default 0 to not null field

--- migrations/sqlite3/20201103173342_trades_add_order_id.sql
+++ migrations/sqlite3/20201103173342_trades_add_order_id.sql
@@ -1,5 +1,5 @@
 -- +up
-ALTER TABLE `trades` ADD COLUMN `order_id` INTEGER NOT NULL;
+ALTER TABLE `trades` ADD COLUMN `order_id` INTEGER NOT NULL DEFATUL 0;
 
 -- +down
 ALTER TABLE `trades` RENAME COLUMN `order_id` TO `order_id_deleted`;
