056fc8131653bd5f113e5533137781c6bb54d0dc
c9s <yoan@maicoin.com>
2021-02-06 09:52:53 +0800

    drop the legacy upgradeDB

--- pkg/bbgo/db.go
+++ pkg/bbgo/db.go
@@ -1,13 +1,9 @@
 package bbgo
 
 import (
-	"context"
-	"database/sql"
-
 	// register the go migrations
 	_ "github.com/c9s/bbgo/pkg/migrations"
 
-	"github.com/c9s/rockhopper"
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -23,28 +19,3 @@ func ConnectMySQL(dsn string) (*sqlx.DB, error) {
 	return sqlx.Connect("mysql", dsn)
 }
 
-func upgradeDB(ctx context.Context, driver string, db *sql.DB) error {
-	dialect, err := rockhopper.LoadDialect(driver)
-	if err != nil {
-		return err
-	}
-
-	loader := &rockhopper.GoMigrationLoader{}
-	migrations, err := loader.Load()
-	if err != nil {
-		return err
-	}
-
-	rh := rockhopper.New(driver, dialect, db)
-
-	currentVersion, err := rh.CurrentVersion()
-	if err != nil {
-		return err
-	}
-
-	if err := rockhopper.Up(ctx, rh, migrations, currentVersion, 0); err != nil {
-		return err
-	}
-
-	return nil
-}
