42977147a3166f45e836c2540db5ed5c3a794f39
c9s <yoan@maicoin.com>
2021-02-06 09:49:05 +0800

    migrations/sqlite3: fix sqlite3 alter syntax

--- migrations/sqlite3/20201103173342_trades_add_order_id.sql
+++ migrations/sqlite3/20201103173342_trades_add_order_id.sql
@@ -1,5 +1,5 @@
 -- +up
-ALTER TABLE `trades` ADD COLUMN `order_id` INTEGER NOT NULL DEFATUL 0;
+ALTER TABLE `trades` ADD COLUMN `order_id` INTEGER;
 
 -- +down
 ALTER TABLE `trades` RENAME COLUMN `order_id` TO `order_id_deleted`;
