48b376147aee66efbaef9ac723313aaa263a9456
ycchen <ychi@users.noreply.github.com>
2021-02-07 22:58:30 +0100

    Address review feedbacks

--- pkg/exchange/max/ticker_test.go
+++ pkg/exchange/max/ticker_test.go
@@ -3,14 +3,15 @@ package max
 import (
 	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAllSymbols(t *testing.T) {
 	e := New("mock_key", "mock_secret")
 	got, err := e.QueryTickers(context.Background())
-	if err != nil {
-		t.Errorf("Max Exchange: Fail to get ticker for all symbols: %s", err)
-	}
+	assert.NoError(t, err)
+
 	if len(got) <= 1 {
 		t.Errorf("Max Exchange: Attempting to get all symbol tickers, but get 1 or less")
 	}
@@ -20,10 +21,7 @@ func TestAllSymbols(t *testing.T) {
 func TestSomeSymbols(t *testing.T) {
 	e := New("mock_key", "mock_secret")
 	got, err := e.QueryTickers(context.Background(), "BTCUSDT", "ETHUSDT")
-
-	if err != nil {
-		t.Errorf("Max Exchange: Fail to get ticker for some symbols: %s", err)
-	}
+	assert.NoError(t, err)
 
 	if len(got) != 2 {
 		t.Errorf("Max Exchange: Attempting to get two symbols, but number of tickers do not match")
@@ -34,9 +32,7 @@ func TestSomeSymbols(t *testing.T) {
 func TestSingleSymbol(t *testing.T) {
 	e := New("mock_key", "mock_secret")
 	got, err := e.QueryTickers(context.Background(), "BTCUSDT")
-	if err != nil {
-		t.Errorf("Max Exchange: Fail to get ticker for single symbol: %s", err)
-	}
+	assert.NoError(t, err)
 
 	if len(got) != 1 {
 		t.Errorf("Max Exchange: Attempting to get one symbol, but number of tickers do not match")
