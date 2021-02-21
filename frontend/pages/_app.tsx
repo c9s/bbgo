e207089ac572f07d72f0482c28d08c034354218a
c9s <yoan@maicoin.com>
2021-02-21 18:21:05 +0800

    add line progress for syncing

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,6 +8,8 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Box from '@material-ui/core/Box';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
@@ -67,6 +69,9 @@ export default function MyApp(props) {
                                         The environment is syncing trades from the exchange sessions.
                                         Please wait a moment...
                                     </DialogContentText>
+                                    <Box m={2}>
+                                        <LinearProgress />
+                                    </Box>
                                 </DialogContent>
                             </Dialog>
                         </React.Fragment>
