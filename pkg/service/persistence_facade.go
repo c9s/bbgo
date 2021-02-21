e01aed0f3d66e4eb8075e5748ec2f3c8b8b4b15a
c9s <yoan@maicoin.com>
2021-02-21 16:55:45 +0800

    add Get method to the persistence service facade

--- pkg/service/persistence_facade.go
+++ pkg/service/persistence_facade.go
@@ -5,3 +5,17 @@ type PersistenceServiceFacade struct {
 	Json   *JsonPersistenceService
 	Memory *MemoryService
 }
+
+// Get returns the preferred persistence service by fallbacks
+// Redis will be preferred at the first position.
+func (facade *PersistenceServiceFacade) Get() PersistenceService {
+	if facade.Redis != nil {
+		return facade.Redis
+	}
+
+	if facade.Json != nil {
+		return facade.Json
+	}
+
+	return facade.Memory
+}
