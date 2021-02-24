db2f96a42df567f2bad991876cd939934eda8337
c9s <yoan@maicoin.com>
2021-02-24 10:46:39 +0800

    add currency position aggregation and tests

--- pkg/service/reward_test.go
+++ pkg/service/reward_test.go
@@ -9,10 +9,11 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/c9s/bbgo/pkg/datatype"
+	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
 )
 
-func Test_RewardService(t *testing.T) {
+func TestRewardService_InsertAndQueryUnspent(t *testing.T) {
 	db, err := prepareDB(t)
 	if err != nil {
 		t.Fatal(err)
@@ -44,7 +45,7 @@ func Test_RewardService(t *testing.T) {
 	assert.Equal(t, types.RewardCommission, rewards[0].Type)
 
 	err = service.Insert(types.Reward{
-		UUID:      "test01",
+		UUID:      "test02",
 		Exchange:  "max",
 		Type:      "airdrop",
 		Currency:  "MAX",
@@ -72,4 +73,70 @@ func Test_RewardService(t *testing.T) {
 	assert.NotEmpty(t, rewards)
 	assert.Len(t, rewards, 1, "should select 1 reward")
 	assert.Equal(t, types.RewardCommission, rewards[0].Type)
+}
+
+
+func TestRewardService_AggregateUnspentCurrencyPosition(t *testing.T) {
+	db, err := prepareDB(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
+
+	ctx := context.Background()
+
+	xdb := sqlx.NewDb(db.DB, "sqlite3")
+	service := &RewardService{DB: xdb}
+
+	now := time.Now()
+
+	err = service.Insert(types.Reward{
+		UUID:      "test01",
+		Exchange:  "max",
+		Type:      "commission",
+		Currency:  "BTC",
+		Quantity:  1,
+		State:     "done",
+		Spent:     false,
+		CreatedAt: datatype.Time(now),
+	})
+	assert.NoError(t, err)
+
+	err = service.Insert(types.Reward{
+		UUID:      "test02",
+		Exchange:  "max",
+		Type:      "commission",
+		Currency:  "LTC",
+		Quantity:  2,
+		State:     "done",
+		Spent:     false,
+		CreatedAt: datatype.Time(now),
+	})
+	assert.NoError(t, err)
+
+	err = service.Insert(types.Reward{
+		UUID:      "test03",
+		Exchange:  "max",
+		Type:      "airdrop",
+		Currency:  "MAX",
+		Quantity:  1000000,
+		State:     "done",
+		Spent:     false,
+		CreatedAt: datatype.Time(now),
+	})
+	assert.NoError(t, err)
+
+	currencyPositions, err := service.AggregateUnspentCurrencyPosition(ctx, types.ExchangeMax, now.Add(-10 * time.Second))
+	assert.NoError(t, err)
+	assert.NotEmpty(t, currencyPositions)
+	assert.Len(t, currencyPositions, 2)
+
+	v, ok := currencyPositions["LTC"]
+	assert.True(t, ok)
+	assert.Equal(t, fixedpoint.Value(2), v)
+
+	v, ok = currencyPositions["BTC"]
+	assert.True(t, ok)
+	assert.Equal(t, fixedpoint.Value(1), v)
 }
