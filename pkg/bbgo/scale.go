83af52c53b325e43f02389977e63778a2f4649cd
c9s <yoan@maicoin.com>
2021-02-28 02:06:33 +0800

    add QuadraticScale

--- pkg/bbgo/scale.go
+++ pkg/bbgo/scale.go
@@ -47,7 +47,6 @@ func (s *ExpScale) Call(x float64) (y float64) {
 	return y
 }
 
-
 type LogScale struct {
 	Domain [2]float64 `json:"domain"`
 	Range  [2]float64 `json:"range"`
@@ -59,7 +58,7 @@ type LogScale struct {
 
 func (s *LogScale) Call(x float64) (y float64) {
 	// y = a * log(x - h) + s
-	y = s.a * math.Log(x - s.h) + s.s
+	y = s.a*math.Log(x-s.h) + s.s
 	return y
 }
 
@@ -89,6 +88,42 @@ func (s *LogScale) Solve() error {
 	// a = (y2 - y1) / log(x2 - h)
 	s.h = s.Domain[0] - 1
 	s.s = s.Range[0]
-	s.a = (s.Range[1] - s.Range[0]) / math.Log(s.Domain[1] - s.h)
+	s.a = (s.Range[1] - s.Range[0]) / math.Log(s.Domain[1]-s.h)
 	return nil
+}
+
+type QuadraticScale struct {
+	Domain [3]float64 `json:"domain"`
+	Range  [3]float64 `json:"range"`
+
+	a, b, c float64
+}
+
+func (s *QuadraticScale) Solve() error {
+	xs := s.Domain
+	ys := s.Range
+	s.a = ((ys[1]-ys[0])*(xs[0]-xs[2]) + (ys[2]-ys[0])*(xs[1]-xs[0])) /
+		((xs[0]-xs[2])*(math.Pow(xs[1], 2)-math.Pow(xs[0], 2)) + (xs[1]-xs[0])*(math.Pow(xs[2], 2)-math.Pow(xs[0], 2)))
+
+	s.b = ((ys[1] - ys[0]) - s.a*(math.Pow(xs[1], 2)-math.Pow(xs[0], 2))) / (xs[1] - xs[0])
+	s.c = ys[1] - s.a*math.Pow(xs[1], 2) - s.b*xs[1]
+	return nil
+}
+
+func (s *QuadraticScale) Call(x float64) (y float64) {
+	// y = a * log(x - h) + s
+	y = s.a * math.Pow(x, 2) + s.b * x + s.c
+	return y
+}
+
+func (s *QuadraticScale) String() string {
+	return s.Formula()
+}
+
+func (s *QuadraticScale) Formula() string {
+	return fmt.Sprintf("f(x) = %f * x ^ 2 + %f * x + %f", s.a, s.b, s.c)
+}
+
+func (s *QuadraticScale) FormulaOf(x float64) string {
+	return fmt.Sprintf("f(%f) = %f * %f ^ 2 + %f * %f + %f", x, s.a, x, s.b, x, s.c)
 }
