f83f17f8b8038cbf2260bc727ebb36e7532d2858
ycdesu <sychewo@gmail.com>
2021-02-27 19:27:44 +0800

    ftx: handle message in a new struct

--- pkg/exchange/ftx/stream.go
+++ pkg/exchange/ftx/stream.go
@@ -23,9 +23,7 @@ func NewStream(key, secret string) *Stream {
 		wsService:      wss,
 	}
 
-	wss.OnMessage(func(message []byte) {
-		logger.Infof("=> message: %+v", string(message))
-	})
+	wss.OnMessage(messageHandler{s.StandardStream}.handleMessage)
 	return s
 }
 
@@ -37,44 +35,11 @@ func (s *Stream) SetPublicOnly() {
 	atomic.StoreInt32(&s.publicOnly, 1)
 }
 
-func (s *Stream) Close() error {
-	return s.wsService.Close()
-}
-
 func (s *Stream) Subscribe(channel types.Channel, symbol string, _ types.SubscribeOptions) {
 	if err := s.wsService.Subscribe(channel, symbol); err != nil {
 		logger.WithError(err).Errorf("subscribe failed, should never happen")
 	}
 }
-
-func (s *Stream) OnTradeUpdate(cb func(trade types.Trade)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnOrderUpdate(cb func(order types.Order)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnBalanceSnapshot(cb func(balances types.BalanceMap)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnBalanceUpdate(cb func(balances types.BalanceMap)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnKLineClosed(cb func(kline types.KLine)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnKLine(cb func(kline types.KLine)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnBookUpdate(cb func(book types.OrderBook)) {
-	panic("implement me")
-}
-
-func (s *Stream) OnBookSnapshot(cb func(book types.OrderBook)) {
-	panic("implement me")
+func (s *Stream) Close() error {
+	return s.wsService.Close()
 }
