4a34639e059f88843dee1b0ab924984b9b919c38
ycdesu <sychewo@gmail.com>
2021-02-27 18:41:46 +0800

    ftx: send subscriptions when connected

--- pkg/exchange/ftx/websocket.go
+++ pkg/exchange/ftx/websocket.go
@@ -1,9 +1,14 @@
 package ftx
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
+	"github.com/gorilla/websocket"
+
 	"github.com/c9s/bbgo/pkg/service"
+	"github.com/c9s/bbgo/pkg/types"
 )
 
 type WebsocketService struct {
@@ -11,6 +16,8 @@ type WebsocketService struct {
 
 	key    string
 	secret string
+
+	subscriptions []SubscribeRequest
 }
 
 const endpoint = "wss://ftx.com/ws/"
@@ -21,7 +28,38 @@ func NewWebsocketService(key string, secret string) *WebsocketService {
 		key:                 key,
 		secret:              secret,
 	}
+	s.OnConnected(func(_ *websocket.Conn) {
+		if err := s.sendSubscriptions(); err != nil {
+			s.EmitError(err)
+		}
+	})
 	return s
+}
+
+func (w *WebsocketService) Subscribe(channel types.Channel, symbol string) error {
+	r := SubscribeRequest{
+		Operation: subscribe,
+	}
+	if channel != types.BookChannel {
+		return fmt.Errorf("unsupported channel %+v", channel)
+	}
+	r.Channel = orderbook
+	r.Market = strings.ToUpper(strings.TrimSpace(symbol))
+
+	w.subscriptions = append(w.subscriptions, r)
+	return nil
+}
+
+var errSubscriptionFailed = fmt.Errorf("failed to subscribe")
+
+func (w *WebsocketService) sendSubscriptions() error {
+	conn := w.Conn()
+	for _, s := range w.subscriptions {
+		if err := conn.WriteJSON(s); err != nil {
+			return fmt.Errorf("can't send subscription request %+v: %w", s, errSubscriptionFailed)
+		}
+	}
+	return nil
 }
 
 func (w *WebsocketService) Close() error {
