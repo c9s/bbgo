a5aa4ec938583cb6a3f5afa133f0ede36317c04a
ycdesu <sychewo@gmail.com>
2021-03-13 09:51:31 +0800

    ftx: implemenet place order func

--- pkg/exchange/ftx/rest_order_request.go
+++ pkg/exchange/ftx/rest_order_request.go
@@ -10,7 +10,59 @@ type orderRequest struct {
 	*restRequest
 }
 
-func (r *orderRequest) OpenOrders(ctx context.Context, market string) (orders, error) {
+/*
+{
+  "market": "XRP-PERP",
+  "side": "sell",
+  "price": 0.306525,
+  "type": "limit",
+  "size": 31431.0,
+  "reduceOnly": false,
+  "ioc": false,
+  "postOnly": false,
+  "clientId": null
+}
+*/
+type PlaceOrderPayload struct {
+	Market     string
+	Side       string
+	Price      float64
+	Type       string
+	Size       float64
+	ReduceOnly bool
+	IOC        bool
+	PostOnly   bool
+	ClientID   string
+}
+
+func (r *orderRequest) PlaceOrder(ctx context.Context, p PlaceOrderPayload) (orderResponse, error) {
+	resp, err := r.
+		Method("POST").
+		ReferenceURL("api/orders").
+		Payloads(map[string]interface{}{
+			"market":     p.Market,
+			"side":       p.Side,
+			"price":      p.Price,
+			"type":       p.Type,
+			"size":       p.Size,
+			"reduceOnly": p.ReduceOnly,
+			"ioc":        p.IOC,
+			"postOnly":   p.PostOnly,
+			"clientId":   p.ClientID,
+		}).
+		DoAuthenticatedRequest(ctx)
+
+	if err != nil {
+		return orderResponse{}, err
+	}
+	var o orderResponse
+	if err := json.Unmarshal(resp.Body, &o); err != nil {
+		return orderResponse{}, fmt.Errorf("failed to unmarshal order response body to json: %w", err)
+	}
+
+	return o, nil
+}
+
 func (r *orderRequest) OpenOrders(ctx context.Context, market string) (ordersResponse, error) {
 	resp, err := r.
 		Method("GET").
