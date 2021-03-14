a9afb27d0817e03d3103ee22ca5d203cd256ec11
c9s <yoan@maicoin.com>
2021-03-14 10:58:26 +0800

    add migration files

---
+++ migrations/sqlite3/20210307201830_add_deposits_table.sql
@@ -0,0 +1,31 @@
+-- +up
+-- +begin
+CREATE TABLE `deposits`
+(
+    `gid`      INTEGER PRIMARY KEY AUTOINCREMENT,
+    `exchange` VARCHAR(24)    NOT NULL,
+
+    -- asset is the asset name (currency)
+    `asset`    VARCHAR(10)    NOT NULL,
+
+    `address`  VARCHAR(64)    NOT NULL DEFAULT '',
+    `amount`   DECIMAL(16, 8) NOT NULL,
+    `txn_id`   VARCHAR(64)    NOT NULL,
+    `time`     DATETIME(3)    NOT NULL
+);
+-- +end
+-- +begin
+CREATE UNIQUE INDEX `deposits_txn_id` ON `deposits` (`exchange`, `txn_id`);
+-- +end
+
+
+-- +down
+
+-- +begin
+DROP INDEX IF EXISTS `deposits_txn_id`;
+-- +end
+
+-- +begin
+DROP TABLE IF EXISTS `deposits`;
+-- +end
+
