ccbb78ce4d97fe63b5306cc942f0c7cce7f5b9d0
c9s <yoan@maicoin.com>
2021-03-14 10:42:39 +0800

    migration: extend tx id and address size

--- migrations/mysql/20210301140656_add_withdraws_table.sql
+++ migrations/mysql/20210301140656_add_withdraws_table.sql
@@ -8,11 +8,11 @@ CREATE TABLE `withdraws`
     -- asset is the asset name (currency)
     `asset`            VARCHAR(10)     NOT NULL,
 
-    `address`          VARCHAR(64)     NOT NULL,
+    `address`          VARCHAR(128)     NOT NULL,
     `network`          VARCHAR(32)     NOT NULL DEFAULT '',
 
     `amount`           DECIMAL(16, 8)  NOT NULL,
-    `txn_id`           VARCHAR(64)     NOT NULL,
+    `txn_id`           VARCHAR(256)    NOT NULL,
     `txn_fee`          DECIMAL(16, 8)  NOT NULL DEFAULT 0,
     `txn_fee_currency` VARCHAR(32)     NOT NULL DEFAULT '',
     `time`             DATETIME(3)     NOT NULL,
--- migrations/sqlite3/20210307201830_add_deposits_table.sql
+++ migrations/sqlite3/20210307201830_add_deposits_table.sql
@@ -8,9 +8,9 @@ CREATE TABLE `deposits`
     -- asset is the asset name (currency)
     `asset`    VARCHAR(10)    NOT NULL,
 
-    `address`  VARCHAR(64)    NOT NULL DEFAULT '',
+    `address`  VARCHAR(128)    NOT NULL DEFAULT '',
     `amount`   DECIMAL(16, 8) NOT NULL,
-    `txn_id`   VARCHAR(64)    NOT NULL,
+    `txn_id`   VARCHAR(256)    NOT NULL,
     `time`     DATETIME(3)    NOT NULL
 );
 -- +end
