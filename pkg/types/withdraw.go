b051f1ecaa9d09f39cea722de8bbf0d1d6cc4426
c9s <yoan@maicoin.com>
2021-03-14 11:01:05 +0800

    types: update deposit fields and withdraw fields

--- pkg/types/withdraw.go
+++ pkg/types/withdraw.go
@@ -1,22 +1,28 @@
 package types
 
-import "time"
+import (
+	"time"
+
+	"github.com/c9s/bbgo/pkg/datatype"
+)
 
 type Withdraw struct {
-	ID         string  `json:"id"`
-	Asset      string  `json:"asset"`
-	Amount     float64 `json:"amount"`
-	Address    string  `json:"address"`
-	AddressTag string  `json:"addressTag"`
-	Status     string  `json:"status"`
+	GID        int64        `json:"gid" db:"gid"`
+	Exchange   ExchangeName `json:"exchange" db:"exchange"`
+	Asset      string       `json:"asset" db:"asset"`
+	Amount     float64      `json:"amount" db:"amount"`
+	Address    string       `json:"address" db:"address"`
+	AddressTag string       `json:"addressTag"`
+	Status     string       `json:"status"`
 
-	TransactionID   string    `json:"txId"`
-	TransactionFee  float64   `json:"transactionFee"`
-	WithdrawOrderID string    `json:"withdrawOrderId"`
-	ApplyTime       time.Time `json:"applyTime"`
-	Network         string    `json:"network"`
+	TransactionID          string        `json:"transactionID" db:"txn_id"`
+	TransactionFee         float64       `json:"transactionFee" db:"txn_fee"`
+	TransactionFeeCurrency string        `json:"transactionFeeCurrency" db:"txn_fee_currency"`
+	WithdrawOrderID        string        `json:"withdrawOrderId"`
+	ApplyTime              datatype.Time `json:"applyTime" db:"time"`
+	Network                string        `json:"network" db:"network"`
 }
 
 func (w Withdraw) EffectiveTime() time.Time {
-	return w.ApplyTime
+	return w.ApplyTime.Time()
 }
