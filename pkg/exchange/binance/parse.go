cd0dadfd24ad07b5119384b85a684433ffae2c04
c9s <yoan@maicoin.com>
2021-03-15 17:31:07 +0800

    align fields and tags

--- pkg/exchange/binance/parse.go
+++ pkg/exchange/binance/parse.go
@@ -410,11 +410,10 @@ type KLine struct {
 	Open  string `json:"o"`
 	Close string `json:"c"`
 	High  string `json:"h"`
+	Low   string `json:"l"`
 
-	Low string `json:"l"`
-
-	Volume           string `json:"v"` // base asset volume (like 10 BTC)
-	QuoteVolume      string `json:"q"` // quote asset volume
+	Volume      string `json:"v"` // base asset volume (like 10 BTC)
+	QuoteVolume string `json:"q"` // quote asset volume
 
 	TakerBaseVolume  string `json:"V"` // taker buy base asset volume (like 10 BTC)
 	TakerQuoteVolume string `json:"Q"` // taker buy quote asset volume (like 1000USDT)
