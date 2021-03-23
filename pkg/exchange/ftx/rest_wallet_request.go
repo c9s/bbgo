17b6122cf8dff94287b6eef6dca72a666b424ed7
ycdesu <sychewo@gmail.com>
2021-03-23 22:26:18 +0800

    ftx: assign query string to order/wallet requests

--- pkg/exchange/ftx/rest_wallet_request.go
+++ pkg/exchange/ftx/rest_wallet_request.go
@@ -4,16 +4,31 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"time"
 )
 
 type walletRequest struct {
 	*restRequest
 }
 
-func (r *walletRequest) DepositHistory(ctx context.Context) (depositHistoryResponse, error) {
+func (r *walletRequest) DepositHistory(ctx context.Context, since time.Time, until time.Time, limit int) (depositHistoryResponse, error) {
+	q := make(map[string]string)
+	if limit > 0 {
+		q["limit"] = strconv.Itoa(limit)
+	}
+
+	if since != (time.Time{}) {
+		q["start_time"] = strconv.FormatInt(since.Unix(), 10)
+	}
+	if until != (time.Time{}) {
+		q["end_time"] = strconv.FormatInt(until.Unix(), 10)
+	}
+
 	resp, err := r.
 		Method("GET").
 		ReferenceURL("api/wallet/deposits").
+		Query(q).
 		DoAuthenticatedRequest(ctx)
 
 	if err != nil {
