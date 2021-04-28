b1605adcd9b6d99a5cef45245fea0bc4ba856927
c9s <yoan@maicoin.com>
2021-04-28 20:25:51 +0800

    bump setup script to v1.15.3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+set -e
 osf=$(uname | tr '[:upper:]' '[:lower:]')
-version=v1.15.2
+version=v1.15.3
+dist_file=bbgo-$version-$osf-amd64.tar.gz
 
-echo "Downloading bbgo"
-curl -L -o bbgo https://github.com/c9s/bbgo/releases/download/$version/bbgo-$osf
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+function warn()
+{
+    echo -e "${YELLOW}$@${NC}"
+}
+
+function error()
+{
+    echo -e "${RED}$@${NC}"
+}
+
+function info()
+{
+    echo -e "${GREEN}$@${NC}"
+}
+
+info "downloading..."
+curl -O -L https://github.com/c9s/bbgo/releases/download/$version/$dist_file
+tar xzf $dist_file
+mv bbgo-$osf bbgo
 chmod +x bbgo
-echo "Binary downloaded"
+info "downloaded successfully"
 
 if [[ -e "bbgo.yaml" ]] ; then
   echo "Found existing bbgo.yaml, you will overwrite the existing bbgo.yaml file!"
