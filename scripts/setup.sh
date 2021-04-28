87178b9115ebf1d5d5867ca6b47fcb38eea68db5
c9s <yoan@maicoin.com>
2021-04-28 17:56:46 +0800

    upgrade setup script to v1.15.2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 osf=$(uname | tr '[:upper:]' '[:lower:]')
-version=v1.6.0
+version=v1.15.2
 
 echo "Downloading bbgo"
 curl -L -o bbgo https://github.com/c9s/bbgo/releases/download/$version/bbgo-$osf
