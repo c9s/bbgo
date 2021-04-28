00393de0bb4b703c88e907421f066623b14be0bf
c9s <yoan@maicoin.com>
2021-04-28 19:20:55 +0800

    extract client order id generation

---
+++ pkg/exchange/binance/exchange_test.go
@@ -0,0 +1,17 @@
+package binance
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_newClientOrderID(t *testing.T) {
+	cID := newSpotClientOrderID("")
+	assert.Len(t, cID, 32)
+	strings.HasPrefix(cID, "x-" + spotBrokerID)
+
+	cID = newSpotClientOrderID("myid1")
+	assert.Equal(t, cID, "x-" + spotBrokerID + "myid1")
+}
