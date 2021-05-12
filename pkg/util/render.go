ffba025cc3825919dc09f3558525c69e3171d26b
c9s <yoan@maicoin.com>
2021-05-12 12:54:46 +0800

    improve error messages

--- pkg/util/render.go
+++ pkg/util/render.go
@@ -2,21 +2,24 @@ package util
 
 import (
 	"bytes"
-	"github.com/sirupsen/logrus"
 	"text/template"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Render(tpl string, args interface{}) string {
 	var buf = bytes.NewBuffer(nil)
 	tmpl, err := template.New("tmp").Parse(tpl)
 	if err != nil {
-		logrus.WithError(err).Error("template error")
+		logrus.WithError(err).Error("template parse error")
 		return ""
 	}
+
 	err = tmpl.Execute(buf, args)
 	if err != nil {
-		logrus.WithError(err).Error("template error")
+		logrus.WithError(err).Error("template execute error")
 		return ""
 	}
+
 	return buf.String()
 }
