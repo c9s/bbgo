452507423a701a1182d1eb54ba960c7a9e7642cd
c9s <yoan@maicoin.com>
2021-05-21 01:36:58 +0800

    implement rbtree delete

---
+++ pkg/rbtbook/tree_test.go
@@ -0,0 +1,37 @@
+package rbtbook
+
+import (
+	"testing"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTree(t *testing.T) {
+	tree := NewTree()
+	tree.Insert(fixedpoint.NewFromFloat(3000.0), fixedpoint.NewFromFloat(10.0))
+	assert.NotNil(t, tree.Root)
+
+	tree.Insert(fixedpoint.NewFromFloat(4000.0), fixedpoint.NewFromFloat(10.0))
+	tree.Insert(fixedpoint.NewFromFloat(2000.0), fixedpoint.NewFromFloat(10.0))
+
+	// root is always black
+	assert.Equal(t, fixedpoint.NewFromFloat(3000.0), tree.Root.Key)
+	assert.Equal(t, Black, tree.Root.Color)
+
+	assert.Equal(t, fixedpoint.NewFromFloat(2000.0), tree.Root.Left.Key)
+	assert.Equal(t, Red, tree.Root.Left.Color)
+
+	assert.Equal(t, fixedpoint.NewFromFloat(4000.0), tree.Root.Right.Key)
+	assert.Equal(t, Red, tree.Root.Right.Color)
+
+	// should rotate
+	tree.Insert(fixedpoint.NewFromFloat(1500.0), fixedpoint.NewFromFloat(10.0))
+	tree.Insert(fixedpoint.NewFromFloat(1000.0), fixedpoint.NewFromFloat(10.0))
+
+	deleted := tree.Delete(fixedpoint.NewFromFloat(1000.0))
+	assert.True(t, deleted)
+
+	deleted = tree.Delete(fixedpoint.NewFromFloat(1500.0))
+	assert.True(t, deleted)
+}
