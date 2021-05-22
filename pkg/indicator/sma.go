f1d3a53e620ec326f72bb3776a42a50aa26b7d51
なるみ <weaper@gmail.com>
2021-05-22 20:20:48 +0800

    Move Float64Slice to types

--- pkg/indicator/sma.go
+++ pkg/indicator/sma.go
@@ -2,7 +2,6 @@ package indicator
 
 import (
 	"fmt"
-	"math"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -10,64 +9,12 @@ import (
 	"github.com/c9s/bbgo/pkg/types"
 )
 
-type Float64Slice []float64
-
-func (s *Float64Slice) Push(v float64) {
-	*s = append(*s, v)
-}
-
-func (s *Float64Slice) Pop(i int64) (v float64) {
-	v = (*s)[i]
-	*s = append((*s)[:i], (*s)[i+1:]...)
-	return v
-}
-
-func (s Float64Slice) Max() float64 {
-	m := -math.MaxFloat64
-	for _, v := range s {
-		m = math.Max(m, v)
-	}
-	return m
-}
-
-func (s Float64Slice) Min() float64 {
-	m := math.MaxFloat64
-	for _, v := range s {
-		m = math.Min(m, v)
-	}
-	return m
-}
-
-func (s Float64Slice) Sum() (sum float64) {
-	for _, v := range s {
-		sum += v
-	}
-	return sum
-}
-
-func (s Float64Slice) Mean() (mean float64) {
-	return s.Sum() / float64(len(s))
-}
-
-func (s Float64Slice) Tail(size int) Float64Slice {
-	length := len(s)
-	if length <= size {
-		win := make(Float64Slice, length)
-		copy(win, s)
-		return win
-	}
-
-	win := make(Float64Slice, size)
-	copy(win, s[length-size:])
-	return win
-}
-
 var zeroTime time.Time
 
 //go:generate callbackgen -type SMA
 type SMA struct {
 	types.IntervalWindow
-	Values  Float64Slice
+	Values  types.Float64Slice
 	EndTime time.Time
 
 	UpdateCallbacks []func(value float64)
