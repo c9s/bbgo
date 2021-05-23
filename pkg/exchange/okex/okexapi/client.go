dd67a5eb2aa41bbead203614c89e3b6c75921521
c9s <yoan@maicoin.com>
2021-05-23 12:11:27 +0800

    implement okex balances endpoint

--- pkg/exchange/okex/okexapi/client.go
+++ pkg/exchange/okex/okexapi/client.go
@@ -10,15 +10,20 @@ import (
 	"strings"
 	"time"
 
+	"github.com/c9s/bbgo/pkg/util"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
+const defaultHTTPTimeout = time.Second * 15
 const RestBaseURL = "https://www.okex.com/"
 const PublicWebSocketURL = "wss://ws.okex.com:8443/ws/v5/public"
 const PrivateWebSocketURL = "wss://ws.okex.com:8443/ws/v5/private"
 
 type RestClient struct {
 	BaseURL *url.URL
+
+	client *http.Client
 
 	Key, Secret, Passphrase string
 }
@@ -31,6 +36,9 @@ func NewClient() *RestClient {
 
 	return &RestClient{
 		BaseURL: u,
+		client: &http.Client{
+			Timeout: defaultHTTPTimeout,
+		},
 	}
 }
 
@@ -83,9 +91,11 @@ func (c *RestClient) newAuthenticatedRequest(method, refURL string, params url.V
 	pathURL := c.BaseURL.ResolveReference(rel)
 	path := pathURL.Path
 
-	// 2020-12-08T09:08:57.715Z
-	t := time.Now()
+	// set location to UTC so that it outputs "2020-12-08T09:08:57.715Z"
+	t := time.Now().In(time.UTC)
 	timestamp := t.Format("2006-01-02T15:04:05.999Z07:00")
+	log.Info(timestamp)
+
 	payload := timestamp + strings.ToUpper(method) + path
 	sign := signPayload(payload, c.Secret)
 
@@ -103,6 +113,70 @@ func (c *RestClient) newAuthenticatedRequest(method, refURL string, params url.V
 	req.Header.Add("OK-ACCESS-PASSPHRASE", c.Passphrase)
 
 	return req, nil
+}
+
+type BalanceDetail struct {
+	Currency                string `json:"ccy"`
+	Available               string `json:"availEq"`
+	CashBalance             string `json:"cashBal"`
+	OrderFrozen             string `json:"ordFrozen"`
+	Frozen                  string `json:"frozenBal"`
+	Equity                  string `json:"eq"`
+	EquityInUSD             string `json:"eqUsd"`
+	UpdateTime              string `json:"uTime"`
+	UnrealizedProfitAndLoss string `json:"upl"`
+}
+
+type BalanceSummary struct {
+	TotalEquityInUSD string          `json:"totalEq"`
+	UpdateTime       string          `json:"uTime"`
+	Details          []BalanceDetail `json:"details"`
+}
+
+type BalanceSummaryList []BalanceSummary
+
+func (c *RestClient) Balances() (BalanceSummaryList, error) {
+	req, err := c.newAuthenticatedRequest("GET", "/api/v5/account/balance", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := c.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var balanceResponse struct {
+		Code    string           `json:"code"`
+		Message string           `json:"msg"`
+		Data    []BalanceSummary `json:"data"`
+	}
+	if err := response.DecodeJSON(&balanceResponse); err != nil {
+		return nil, err
+	}
+
+	return balanceResponse.Data, nil
+}
+
+// sendRequest sends the request to the API server and handle the response
+func (c *RestClient) sendRequest(req *http.Request) (*util.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// newResponse reads the response body and return a new Response object
+	response, err := util.NewResponse(resp)
+	if err != nil {
+		return response, err
+	}
+
+	// Check error, if there is an error, return the ErrorResponse struct type
+	if response.IsError() {
+		return response, errors.New(string(response.Body))
+	}
+
+	return response, nil
 }
 
 func signPayload(payload string, secret string) string {
