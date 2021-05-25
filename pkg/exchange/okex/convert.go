c8265fc2e25a7cc9aa631a28ce3da15001e29d19
c9s <yoan@maicoin.com>
2021-05-26 02:53:55 +0800

    gensymbols to generate spot symbol map

--- pkg/exchange/okex/convert.go
+++ pkg/exchange/okex/convert.go
@@ -6,9 +6,10 @@ func toGlobalSymbol(symbol string) string {
 	return strings.ReplaceAll(symbol, "-", "")
 }
 
-//go:generate sh -c "echo \"package okex\nvar symbolMap = map[string]string{\n\" $(curl -s -L 'https://okex.com/api/v5/public/instruments?instType=SPOT' | jq -r '.data[] | \"\\(.instId | sub(\"-\" ; \"\") | tojson ): \\( .instId | tojson),\n\"') \"\n}\" > symbols.go"
+////go:generate sh -c "echo \"package okex\nvar spotSymbolMap = map[string]string{\n\" $(curl -s -L 'https://okex.com/api/v5/public/instruments?instType=SPOT' | jq -r '.data[] | \"\\(.instId | sub(\"-\" ; \"\") | tojson ): \\( .instId | tojson),\n\"') \"\n}\" > symbols.go"
+//go:generate go run gensymbols.go
 func toLocalSymbol(symbol string) string {
-	if s, ok := symbolMap[symbol]; ok {
+	if s, ok := spotSymbolMap[symbol]; ok {
 		return s
 	}
 
