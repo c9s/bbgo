364e6fc990d39690284cca5354689b41ac0bce24
c9s <yoan@maicoin.com>
2021-05-26 02:37:28 +0800

    okex: add local symbol convert function

--- pkg/exchange/okex/convert.go
+++ pkg/exchange/okex/convert.go
@@ -5,3 +5,12 @@ import "strings"
 func toGlobalSymbol(symbol string) string {
 	return strings.ReplaceAll(symbol, "-", "")
 }
+
+func toLocalSymbol(symbol string) string {
+	if s, ok := symbolMap[symbol]; ok {
+		return s
+	}
+
+	log.Errorf("failed to look up local symbol from %s", symbol)
+	return symbol
+}
