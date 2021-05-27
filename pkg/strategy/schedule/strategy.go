d932a686a04b51a40bd6e518dd2756533b831fe7
c9s <yoan@maicoin.com>
2021-05-28 03:15:29 +0800

    fix strategy market data stream usage

--- pkg/strategy/schedule/strategy.go
+++ pkg/strategy/schedule/strategy.go
@@ -128,7 +128,7 @@ func (s *Strategy) Run(ctx context.Context, orderExecutor bbgo.OrderExecutor, se
 		}
 	}
 
-	session.UserDataStream.OnKLineClosed(func(kline types.KLine) {
+	session.MarketDataStream.OnKLineClosed(func(kline types.KLine) {
 		if kline.Symbol != s.Symbol {
 			return
 		}
