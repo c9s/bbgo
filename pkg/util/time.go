191c97d3e1bbb5f24a9c7561243158bbfea0e2f6
c9s <yoan@maicoin.com>
2021-06-26 20:09:26 +0800

    util: add BeginningOfTheDay function

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -9,3 +9,9 @@ func MillisecondsJitter(d time.Duration, jitterInMilliseconds int) time.Duration
 	n := rand.Intn(jitterInMilliseconds)
 	return d + time.Duration(n)*time.Millisecond
 }
+
+func BeginningOfTheDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+}
+
