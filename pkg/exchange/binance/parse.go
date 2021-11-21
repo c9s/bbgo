15b37e17c202f24dad9a7931e98f7fc5adfef284
Austin <austin362667@gmail.com>
2021-11-22 03:42:31 +0800

    binance: reformat kline handler

--- pkg/exchange/binance/parse.go
+++ pkg/exchange/binance/parse.go
@@ -476,6 +476,34 @@ type KLineEvent struct {
 	KLine  KLine  `json:"k,omitempty"`
 }
 
+/*
+kline
+{
+  "e": "kline",     // KLineEvent type
+  "E": 123456789,   // KLineEvent time
+  "s": "BNBBTC",    // Symbol
+  "k": {
+    "t": 123400000, // Kline start time
+    "T": 123460000, // Kline close time
+    "s": "BNBBTC",  // Symbol
+    "i": "1m",      // Interval
+    "f": 100,       // First trade ID
+    "L": 200,       // Last trade ID
+    "o": "0.0010",  // Open price
+    "c": "0.0020",  // Close price
+    "h": "0.0025",  // High price
+    "l": "0.0015",  // Low price
+    "v": "1000",    // Base asset volume
+    "n": 100,       // Number of trades
+    "x": false,     // Is this kline closed?
+    "q": "1.0000",  // Quote asset volume
+    "V": "500",     // Taker buy base asset volume
+    "Q": "0.500",   // Taker buy quote asset volume
+    "B": "123456"   // Ignore
+  }
+}
+*/
+
 func (k *KLine) KLine() types.KLine {
 	return types.KLine{
 		Exchange:                 types.ExchangeBinance,
@@ -558,36 +586,6 @@ type ContinuousKLineEvent struct {
 }
 */
 
-/*
-
-kline
-
-{
-  "e": "kline",     // KLineEvent type
-  "E": 123456789,   // KLineEvent time
-  "s": "BNBBTC",    // Symbol
-  "k": {
-    "t": 123400000, // Kline start time
-    "T": 123460000, // Kline close time
-    "s": "BNBBTC",  // Symbol
-    "i": "1m",      // Interval
-    "f": 100,       // First trade ID
-    "L": 200,       // Last trade ID
-    "o": "0.0010",  // Open price
-    "c": "0.0020",  // Close price
-    "h": "0.0025",  // High price
-    "l": "0.0015",  // Low price
-    "v": "1000",    // Base asset volume
-    "n": 100,       // Number of trades
-    "x": false,     // Is this kline closed?
-    "q": "1.0000",  // Quote asset volume
-    "V": "500",     // Taker buy base asset volume
-    "Q": "0.500",   // Taker buy quote asset volume
-    "B": "123456"   // Ignore
-  }
-}
-
-*/
 type EventBase struct {
 	Event string `json:"e"` // event
 	Time  int64  `json:"E"`
