25b47fb2c451995525026cd31f7adf5d05ae7ddd
TonyQ <tonylovejava@gmail.com>
2021-12-04 03:06:04 +0000

    compile and update migration package

---
+++ pkg/migrations/20210129182704_trade_price_quantity_index.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"database/sql"
+	"context"
+
+	"github.com/c9s/rockhopper"
+)
+
+func init() {
+	rockhopper.AddMigration(upTradePriceQuantityIndex, downTradePriceQuantityIndex)
+}
+
+func upTradePriceQuantityIndex(ctx context.Context, tx *sql.Tx) (err error) {
+	// This code is executed when the migration is applied.
+
+	_, err = tx.ExecContext(ctx, "CREATE INDEX trades_price_quantity ON trades (order_id,price,quantity);")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func downTradePriceQuantityIndex(ctx context.Context, tx *sql.Tx) (err error) {
+	// This code is executed when the migration is rolled back.
+
+	_, err = tx.ExecContext(ctx, "DROP INDEX trades_price_quantity ON trades")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
