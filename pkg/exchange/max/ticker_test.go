bf81adb116d121d92a0dc94ea4c2256ebd55f94e
c9s <yoan@maicoin.com>
2021-12-05 01:08:50 +0800

    max: fix query ticker tests

--- pkg/exchange/max/ticker_test.go
+++ pkg/exchange/max/ticker_test.go
@@ -7,35 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAllSymbols(t *testing.T) {
+func TestExchange_QueryTickers_AllSymbols(t *testing.T) {
 	e := New("mock_key", "mock_secret")
 	got, err := e.QueryTickers(context.Background())
-	assert.NoError(t, err)
-
-	if len(got) <= 1 {
-		t.Errorf("Max Exchange: Attempting to get all symbol tickers, but get 1 or less")
+	if assert.NoError(t, err) {
+		assert.True(t, len(got) > 1, "max: attempting to get all symbol tickers, but get 1 or less")
 	}
-
 }
 
-func TestSomeSymbols(t *testing.T) {
+func TestExchange_QueryTickers_SomeSymbols(t *testing.T) {
 	e := New("mock_key", "mock_secret")
 	got, err := e.QueryTickers(context.Background(), "BTCUSDT", "ETHUSDT")
-	assert.NoError(t, err)
-
-	if len(got) != 2 {
-		t.Errorf("Max Exchange: Attempting to get two symbols, but number of tickers do not match")
-
+	if assert.NoError(t, err) {
+		assert.Len(t, got, 2, "max: attempting to get two symbols, but number of tickers do not match")
 	}
 }
 
-func TestSingleSymbol(t *testing.T) {
+func TestExchange_QueryTickers_SingleSymbol(t *testing.T) {
 	e := New("mock_key", "mock_secret")
 	got, err := e.QueryTickers(context.Background(), "BTCUSDT")
-	assert.NoError(t, err)
-
-	if len(got) != 1 {
-		t.Errorf("Max Exchange: Attempting to get one symbol, but number of tickers do not match")
-
+	if assert.NoError(t, err) {
+		assert.Len(t, got, 1, "max: attempting to get 1 symbols, but number of tickers do not match")
 	}
 }
