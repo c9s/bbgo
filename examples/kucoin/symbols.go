50b79cb742717b101ef87fa7a3d32e7c6ae875a5
c9s <yoan@maicoin.com>
2021-12-11 00:44:55 +0800

    implement ListSymbols api

---
+++ examples/kucoin/symbols.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var symbolsCmd = &cobra.Command{
+	Use: "symbols",
+
+	// SilenceUsage is an option to silence usage when an error occurs.
+	SilenceUsage: true,
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		symbols, err := client.MarketDataService.ListSymbols(args...)
+		if err != nil {
+			return err
+		}
+
+		logrus.Infof("symbols: %+v", symbols)
+		return nil
+	},
+}
+
