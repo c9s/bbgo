44606738473db2f580c1035948d690ecbaf09c8d
c9s <yoan@maicoin.com>
2021-12-11 00:29:02 +0800

    kucoin: split cmd to files

---
+++ examples/kucoin/subaccounts.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var subAccountsCmd = &cobra.Command{
+	Use:   "subaccounts",
+	Short: "subaccounts",
+
+	// SilenceUsage is an option to silence usage when an error occurs.
+	SilenceUsage: true,
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		subAccounts, err := client.AccountService.QuerySubAccounts()
+		if err != nil {
+			return err
+		}
+
+		logrus.Infof("subAccounts: %+v", subAccounts)
+		return nil
+	},
+}
