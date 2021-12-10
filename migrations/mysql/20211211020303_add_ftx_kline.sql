5fb50fa2cb9e4054d65b914355907926d5180223
TonyQ <tonyq@momentx.co>
2021-12-11 02:11:01 +0800

    exchange: backtest: add ftx kline table

---
+++ migrations/mysql/20211211020303_add_ftx_kline.sql
@@ -0,0 +1,33 @@
+-- +up
+-- +begin
+create table if not exists ftx_klines
+(
+    gid bigint unsigned auto_increment
+    primary key,
+    exchange varchar(10) not null,
+    start_time datetime(3) not null,
+    end_time datetime(3) not null,
+    `interval` varchar(3) not null,
+    symbol varchar(12) not null,
+    open decimal(16,8) unsigned not null,
+    high decimal(16,8) unsigned not null,
+    low decimal(16,8) unsigned not null,
+    close decimal(16,8) unsigned default 0.00000000 not null,
+    volume decimal(20,8) unsigned default 0.00000000 not null,
+    closed tinyint(1) default 1 not null,
+    last_trade_id int unsigned default '0' not null,
+    num_trades int unsigned default '0' not null,
+    quote_volume decimal(32,4) default 0.0000 not null,
+    taker_buy_base_volume decimal(32,8) not null,
+    taker_buy_quote_volume decimal(32,4) default 0.0000 not null
+    );
+-- +end
+-- +begin
+create index klines_end_time_symbol_interval
+    on ftx_klines (end_time, symbol, `interval`);
+-- +end
+-- +down
+
+-- +begin
+drop table ftx_klines;
+-- +end
