194d23b7d9c2576ceed4753c2596cb2975e9b348
なるみ <narumi@maicoin.com>
2021-12-12 17:19:27 +0800

    Initial commit of rebalance strategy

--- pkg/cmd/builtin.go
+++ pkg/cmd/builtin.go
@@ -12,6 +12,7 @@ import (
 	_ "github.com/c9s/bbgo/pkg/strategy/kline"
 	_ "github.com/c9s/bbgo/pkg/strategy/pricealert"
 	_ "github.com/c9s/bbgo/pkg/strategy/pricedrop"
+	_ "github.com/c9s/bbgo/pkg/strategy/rebalance"
 	_ "github.com/c9s/bbgo/pkg/strategy/schedule"
 	_ "github.com/c9s/bbgo/pkg/strategy/support"
 	_ "github.com/c9s/bbgo/pkg/strategy/swing"
