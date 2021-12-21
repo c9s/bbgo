fce71cb37e417689af15f716d59ff53810c8d452
c9s <yoan@maicoin.com>
2021-12-22 00:30:16 +0800

    implement QueryAccounts and QueryMarkets

--- pkg/exchange/kucoin/convert.go
+++ pkg/exchange/kucoin/convert.go
@@ -1,1 +1,25 @@
 package kucoin
+
+import (
+	"github.com/c9s/bbgo/pkg/exchange/kucoin/kucoinapi"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+func toGlobalBalanceMap(accounts []kucoinapi.Account) types.BalanceMap {
+	balances := types.BalanceMap{}
+
+	// for now, we only return the trading account
+	for _, account := range accounts {
+		switch account.Type {
+		case kucoinapi.AccountTypeTrade:
+			balances[account.Currency] = types.Balance{
+				Currency:  account.Currency,
+				Available: account.Available,
+				Locked:    account.Holds,
+			}
+		}
+	}
+
+	return balances
+}
+
