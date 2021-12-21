62fa6dd27400c8499ffa34cc85b906e360d843db
c9s <yoan@maicoin.com>
2021-12-21 23:25:19 +0800

    implement get fills request

--- pkg/exchange/kucoin/kucoinapi/trade.go
+++ pkg/exchange/kucoin/kucoinapi/trade.go
@@ -41,6 +41,92 @@ func (c *TradeService) NewCancelAllOrderRequest() *CancelAllOrderRequest {
 	}
 }
 
+func (c *TradeService) NewGetFillsRequest() *GetFillsRequest {
+	return &GetFillsRequest{client: c.client}
+}
+
+//go:generate requestgen -type GetFillsRequest
+type GetFillsRequest struct {
+	client *RestClient
+
+	orderID *string `param:"orderId"`
+
+	symbol *string `param:"symbol"`
+
+	side *string `param:"side" validValues:"buy,sell"`
+
+	orderType *string `param:"type" validValues:"limit,market,limit_stop,market_stop"`
+
+	startAt *time.Time `param:"startAt,milliseconds"`
+
+	endAt *time.Time `param:"endAt,milliseconds"`
+}
+
+type FillListPage struct {
+	CurrentPage int    `json:"currentPage"`
+	PageSize    int    `json:"pageSize"`
+	TotalNumber int    `json:"totalNum"`
+	TotalPage   int    `json:"totalPage"`
+	Items       []Fill `json:"items"`
+}
+
+type Fill struct {
+	Symbol         string                     `json:"symbol"`
+	TradeId        string                     `json:"tradeId"`
+	OrderId        string                     `json:"orderId"`
+	CounterOrderId string                     `json:"counterOrderId"`
+	Side           SideType                   `json:"side"`
+	Liquidity      LiquidityType              `json:"liquidity"`
+	ForceTaker     bool                       `json:"forceTaker"`
+	Price          fixedpoint.Value           `json:"price"`
+	Size           fixedpoint.Value           `json:"size"`
+	Funds          fixedpoint.Value           `json:"funds"`
+	Fee            fixedpoint.Value           `json:"fee"`
+	FeeRate        fixedpoint.Value           `json:"feeRate"`
+	FeeCurrency    string                     `json:"feeCurrency"`
+	Stop           string                     `json:"stop"`
+	Type           OrderType                  `json:"type"`
+	CreatedAt      types.MillisecondTimestamp `json:"createdAt"`
+	TradeType      TradeType                  `json:"tradeType"`
+}
+
+func (r *GetFillsRequest) Do(ctx context.Context) (*FillListPage, error) {
+	params, err := r.GetParametersQuery()
+	if err != nil {
+		return nil, err
+	}
+
+	if !params.Has("tradeType") {
+		params.Add("tradeType", "TRADE")
+	}
+
+	req, err := r.client.newAuthenticatedRequest("GET", "/api/v1/fills", params, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := r.client.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var orderResponse struct {
+		Code    string        `json:"code"`
+		Message string        `json:"msg"`
+		Data    *FillListPage `json:"data"`
+	}
+
+	if err := response.DecodeJSON(&orderResponse); err != nil {
+		return nil, err
+	}
+
+	if orderResponse.Data == nil {
+		return nil, errors.New("api error: [" + orderResponse.Code + "] " + orderResponse.Message)
+	}
+
+	return orderResponse.Data, nil
+}
+
 //go:generate requestgen -type ListOrdersRequest
 type ListOrdersRequest struct {
 	client *RestClient
---
+++ pkg/exchange/kucoin/kucoinapi/types.go
@@ -0,0 +1,55 @@
+package kucoinapi
+
+type TradeType string
+
+const (
+	TradeTypeSpot   TradeType = "TRADE"
+	TradeTypeMargin TradeType = "MARGIN"
+)
+
+type SideType string
+
+const (
+	SideTypeBuy  SideType = "buy"
+	SideTypeSell SideType = "sell"
+)
+
+type TimeInForceType string
+
+const (
+	// GTC Good Till Canceled orders remain open on the book until canceled. This is the default behavior if no policy is specified.
+	TimeInForceGTC TimeInForceType = "GTC"
+
+	// GTT Good Till Time orders remain open on the book until canceled or the allotted cancelAfter is depleted on the matching engine. GTT orders are guaranteed to cancel before any other order is processed after the cancelAfter seconds placed in order book.
+	TimeInForceGTT TimeInForceType = "GTT"
+
+	// FOK Fill Or Kill orders are rejected if the entire size cannot be matched.
+	TimeInForceFOK TimeInForceType = "FOK"
+
+	// IOC Immediate Or Cancel orders instantly cancel the remaining size of the limit order instead of opening it on the book.
+	TimeInForceIOC TimeInForceType = "IOC"
+)
+
+type LiquidityType string
+
+const (
+	LiquidityTypeMaker LiquidityType = "maker"
+	LiquidityTypeTaker LiquidityType = "taker"
+)
+
+type OrderType string
+
+const (
+	OrderTypeMarket    OrderType = "market"
+	OrderTypeLimit     OrderType = "limit"
+	OrderTypeStopLimit OrderType = "stop_limit"
+)
+
+type OrderState string
+
+const (
+	OrderStateCanceled        OrderState = "canceled"
+	OrderStateLive            OrderState = "live"
+	OrderStatePartiallyFilled OrderState = "partially_filled"
+	OrderStateFilled          OrderState = "filled"
+)
