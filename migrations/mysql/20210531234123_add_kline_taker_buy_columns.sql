f72221e804c433040f458b1f43a0a4439a62dffa
c9s <yoan@maicoin.com>
2021-12-26 02:40:00 +0800

    fix klines table migration

--- migrations/mysql/20210531234123_add_kline_taker_buy_columns.sql
+++ migrations/mysql/20210531234123_add_kline_taker_buy_columns.sql
@@ -17,6 +17,12 @@ ALTER TABLE `okex_klines`
     ADD COLUMN `taker_buy_base_volume`  DECIMAL(32, 8) NOT NULL DEFAULT 0.0,
     ADD COLUMN `taker_buy_quote_volume` DECIMAL(32, 8) NOT NULL DEFAULT 0.0;
 -- +end
+-- +begin
+ALTER TABLE `klines`
+    ADD COLUMN `quote_volume`           DECIMAL(32, 8) NOT NULL DEFAULT 0.0,
+    ADD COLUMN `taker_buy_base_volume`  DECIMAL(32, 8) NOT NULL DEFAULT 0.0,
+    ADD COLUMN `taker_buy_quote_volume` DECIMAL(32, 8) NOT NULL DEFAULT 0.0;
+-- +end
 
 -- +down
 -- +begin
