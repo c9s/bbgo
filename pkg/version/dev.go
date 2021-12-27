ad52b64e18470053976ce371d70d7c72e603bc0a
c9s <yoan@maicoin.com>
2021-12-27 23:10:37 +0800

    update dev build version

--- pkg/version/dev.go
+++ pkg/version/dev.go
@@ -2,7 +2,7 @@
 
 package version
 
-const Version = "v1.21.0-4ea4bfb3-dev"
+const Version = "v1.21.0-1-g9b1783a9-9b1783a9-dev"
 
-const VersionGitRef = "4ea4bfb3"
+const VersionGitRef = "9b1783a9"
 
