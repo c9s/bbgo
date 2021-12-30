fb9dfa6e6338a583f586a6992b6d01944b1fb08c
c9s <yoan@maicoin.com>
2021-12-30 15:59:19 +0800

    bump version to v1.21.2

--- pkg/version/dev.go
+++ pkg/version/dev.go
@@ -2,7 +2,7 @@
 
 package version
 
-const Version = "v1.21.1-cfe11c93-dev"
+const Version = "v1.21.2-37a70558-dev"
 
-const VersionGitRef = "cfe11c93"
+const VersionGitRef = "37a70558"
 
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@
 
 package version
 
-const Version = "v1.21.1-cfe11c93"
+const Version = "v1.21.2-37a70558"
 
-const VersionGitRef = "cfe11c93"
+const VersionGitRef = "37a70558"
 
