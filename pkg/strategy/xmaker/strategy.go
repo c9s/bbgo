1d188932fc8ca11814b721245dc0af956ba56b52
c9s <yoan@maicoin.com>
2021-12-31 14:23:02 +0800

    Revert "xmaker: fix s.state.CoveredPosition.AtomicAdd add"
    This reverts commit 49ac10e8b96a81d2aa47a349a2c18ca4e16e97a1.

--- pkg/strategy/xmaker/strategy.go
+++ pkg/strategy/xmaker/strategy.go
@@ -748,7 +748,10 @@ func (s *Strategy) CrossRun(ctx context.Context, orderExecutionRouter bbgo.Order
 	s.tradeCollector.OnTrade(func(trade types.Trade) {
 		c := trade.PositionChange()
 		s.state.HedgePosition.AtomicAdd(c)
-		s.state.CoveredPosition.AtomicAdd(c)
+		if trade.Exchange == s.sourceSession.ExchangeName {
+			s.state.CoveredPosition.AtomicAdd(c)
+		}
+
 		s.state.ProfitStats.AddTrade(trade)
 
 		if err := s.SaveState(); err != nil {
