4cdb5b607bac6384ee34145b020c5f192fc45f8a
c9s <yoan@maicoin.com>
2022-01-09 01:20:47 +0800

    rename bollpp to bollmaker

--- pkg/cmd/builtin.go
+++ pkg/cmd/builtin.go
@@ -3,7 +3,7 @@ package cmd
 // import built-in strategies
 import (
 	_ "github.com/c9s/bbgo/pkg/strategy/bollgrid"
-	_ "github.com/c9s/bbgo/pkg/strategy/bollpp"
+	_ "github.com/c9s/bbgo/pkg/strategy/bollmaker"
 	_ "github.com/c9s/bbgo/pkg/strategy/emastop"
 	_ "github.com/c9s/bbgo/pkg/strategy/etf"
 	_ "github.com/c9s/bbgo/pkg/strategy/flashcrash"
