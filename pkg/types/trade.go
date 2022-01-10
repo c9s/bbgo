fb3c19844773c24243bd2399f4eaca340562f3db
c9s <yoan@maicoin.com>
2022-01-10 14:18:09 +0800

    types: add okex icon and kucoin icon

--- pkg/types/trade.go
+++ pkg/types/trade.go
@@ -164,6 +164,10 @@ func (trade Trade) SlackAttachment() slack.Attachment {
 		footerIcon = "https://max.maicoin.com/favicon-16x16.png"
 	case ExchangeFTX:
 		footerIcon = "https://ftx.com/favicon.ico?v=2"
+	case ExchangeOKEx:
+		footerIcon = "https://static.okex.com/cdn/assets/imgs/MjAxODg/D91A7323087D31A588E0D2A379DD7747.png"
+	case ExchangeKucoin:
+		footerIcon = "https://assets.staticimg.com/cms/media/7AV75b9jzr9S8H3eNuOuoqj8PwdUjaDQGKGczGqTS.png"
 	}
 
 	return slack.Attachment{
