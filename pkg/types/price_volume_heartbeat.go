8b56c47f65d087e21062c108654e2dd0df1e8c49
c9s <yoan@maicoin.com>
2022-01-13 00:01:20 +0800

    add doc-comment for PriceHeartBeat

--- pkg/types/price_volume_heartbeat.go
+++ pkg/types/price_volume_heartbeat.go
@@ -5,11 +5,16 @@ import (
 	"time"
 )
 
+// PriceHeartBeat is used for monitoring the price volume update.
 type PriceHeartBeat struct {
 	PriceVolume PriceVolume
 	LastTime    time.Time
 }
 
+// Update updates the price volume object and the last update time
+// It returns (bool, error), when the price is successfully updated, it returns true.
+// If the price is not updated (same price) and the last time exceeded the timeout,
+// Then false, and an error will be returned
 func (b *PriceHeartBeat) Update(pv PriceVolume, timeout time.Duration) (bool, error) {
 	if b.PriceVolume.Price == 0 || b.PriceVolume != pv {
 		b.PriceVolume = pv
