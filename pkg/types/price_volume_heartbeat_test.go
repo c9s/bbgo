c3356fa694f1922d8218690743d6d49c8bda0149
c9s <yoan@maicoin.com>
2022-01-12 14:42:11 +0800

    types: add test for PriceHeartBeat

---
+++ pkg/types/price_volume_heartbeat_test.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+)
+
+func TestPriceHeartBeat_Update(t *testing.T) {
+	hb := PriceHeartBeat{}
+	updated, err := hb.Update(PriceVolume{Price: fixedpoint.NewFromFloat(22.0), Volume: fixedpoint.NewFromFloat(100.0)}, time.Minute)
+	assert.NoError(t, err)
+	assert.True(t, updated)
+
+	updated, err = hb.Update(PriceVolume{Price: fixedpoint.NewFromFloat(22.0), Volume: fixedpoint.NewFromFloat(100.0)}, time.Minute)
+	assert.NoError(t, err)
+	assert.False(t, updated, "should not be updated when pv is not changed")
+
+	updated, err = hb.Update(PriceVolume{Price: fixedpoint.NewFromFloat(23.0), Volume: fixedpoint.NewFromFloat(100.0)}, time.Minute)
+	assert.NoError(t, err)
+	assert.True(t, updated, "should be updated when the price is changed")
+
+	updated, err = hb.Update(PriceVolume{Price: fixedpoint.NewFromFloat(23.0), Volume: fixedpoint.NewFromFloat(200.0)}, time.Minute)
+	assert.NoError(t, err)
+	assert.True(t, updated, "should be updated when the volume is changed")
+}
