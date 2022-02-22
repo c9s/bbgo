556a03be95719c9a5feee701d37b90508f257658
c9s <yoan@maicoin.com>
2022-02-22 14:28:54 +0800

    ftxapi: add get coins api

--- pkg/exchange/ftx/ftxapi/client.go
+++ pkg/exchange/ftx/ftxapi/client.go
@@ -304,3 +304,34 @@ func (c *RestClient) NewGetMarketRequest(market string) *GetMarketRequest {
 		market: market,
 	}
 }
+
+type Coin struct {
+	Bep2Asset        *string  `json:"bep2Asset"`
+	CanConvert       bool     `json:"canConvert"`
+	CanDeposit       bool     `json:"canDeposit"`
+	CanWithdraw      bool     `json:"canWithdraw"`
+	Collateral       bool     `json:"collateral"`
+	CollateralWeight fixedpoint.Value  `json:"collateralWeight"`
+	CreditTo         *string  `json:"creditTo"`
+	Erc20Contract    string   `json:"erc20Contract"`
+	Fiat             bool     `json:"fiat"`
+	HasTag           bool     `json:"hasTag"`
+	Id               string   `json:"id"`
+	IsToken          bool     `json:"isToken"`
+	Methods          []string `json:"methods"`
+	Name             string   `json:"name"`
+	SplMint          string   `json:"splMint"`
+	Trc20Contract    string   `json:"trc20Contract"`
+	UsdFungible      bool     `json:"usdFungible"`
+}
+
+//go:generate GetRequest -url "/api/coins" -type GetCoinsRequest -responseDataType []Coin
+type GetCoinsRequest struct {
+	client requestgen.AuthenticatedAPIClient
+}
+
+func (c *RestClient) NewGetCoinsRequest() *GetCoinsRequest {
+	return &GetCoinsRequest{
+		client: c,
+	}
+}
