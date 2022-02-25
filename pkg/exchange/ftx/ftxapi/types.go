14a49989fe380d0130e39c31d9517a1b4e182fd9
c9s <yoan@maicoin.com>
2022-02-25 16:40:11 +0800

    ftxapi: define types

---
+++ pkg/exchange/ftx/ftxapi/types.go
@@ -0,0 +1,30 @@
+package ftxapi
+
+
+type Side string
+
+const (
+	SideBuy Side = "buy"
+	SideSell Side = "sell"
+)
+
+
+type OrderType string
+
+const (
+	OrderTypeLimit OrderType = "limit"
+	OrderTypeMarket OrderType = "market"
+
+	// trigger order types
+	OrderTypeStopLimit OrderType = "stop"
+	OrderTypeTrailingStop OrderType = "trailingStop"
+	OrderTypeTakeProfit OrderType = "takeProfit"
+)
+
+type OrderStatus string
+
+const (
+	OrderStatusNew OrderStatus = "new"
+	OrderStatusOpen OrderStatus = "open"
+	OrderStatusClosed OrderStatus = "closed"
+)
