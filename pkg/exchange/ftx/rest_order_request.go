2510f14d5332619152fa90fe30c2702c73f72f97
c9s <yoan@maicoin.com>
2022-03-03 11:42:40 +0800

    ftx: remove legacy place order request method

--- pkg/exchange/ftx/rest_order_request.go
+++ pkg/exchange/ftx/rest_order_request.go
@@ -1,10 +1,6 @@
 package ftx
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 )
 
@@ -37,30 +33,3 @@ type PlaceOrderPayload struct {
 	ClientID   string
 }
 
-func (r *orderRequest) PlaceOrder(ctx context.Context, p PlaceOrderPayload) (orderResponse, error) {
-	resp, err := r.
-		Method("POST").
-		ReferenceURL("api/orders").
-		Payloads(map[string]interface{}{
-			"market":     p.Market,
-			"side":       p.Side,
-			"price":      p.Price,
-			"type":       p.Type,
-			"size":       p.Size,
-			"reduceOnly": p.ReduceOnly,
-			"ioc":        p.IOC,
-			"postOnly":   p.PostOnly,
-			"clientId":   p.ClientID,
-		}).
-		DoAuthenticatedRequest(ctx)
-
-	if err != nil {
-		return orderResponse{}, err
-	}
-	var o orderResponse
-	if err := json.Unmarshal(resp.Body, &o); err != nil {
-		return orderResponse{}, fmt.Errorf("failed to unmarshal order response body to json: %w", err)
-	}
-
-	return o, nil
-}
