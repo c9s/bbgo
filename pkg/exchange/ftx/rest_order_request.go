270ae51c9b26076fb794a64b777e8e3ced04c2f0
c9s <yoan@maicoin.com>
2022-03-03 11:42:57 +0800

    ftx: remove legacy PlaceOrderPayload

--- pkg/exchange/ftx/rest_order_request.go
+++ pkg/exchange/ftx/rest_order_request.go
@@ -1,35 +1,6 @@
 package ftx
 
-import (
-	"github.com/c9s/bbgo/pkg/fixedpoint"
-)
-
 type orderRequest struct {
 	*restRequest
-}
-
-/*
-{
-  "market": "XRP-PERP",
-  "side": "sell",
-  "price": 0.306525,
-  "type": "limit",
-  "size": 31431.0,
-  "reduceOnly": false,
-  "ioc": false,
-  "postOnly": false,
-  "clientId": null
-}
-*/
-type PlaceOrderPayload struct {
-	Market     string
-	Side       string
-	Price      fixedpoint.Value
-	Type       string
-	Size       fixedpoint.Value
-	ReduceOnly bool
-	IOC        bool
-	PostOnly   bool
-	ClientID   string
 }
 
