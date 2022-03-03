32f08c84ca4d051a600a37237f02b0609a78dd11
c9s <yoan@maicoin.com>
2022-03-03 11:39:11 +0800

    ftx: remove legacy market api method

--- pkg/exchange/ftx/rest_market_request.go
+++ pkg/exchange/ftx/rest_market_request.go
@@ -14,24 +14,6 @@ type marketRequest struct {
 	*restRequest
 }
 
-func (r *marketRequest) Markets(ctx context.Context) (marketsResponse, error) {
-	resp, err := r.
-		Method("GET").
-		ReferenceURL("api/markets").
-		DoAuthenticatedRequest(ctx)
-
-	if err != nil {
-		return marketsResponse{}, err
-	}
-
-	var m marketsResponse
-	if err := json.Unmarshal(resp.Body, &m); err != nil {
-		return marketsResponse{}, fmt.Errorf("failed to unmarshal market response body to json: %w", err)
-	}
-
-	return m, nil
-}
-
 /*
 supported resolutions: window length in seconds. options: 15, 60, 300, 900, 3600, 14400, 86400
 doc: https://docs.ftx.com/?javascript#get-historical-prices
