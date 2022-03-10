7352d70c7f25eafa7237cd00e8ed56a700cf48fe
c9s <yoan@maicoin.com>
2022-03-10 19:00:02 +0800

    add position table and service

--- migrations/sqlite3/20220307132917_add_positions.sql
+++ migrations/sqlite3/20220307132917_add_positions.sql
@@ -3,20 +3,21 @@ CREATE TABLE `positions`
 (
     `gid`                  INTEGER PRIMARY KEY AUTOINCREMENT,
 
-    `strategy`             VARCHAR(32)     NOT NULL,
-    `strategy_instance_id` VARCHAR(64)     NOT NULL,
+    `strategy`             VARCHAR(32)    NOT NULL,
+    `strategy_instance_id` VARCHAR(64)    NOT NULL,
 
-    `symbol`               VARCHAR(20)     NOT NULL,
-    `quote_currency`       VARCHAR(10)     NOT NULL,
-    `base_currency`        VARCHAR(10)     NOT NULL,
+    `symbol`               VARCHAR(20)    NOT NULL,
+    `quote_currency`       VARCHAR(10)    NOT NULL,
+    `base_currency`        VARCHAR(10)    NOT NULL,
 
     -- average_cost is the position average cost
-    `average_cost`         DECIMAL(16, 8)  NOT NULL,
-    `base`                 DECIMAL(16, 8)  NOT NULL,
-    `quote`                DECIMAL(16, 8)  NOT NULL,
+    `average_cost`         DECIMAL(16, 8) NOT NULL,
+    `base`                 DECIMAL(16, 8) NOT NULL,
+    `quote`                DECIMAL(16, 8) NOT NULL,
+    `profit`               DECIMAL(16, 8) NULL,
 
-    `trade_id`             BIGINT NOT NULL,
-    `traded_at`            DATETIME(3)     NOT NULL
+    `trade_id`             BIGINT         NOT NULL,
+    `traded_at`            DATETIME(3)    NOT NULL
 );
 
 -- +down
