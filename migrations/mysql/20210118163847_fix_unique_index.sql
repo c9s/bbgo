402bb145b8f9fbded7134cf3c354f00e8a2c5b06
c9s <yoan@maicoin.com>
2022-03-11 13:32:40 +0800

    migrations: merge orders,trades migration

--- migrations/mysql/20210118163847_fix_unique_index.sql
+++ migrations/mysql/20210118163847_fix_unique_index.sql
@@ -1,15 +1,9 @@
 -- +up
 -- +begin
-ALTER TABLE `trades` DROP INDEX `id`;
--- +end
--- +begin
-ALTER TABLE `trades` ADD UNIQUE INDEX `id` (`exchange`,`symbol`, `side`, `id`);
+SELECT 1;
 -- +end
 
 -- +down
 -- +begin
-ALTER TABLE `trades` DROP INDEX `id`;
--- +end
--- +begin
-ALTER TABLE `trades` ADD UNIQUE INDEX `id` (`id`);
+SELECT 1;
 -- +end
