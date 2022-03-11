32a5cd9f855081893f8fe122d6e98c17490a6760
c9s <yoan@maicoin.com>
2022-03-11 13:32:40 +0800

    migrations: merge orders,trades migration

--- migrations/mysql/20201102222546_orders.sql
+++ migrations/mysql/20201102222546_orders.sql
@@ -6,9 +6,9 @@ CREATE TABLE `orders`
     `exchange`          VARCHAR(24)             NOT NULL DEFAULT '',
     -- order_id is the order id returned from the exchange
     `order_id`          BIGINT UNSIGNED         NOT NULL,
-    `client_order_id`   VARCHAR(122)             NOT NULL DEFAULT '',
+    `client_order_id`   VARCHAR(122)            NOT NULL DEFAULT '',
     `order_type`        VARCHAR(16)             NOT NULL,
-    `symbol`            VARCHAR(8)              NOT NULL,
+    `symbol`            VARCHAR(20)             NOT NULL,
     `status`            VARCHAR(12)             NOT NULL,
     `time_in_force`     VARCHAR(4)              NOT NULL,
     `price`             DECIMAL(16, 8) UNSIGNED NOT NULL,
@@ -19,8 +19,16 @@ CREATE TABLE `orders`
     `is_working`        BOOL                    NOT NULL DEFAULT FALSE,
     `created_at`        DATETIME(3)             NOT NULL,
     `updated_at`        DATETIME(3)             NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-    PRIMARY KEY (`gid`)
 
+    `is_margin`         BOOLEAN                 NOT NULL DEFAULT FALSE,
+    `is_isolated`       BOOLEAN                 NOT NULL DEFAULT FALSE,
+
+    PRIMARY KEY (`gid`)
 );
+CREATE INDEX orders_symbol ON orders (exchange, symbol);
+CREATE UNIQUE INDEX orders_order_id ON orders (order_id, exchange);
+
 -- +down
+DROP INDEX orders_symbol ON orders;
+DROP INDEX orders_order_id ON orders;
 DROP TABLE `orders`;
--- migrations/mysql/20201105092857_trades_index_fix.sql
+++ migrations/mysql/20201105092857_trades_index_fix.sql
@@ -1,19 +1,8 @@
 -- +up
-DROP INDEX trades_symbol ON trades;
-DROP INDEX trades_symbol_fee_currency ON trades;
-DROP INDEX trades_traded_at_symbol ON trades;
-
-CREATE INDEX trades_symbol ON trades (exchange, symbol);
-CREATE INDEX trades_symbol_fee_currency ON trades (exchange, symbol, fee_currency, traded_at);
-CREATE INDEX trades_traded_at_symbol ON trades (exchange, traded_at, symbol);
+SELECT 1;
 
 -- +down
-DROP INDEX trades_symbol ON trades;
-DROP INDEX trades_symbol_fee_currency ON trades;
-DROP INDEX trades_traded_at_symbol ON trades;
+SELECT 1;
 
-CREATE INDEX trades_symbol ON trades (symbol);
-CREATE INDEX trades_symbol_fee_currency ON trades (symbol, fee_currency, traded_at);
-CREATE INDEX trades_traded_at_symbol ON trades (traded_at, symbol);
 
 
