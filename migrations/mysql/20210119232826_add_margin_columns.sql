fa9294458b7868898a218cfb8cf25388fcab836b
c9s <yoan@maicoin.com>
2022-03-11 13:32:40 +0800

    migrations: merge orders,trades migration

--- migrations/mysql/20210119232826_add_margin_columns.sql
+++ migrations/mysql/20210119232826_add_margin_columns.sql
@@ -1,19 +1,4 @@
 -- +up
-ALTER TABLE `trades`
-    ADD COLUMN `is_margin` BOOLEAN NOT NULL DEFAULT FALSE,
-    ADD COLUMN `is_isolated` BOOLEAN NOT NULL DEFAULT FALSE
-    ;
-
-ALTER TABLE `orders`
-    ADD COLUMN `is_margin` BOOLEAN NOT NULL DEFAULT FALSE,
-    ADD COLUMN `is_isolated` BOOLEAN NOT NULL DEFAULT FALSE
-    ;
-
+SELECT 1;
 -- +down
-ALTER TABLE `trades`
-    DROP COLUMN `is_margin`,
-    DROP COLUMN `is_isolated`;
-
-ALTER TABLE `orders`
-    DROP COLUMN `is_margin`,
-    DROP COLUMN `is_isolated`;
+SELECT 1;
