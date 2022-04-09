8e601a5cdef3a654fd93fa1047e9184a2591e818
なるみ <narumi@maicoin.com>
2022-04-09 17:30:13 +0800

    Add user data service

--- python/tests/servicer.py
+++ python/tests/servicer.py
@@ -1,36 +1,8 @@
-import random
-import time
-
 from bbgo import bbgo_pb2
 from bbgo import bbgo_pb2_grpc
 
 
 class TestTradingServicer(bbgo_pb2_grpc.TradingServiceServicer):
-
-    def Subcribe(self, request, context):
-        i = 0
-        while True:
-            for subscription in request.subscriptions:
-                yield bbgo_pb2.SubscribeResponse(
-                    channel=subscription.channel,
-                    event=bbgo_pb2.Event.UPDATE,
-                    exchange=subscription.exchange,
-                    symbol=subscription.symbol + f'_{i}',
-                )
-            i += 1
-            time.sleep(random.random())
-
-    def SubcribeUserData(self, request, context):
-        i = 0
-        while True:
-            yield bbgo_pb2.SubscribeResponse(
-                channel=bbgo_pb2.Channel.USER,
-                event=bbgo_pb2.Event.ORDER_UPDATE,
-                exchange='max',
-                symbol=f'user_{i}',
-            )
-            i += 1
-            time.sleep(random.random())
 
     def SubmitOrder(self, request, context):
         submit_order = request.submit_order
@@ -50,18 +22,3 @@ class TestTradingServicer(bbgo_pb2_grpc.TradingServiceServicer):
         error = bbgo_pb2.Error(error_code=0, error_message='')
 
         return bbgo_pb2.SubmitOrderResponse(order=order, error=error)
-
-    def CancelOrder(self, request, context):
-        pass
-
-    def QueryOrder(self, request, context):
-        pass
-
-    def QueryOrders(self, request, context):
-        pass
-
-    def QueryTrades(self, request, context):
-        pass
-
-    def QueryKLines(self, request, context):
-        pass
