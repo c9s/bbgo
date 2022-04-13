2844e8ad1550af51a4f00b7574fbbca3af58626b
なるみ <narumi@maicoin.com>
2022-04-13 14:52:34 +0800

    python: fix examples

--- python/bbgo/data/__init__.py
+++ python/bbgo/data/__init__.py
@@ -1,3 +1,4 @@
 from .balance import Balance
+from .error import ErrorMessage
 from .kline import KLine
 from .order import Order
--- python/bbgo/data/kline.py
+++ python/bbgo/data/kline.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from datetime import datetime
+
+import bbgo_pb2
 
 
 @dataclass
@@ -16,3 +20,19 @@ class KLine:
     end_time: datetime = None
     quote_volume: float = None
     closed: bool = None
+
+    @classmethod
+    def from_pb(cls, obj: bbgo_pb2.KLine) -> KLine:
+        return cls(
+            exchange=obj.exchange,
+            symbol=obj.symbol,
+            open=float(obj.open),
+            high=float(obj.high),
+            low=float(obj.low),
+            close=float(obj.close),
+            volume=float(obj.volume),
+            quote_volume=float(obj.quote_volume),
+            start_time=datetime.fromtimestamp(obj.start_time / 1000),
+            end_time=datetime.fromtimestamp(obj.end_time / 1000),
+            closed=obj.closed,
+        )
