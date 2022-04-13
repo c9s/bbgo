b7ad2f68306675287b04bc15002d880ae6684a9f
なるみ <narumi@maicoin.com>
2022-04-13 18:11:21 +0800

    python: simplify event handler

--- python/stream.py
+++ python/stream.py
@@ -1,25 +1,25 @@
 import click
 from loguru import logger
 
-import bbgo_pb2
 from bbgo import Stream
+from bbgo.data import Event
+from bbgo.handlers import UpdateHandler
+
+
+class LogBook(UpdateHandler):
+
+    def handle(self, event: Event) -> None:
+        logger.info(event)
 
 
 @click.command()
 @click.option('--host', default='127.0.0.1')
 @click.option('--port', default=50051)
 def main(host, port):
-    subscriptions = [
-        bbgo_pb2.Subscription(exchange='max', channel=bbgo_pb2.Channel.BOOK, symbol='BTCUSDT', depth='full'),
-        bbgo_pb2.Subscription(exchange='binance', channel=bbgo_pb2.Channel.BOOK, symbol='BTCUSDT', depth='full'),
-    ]
-
-    def book_event_callback(event):
-        logger.info(event)
-
-    stream = Stream(host, port, subscriptions)
-    stream.on_book_event(book_event_callback)
-
+    stream = Stream(host, port)
+    stream.subscribe('max', 'book', 'BTCUSDT', 'full')
+    stream.subscribe('max', 'book', 'ETHUSDT', 'full')
+    stream.add_event_handler(LogBook())
     stream.start()
 
 
