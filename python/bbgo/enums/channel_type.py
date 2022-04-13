0e3daab3d78a38006cc2676e0218586aa2bceb23
なるみ <narumi@maicoin.com>
2022-04-13 17:40:21 +0800

    python: fix market service

--- python/bbgo/enums/channel_type.py
+++ python/bbgo/enums/channel_type.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
 from enum import Enum
+
+import bbgo_pb2
 
 
 class ChannelType(Enum):
@@ -7,3 +11,22 @@ class ChannelType(Enum):
     TICKER = 'ticker'
     USER = 'user'
     KLINE = 'kline'
+
+    @classmethod
+    def from_pb(cls, obj) -> ChannelType:
+        return {
+            bbgo_pb2.Channel.BOOK: cls.BOOK,
+            bbgo_pb2.Channel.TRADE: cls.TRADE,
+            bbgo_pb2.Channel.TICKER: cls.TICKER,
+            bbgo_pb2.Channel.USER: cls.USER,
+            bbgo_pb2.Channel.KLINE: cls.KLINE,
+        }[obj]
+
+    def to_pb(self) -> bbgo_pb2.Channel:
+        return {
+            'book': bbgo_pb2.Channel.BOOK,
+            'trade': bbgo_pb2.Channel.TRADE,
+            'ticker': bbgo_pb2.Channel.TICKER,
+            'user': bbgo_pb2.Channel.USER,
+            'kline': bbgo_pb2.Channel.KLINE,
+        }[self.value]
--- python/bbgo/enums/event_type.py
+++ python/bbgo/enums/event_type.py
@@ -1,7 +1,27 @@
+from __future__ import annotations
+
 from enum import Enum
+
+# enum Event {
+#   UNKNOWN = 0;
+#   SUBSCRIBED = 1;
+#   UNSUBSCRIBED = 2;
+#   SNAPSHOT = 3;
+#   UPDATE = 4;
+#   AUTHENTICATED = 5;
+#   ORDER_SNAPSHOT = 6;
+#   ORDER_UPDATE = 7;
+#   TRADE_SNAPSHOT = 8;
+#   TRADE_UPDATE = 9;
+#   ACCOUNT_SNAPSHOT = 10;
+#   ACCOUNT_UPDATE = 11;
+#   ERROR = 99;
+# }
+import bbgo_pb2
 
 
 class EventType(Enum):
+    UNKNOWN = 'unknown'
     ERROR = 'error'
     SUBSCRIBED = 'subscribed'
     UNSUBSCRIBED = 'unsubscribed'
@@ -14,3 +34,22 @@ class EventType(Enum):
     TRADE_UPDATE = 'trade_update'
     ACCOUNT_SNAPSHOT = 'account_snapshot'
     ACCOUNT_UPDATE = 'account_update'
+
+    @classmethod
+    def from_pb(cls, obj: bbgo_pb2.Event) -> EventType:
+        d = {
+            bbgo_pb2.Event.UNKNOWN: cls.UNKNOWN,
+            bbgo_pb2.Event.ERROR: cls.ERROR,
+            bbgo_pb2.Event.SUBSCRIBED: cls.SUBSCRIBED,
+            bbgo_pb2.Event.UNSUBSCRIBED: cls.UNSUBSCRIBED,
+            bbgo_pb2.Event.AUTHENTICATED: cls.AUTHENTICATED,
+            bbgo_pb2.Event.SNAPSHOT: cls.SNAPSHOT,
+            bbgo_pb2.Event.UPDATE: cls.UPDATE,
+            bbgo_pb2.Event.ORDER_SNAPSHOT: cls.ORDER_SNAPSHOT,
+            bbgo_pb2.Event.ORDER_UPDATE: cls.ORDER_UPDATE,
+            bbgo_pb2.Event.TRADE_SNAPSHOT: cls.TRADE_SNAPSHOT,
+            bbgo_pb2.Event.TRADE_UPDATE: cls.TRADE_UPDATE,
+            bbgo_pb2.Event.ACCOUNT_SNAPSHOT: cls.ACCOUNT_SNAPSHOT,
+            bbgo_pb2.Event.ACCOUNT_UPDATE: cls.ACCOUNT_UPDATE,
+        }
+        return d[obj]
