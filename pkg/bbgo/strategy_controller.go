73c2c84cab1854d93f5bf9f2952548126e8e3f15
Andy Cheng <andy.cheng@richsoda.com>
2022-04-15 16:06:19 +0800

    feature: prototype of strategy controller struct

--- pkg/bbgo/strategy_controller.go
+++ pkg/bbgo/strategy_controller.go
@@ -1,1 +1,22 @@
 package bbgo
+
+import (
+	"context"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+type StrategyController struct {
+	status types.StrategyStatus
+
+	SuspendCallbacks []func(ctx context.Context)
+}
+
+func (s *StrategyController) OnSuspend(cb func(ctx context.Context)) {
+	s.SuspendCallbacks = append(s.SuspendCallbacks, cb)
+}
+
+func (s *StrategyController) EmitKLineClosed(ctx context.Context) {
+	for _, cb := range s.SuspendCallbacks {
+		cb(ctx)
+	}
+}
