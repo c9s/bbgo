4572699e384e85dc946cf9888b3164d2a1dc4928
c9s <yoanlin93@gmail.com>
2022-04-28 01:12:37 +0800

    binance: add NewGetDepositAddressRequest api

--- pkg/exchange/binance/binanceapi/get_deposit_address_request.go
+++ pkg/exchange/binance/binanceapi/get_deposit_address_request.go
@@ -4,10 +4,6 @@ import (
 	"github.com/c9s/requestgen"
 )
 
-//go:generate -command GetRequest requestgen -method GET
-//go:generate -command PostRequest requestgen -method POST
-//go:generate -command DeleteRequest requestgen -method DELETE
-
 type DepositAddress struct {
 	Address string `json:"address"`
 	Coin    string `json:"coin"`
@@ -15,8 +11,7 @@ type DepositAddress struct {
 	Url     string `json:"url"`
 }
 
-
-//go:generate GetRequest -url "/sapi/v1/capital/deposit/address" -type GetDepositAddressRequest -responseType .DepositAddress
+//go:generate requestgen -method GET -url "/sapi/v1/capital/deposit/address" -type GetDepositAddressRequest -responseType .DepositAddress
 type GetDepositAddressRequest struct {
 	client requestgen.AuthenticatedAPIClient
 
