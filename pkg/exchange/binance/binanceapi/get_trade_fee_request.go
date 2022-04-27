434434c8d9e2623c9e962e94ecaa9a6b261c6ee8
c9s <yoanlin93@gmail.com>
2022-04-28 01:22:04 +0800

    binanceapi: add withdraw request

--- pkg/exchange/binance/binanceapi/get_trade_fee_request.go
+++ pkg/exchange/binance/binanceapi/get_trade_fee_request.go
@@ -6,17 +6,13 @@ import (
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 )
 
-//go:generate -command GetRequest requestgen -method GET
-//go:generate -command PostRequest requestgen -method POST
-//go:generate -command DeleteRequest requestgen -method DELETE
-
 type TradeFee struct {
 	Symbol          string `json:"symbol"`
 	MakerCommission fixedpoint.Value `json:"makerCommission"`
 	TakerCommission fixedpoint.Value  `json:"takerCommission"`
 }
 
-//go:generate GetRequest -url "/sapi/v1/asset/tradeFee" -type GetTradeFeeRequest -responseType []TradeFee
+//go:generate requestgen -method GET -url "/sapi/v1/asset/tradeFee" -type GetTradeFeeRequest -responseType []TradeFee
 type GetTradeFeeRequest struct {
 	client requestgen.AuthenticatedAPIClient
 }
