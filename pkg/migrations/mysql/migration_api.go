ce54e917a208af2991cf29a50c01c20886aa8d00
c9s <yoan@maicoin.com>
2022-05-01 01:16:10 +0800

    compile and update migration package

--- pkg/migrations/mysql/migration_api.go
+++ pkg/migrations/mysql/migration_api.go
@@ -2,7 +2,6 @@ package mysql
 
 import (
 	"fmt"
-	"log"
 	"runtime"
 	"strings"
 
@@ -11,26 +10,6 @@ import (
 
 var registeredGoMigrations map[int64]*rockhopper.Migration
 
-func MergeMigrationsMap(ms map[int64]*rockhopper.Migration) {
-	for k, m := range ms {
-		if _, ok := registeredGoMigrations[k]; !ok {
-			registeredGoMigrations[k] = m
-		} else {
-			log.Printf("the migration key %d is duplicated: %+v", k, m)
-		}
-	}
-}
-
-func GetMigrationsMap() map[int64]*rockhopper.Migration {
-	return registeredGoMigrations
-}
-
-// SortedMigrations builds up the migration objects, sort them by timestamp and return as a slice
-func SortedMigrations() rockhopper.MigrationSlice {
-	return Migrations()
-}
-
-// Migrations builds up the migration objects, sort them by timestamp and return as a slice
 func Migrations() rockhopper.MigrationSlice {
 	var migrations = rockhopper.MigrationSlice{}
 	for _, migration := range registeredGoMigrations {
@@ -40,28 +19,21 @@ func Migrations() rockhopper.MigrationSlice {
 	return migrations.SortAndConnect()
 }
 
-// AddMigration adds a migration with its runtime caller information
+// AddMigration adds a migration.
 func AddMigration(up, down rockhopper.TransactionHandler) {
 	pc, filename, _, _ := runtime.Caller(1)
 
 	funcName := runtime.FuncForPC(pc).Name()
-	packageName := _parseFuncPackageName(funcName)
-	AddNamedMigration(packageName, filename, up, down)
-}
-
-// parseFuncPackageName parses the package name from a given runtime caller function name
-func _parseFuncPackageName(funcName string) string {
 	lastSlash := strings.LastIndexByte(funcName, '/')
 	if lastSlash < 0 {
 		lastSlash = 0
 	}
-
 	lastDot := strings.LastIndexByte(funcName[lastSlash:], '.') + lastSlash
 	packageName := funcName[:lastDot]
-	return packageName
+	AddNamedMigration(packageName, filename, up, down)
 }
 
-// AddNamedMigration adds a named migration to the registered go migration map
+// AddNamedMigration : Add a named migration.
 func AddNamedMigration(packageName, filename string, up, down rockhopper.TransactionHandler) {
 	if registeredGoMigrations == nil {
 		registeredGoMigrations = make(map[int64]*rockhopper.Migration)
