36630004da599d4ceed3562880bf0b290d24494c
c9s <yoanlin93@gmail.com>
2022-05-03 19:26:41 +0800

    add session name column

--- migrations/mysql/20220503144849_add_margin_info_to_nav.sql
+++ migrations/mysql/20220503144849_add_margin_info_to_nav.sql
@@ -1,8 +1,9 @@
 -- +up
 -- +begin
 ALTER TABLE `nav_history_details`
-    ADD COLUMN `net_asset`     DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,
-    ADD COLUMN `borrowed`      DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,
+    ADD COLUMN `session`      VARCHAR(30)                                NOT NULL,
+    ADD COLUMN `net_asset`    DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,
+    ADD COLUMN `borrowed`     DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,
     ADD COLUMN `price_in_usd` DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL
 ;
 -- +end
@@ -12,6 +13,7 @@ ALTER TABLE `nav_history_details`
 
 -- +begin
 ALTER TABLE `nav_history_details`
+    DROP COLUMN `session`,
     DROP COLUMN `net_asset`,
     DROP COLUMN `borrowed`,
     DROP COLUMN `price_in_usd`
