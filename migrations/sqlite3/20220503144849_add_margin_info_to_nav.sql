816f05cd887d5cf3e969444d44053e0b6b8f7ef4
c9s <yoanlin93@gmail.com>
2022-05-03 14:54:16 +0800

    migrations: add margin info columns to nav table

---
+++ migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
@@ -0,0 +1,8 @@
+-- +up
+ALTER TABLE `nav_history_details` ADD COLUMN `borrowed` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `net_asset` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `price_in_usd` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+
+-- +down
+-- we can not rollback alter table change in sqlite
+SELECT 1;
