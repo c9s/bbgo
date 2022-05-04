bb9a78757509c2af16adff38cd60c101f9bd39a7
c9s <yoanlin93@gmail.com>
2022-05-04 18:45:36 +0800

    add interest and fix net asset column

--- migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
+++ migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
@@ -1,7 +1,7 @@
 -- +up
 ALTER TABLE `nav_history_details` ADD COLUMN `session` VARCHAR(50) NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `borrowed` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
-ALTER TABLE `nav_history_details` ADD COLUMN `net_asset` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `net_asset` DECIMAL DEFAULT 0.00000000 NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `price_in_usd` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `is_margin` BOOL DEFAULT FALSE NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `is_isolated` BOOL DEFAULT FALSE NOT NULL;
