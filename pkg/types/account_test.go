63096c2f3ca6ab2e4a02b58a92bc19f22c40fda8
c9s <yoanlin93@gmail.com>
2022-05-04 21:40:16 +0800

    move balance test

--- pkg/types/account_test.go
+++ pkg/types/account_test.go
@@ -8,36 +8,6 @@ import (
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 )
 
-func TestBalanceMap_Add(t *testing.T) {
-	var bm = BalanceMap{}
-	var bm2 = bm.Add(BalanceMap{
-		"BTC": Balance{
-			Currency:  "BTC",
-			Available: fixedpoint.MustNewFromString("10.0"),
-			Locked:    fixedpoint.MustNewFromString("0"),
-			NetAsset:  fixedpoint.MustNewFromString("10.0"),
-		},
-	})
-	assert.Len(t, bm2, 1)
-
-	var bm3 = bm2.Add(BalanceMap{
-		"BTC": Balance{
-			Currency:  "BTC",
-			Available: fixedpoint.MustNewFromString("1.0"),
-			Locked:    fixedpoint.MustNewFromString("0"),
-			NetAsset:  fixedpoint.MustNewFromString("1.0"),
-		},
-		"LTC": Balance{
-			Currency:  "LTC",
-			Available: fixedpoint.MustNewFromString("20.0"),
-			Locked:    fixedpoint.MustNewFromString("0"),
-			NetAsset:  fixedpoint.MustNewFromString("20.0"),
-		},
-	})
-	assert.Len(t, bm3, 2)
-	assert.Equal(t, fixedpoint.MustNewFromString("11.0"), bm3["BTC"].Available)
-}
-
 func TestAccountLockAndUnlock(t *testing.T) {
 	a := NewAccount()
 	a.AddBalance("USDT", fixedpoint.NewFromInt(1000))
