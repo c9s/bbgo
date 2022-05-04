b6d1cb98dd41742e22d2e06e95cf7754dfd66555
c9s <yoanlin93@gmail.com>
2022-05-04 19:05:31 +0800

    change balance_in_usd and balance_in_btc to signed

--- migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
+++ migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
@@ -1,8 +1,8 @@
 -- +up
 ALTER TABLE `nav_history_details` ADD COLUMN `session` VARCHAR(50) NOT NULL;
-ALTER TABLE `nav_history_details` ADD COLUMN `borrowed` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `borrowed` DECIMAL DEFAULT 0.00000000 NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `net_asset` DECIMAL DEFAULT 0.00000000 NOT NULL;
-ALTER TABLE `nav_history_details` ADD COLUMN `price_in_usd` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `price_in_usd` DECIMAL DEFAULT 0.00000000 NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `is_margin` BOOL DEFAULT FALSE NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `is_isolated` BOOL DEFAULT FALSE NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `isolated_symbol` VARCHAR(30) DEFAULT '' NOT NULL;
