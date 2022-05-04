9333d063ecd42c056be54e3c3b0fd18cd78bf722
c9s <yoanlin93@gmail.com>
2022-05-04 14:40:41 +0800

    update migration sql

--- migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
+++ migrations/sqlite3/20220503144849_add_margin_info_to_nav.sql
@@ -3,6 +3,9 @@ ALTER TABLE `nav_history_details` ADD COLUMN `session` VARCHAR(50) NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `borrowed` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `net_asset` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
 ALTER TABLE `nav_history_details` ADD COLUMN `price_in_usd` DECIMAL UNSIGNED DEFAULT 0.00000000 NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `is_margin` BOOL DEFAULT FALSE NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `is_isolated` BOOL DEFAULT FALSE NOT NULL;
+ALTER TABLE `nav_history_details` ADD COLUMN `isolated_symbol` VARCHAR(30) DEFAULT '' NOT NULL;
 
 -- +down
 -- we can not rollback alter table change in sqlite
