1fd18f93266835f82ad5b8e6bbf902b30da25a56
c9s <yoanlin93@gmail.com>
2022-05-04 18:58:10 +0800

    compile and update migration package

--- pkg/migrations/sqlite3/20211211034818_add_nav_history_details.go
+++ pkg/migrations/sqlite3/20211211034818_add_nav_history_details.go
@@ -14,7 +14,7 @@ func init() {
 func upAddNavHistoryDetails(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
 	// This code is executed when the migration is applied.
 
-	_, err = tx.ExecContext(ctx, "CREATE TABLE `nav_history_details`\n(\n    gid    bigint unsigned auto_increment PRIMARY KEY,\n    `exchange`          VARCHAR             NOT NULL DEFAULT '',\n    `subaccount`          VARCHAR             NOT NULL DEFAULT '',\n    time   DATETIME(3)     NOT NULL DEFAULT (strftime('%s','now')),\n    currency     VARCHAR                                NOT NULL,\n    balance_in_usd DECIMAL DEFAULT 0.00000000 NOT NULL,\n    balance_in_btc DECIMAL DEFAULT 0.00000000 NOT NULL,\n    balance    DECIMAL DEFAULT 0.00000000 NOT NULL,\n    available  DECIMAL DEFAULT 0.00000000 NOT NULL,\n    locked     DECIMAL DEFAULT 0.00000000 NOT NULL\n);")
+	_, err = tx.ExecContext(ctx, "CREATE TABLE `nav_history_details`\n(\n    `gid`              BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,\n    `exchange`         VARCHAR(30)                NOT NULL DEFAULT '',\n    `subaccount`       VARCHAR(30)                NOT NULL DEFAULT '',\n    `time`             DATETIME(3)                NOT NULL DEFAULT (strftime('%s', 'now')),\n    `currency`         VARCHAR(30)                NOT NULL,\n    `net_asset_in_usd` DECIMAL DEFAULT 0.00000000 NOT NULL,\n    `net_asset_in_btc` DECIMAL DEFAULT 0.00000000 NOT NULL,\n    `balance`          DECIMAL DEFAULT 0.00000000 NOT NULL,\n    `available`        DECIMAL DEFAULT 0.00000000 NOT NULL,\n    `locked`           DECIMAL DEFAULT 0.00000000 NOT NULL\n);")
 	if err != nil {
 		return err
 	}
