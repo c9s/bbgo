4404098bf92c403bcafc6920e458c63cec3cdbc9
c9s <yoanlin93@gmail.com>
2022-05-04 17:39:35 +0800

    fix balance map add

--- pkg/types/account_test.go
+++ pkg/types/account_test.go
@@ -19,6 +19,23 @@ func TestBalanceMap_Add(t *testing.T) {
 		},
 	})
 	assert.Len(t, bm2, 1)
+
+	var bm3 = bm2.Add(BalanceMap{
+		"BTC": Balance{
+			Currency:  "BTC",
+			Available: fixedpoint.MustNewFromString("1.0"),
+			Locked:    fixedpoint.MustNewFromString("0"),
+			NetAsset:  fixedpoint.MustNewFromString("1.0"),
+		},
+		"LTC": Balance{
+			Currency:  "LTC",
+			Available: fixedpoint.MustNewFromString("20.0"),
+			Locked:    fixedpoint.MustNewFromString("0"),
+			NetAsset:  fixedpoint.MustNewFromString("20.0"),
+		},
+	})
+	assert.Len(t, bm3, 2)
+	assert.Equal(t, fixedpoint.MustNewFromString("11.0"), bm3["BTC"].Available)
 }
 
 func TestAccountLockAndUnlock(t *testing.T) {
