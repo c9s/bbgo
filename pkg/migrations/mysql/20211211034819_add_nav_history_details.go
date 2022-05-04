01273f7c4c0da983fc6c532f870361e3c17242e2
c9s <yoanlin93@gmail.com>
2022-05-04 14:23:53 +0800

    compile and update migration package

--- pkg/migrations/mysql/20211211034819_add_nav_history_details.go
+++ pkg/migrations/mysql/20211211034819_add_nav_history_details.go
@@ -14,12 +14,12 @@ func init() {
 func upAddNavHistoryDetails(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
 	// This code is executed when the migration is applied.
 
-	_, err = tx.ExecContext(ctx, "CREATE TABLE nav_history_details\n(\n    gid        bigint unsigned auto_increment PRIMARY KEY,\n    exchange   VARCHAR(30)                                 NOT NULL,\n    subaccount VARCHAR(30)                                 NOT NULL,\n    time       DATETIME(3)                                 NOT NULL,\n    currency     VARCHAR(12)                                 NOT NULL,\n    balance_in_usd DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000  NOT NULL,\n    balance_in_btc DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000  NOT NULL,\n    balance    DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000  NOT NULL,\n    available  DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000  NOT NULL,\n    locked     DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000  NOT NULL\n);")
+	_, err = tx.ExecContext(ctx, "CREATE TABLE nav_history_details\n(\n    gid            bigint unsigned auto_increment PRIMARY KEY,\n    exchange       VARCHAR(30)                                NOT NULL,\n    subaccount     VARCHAR(30)                                NOT NULL,\n    time           DATETIME(3)                                NOT NULL,\n    currency       VARCHAR(12)                                NOT NULL,\n    balance_in_usd DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,\n    balance_in_btc DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,\n    balance        DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,\n    available      DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL,\n    locked         DECIMAL(32, 8) UNSIGNED DEFAULT 0.00000000 NOT NULL\n);")
 	if err != nil {
 		return err
 	}
 
-	_, err = tx.ExecContext(ctx, "CREATE INDEX idx_nav_history_details\n    on nav_history_details(time, currency, exchange);")
+	_, err = tx.ExecContext(ctx, "CREATE INDEX idx_nav_history_details\n    on nav_history_details (time, currency, exchange);")
 	if err != nil {
 		return err
 	}
