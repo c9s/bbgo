bff73a3a80e3e9bcaa0d91e1cee4f248349c6bf7
c9s <yoanlin93@gmail.com>
2022-05-09 19:27:02 +0800

    format backtest report session name

--- pkg/backtest/report.go
+++ pkg/backtest/report.go
@@ -1,6 +1,10 @@
 package backtest
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/c9s/bbgo/pkg/accounting/pnl"
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
@@ -15,3 +19,14 @@ type Report struct {
 	FinalBalances   types.BalanceMap          `json:"finalBalances,omitempty"`
 }
 
+const SessionTimeFormat = "2006-01-02T15_04"
+
+// FormatSessionName returns the back-test session name
+func FormatSessionName(sessions []string, symbols []string, startTime, endTime time.Time) string {
+	return fmt.Sprintf("%s_%s_%s-%s",
+		strings.Join(sessions, "-"),
+		strings.Join(symbols, "-"),
+		startTime.Format(SessionTimeFormat),
+		endTime.Format(SessionTimeFormat),
+	)
+}
