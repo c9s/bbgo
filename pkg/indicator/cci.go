2bbb36031c2c72396eb887c4a1f86c0a6bbdc6cd
zenix <zenixls2@gmail.com>
2022-05-10 17:15:26 +0900

    fix: index range in float64slice and wrong formula given by investopedia, test: add cci test

--- pkg/indicator/cci.go
+++ pkg/indicator/cci.go
@@ -1,54 +1,55 @@
 package indicator
 
 import (
+	"math"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
 )
 
 // Refer: Commodity Channel Index
-// Refer URL: https://www.investopedia.com/terms/c/commoditychannelindex.asp
+// Refer URL: http://www.andrewshamlet.net/2017/07/08/python-tutorial-cci
+// with modification of ddof=0 to let standard deviation to be divided by N instead of N-1
 //go:generate callbackgen -type CCI
 type CCI struct {
 	types.IntervalWindow
-	HLC3 types.Float64Slice
+	Input        types.Float64Slice
 	TypicalPrice types.Float64Slice
-	MA types.Float64Slice
-	Values types.Float64Slice
+	MA           types.Float64Slice
+	Values       types.Float64Slice
 
 	UpdateCallbacks []func(value float64)
 }
 
-func (inc *CCI) Update(high, low, cloze float64) {
+func (inc *CCI) Update(value float64) {
 	if len(inc.TypicalPrice) == 0 {
-		inc.HLC3.Push((high + low + cloze) / 3.)
-		inc.TypicalPrice.Push(inc.HLC3.Last())
+		inc.TypicalPrice.Push(value)
+		inc.Input.Push(value)
 		return
 	} else if len(inc.TypicalPrice) > MaxNumOfEWMA {
 		inc.TypicalPrice = inc.TypicalPrice[MaxNumOfEWMATruncateSize-1:]
-		inc.HLC3 = inc.HLC3[MaxNumOfEWMATruncateSize-1:]
+		inc.Input = inc.Input[MaxNumOfEWMATruncateSize-1:]
 	}
 
-	hlc3 := (high + low + cloze) / 3.
-	tp := inc.TypicalPrice.Last() - inc.HLC3.Index(inc.Window) + hlc3
+	inc.Input.Push(value)
+	tp := inc.TypicalPrice.Last() - inc.Input.Index(inc.Window) + value
 	inc.TypicalPrice.Push(tp)
-	if len(inc.TypicalPrice) < inc.Window {
+	if len(inc.Input) < inc.Window {
 		return
 	}
-	ma := 0.
-	for i := 0; i < inc.Window; i++ {
-		ma += inc.TypicalPrice.Index(i)
-	}
-	ma /= float64(inc.Window)
+	ma := tp / float64(inc.Window)
 	inc.MA.Push(ma)
-	if len(inc.MA) < MaxNumOfEWMA {
+	if len(inc.MA) > MaxNumOfEWMA {
 		inc.MA = inc.MA[MaxNumOfEWMATruncateSize-1:]
 	}
 	md := 0.
 	for i := 0; i < inc.Window; i++ {
-		md += inc.TypicalPrice.Index(i) - inc.MA.Index(i)
+		diff := inc.Input.Index(i) - ma
+		md += diff * diff
 	}
-	md /= float64(inc.Window)
+	md = math.Sqrt(md / float64(inc.Window))
 
-	cci := (tp - ma) / (0.15 * md)
+	cci := (value - ma) / (0.015 * md)
 
 	inc.Values.Push(cci)
 	if len(inc.Values) > MaxNumOfEWMA {
@@ -60,7 +61,7 @@ func (inc *CCI) Last() float64 {
 	if len(inc.Values) == 0 {
 		return 0
 	}
-	return inc.Values[len(inc.Values) - 1]
+	return inc.Values[len(inc.Values)-1]
 }
 
 func (inc *CCI) Index(i int) float64 {
@@ -76,15 +77,17 @@ func (inc *CCI) Length() int {
 
 var _ types.Series = &CCI{}
 
+var three = fixedpoint.NewFromInt(3)
+
 func (inc *CCI) calculateAndUpdate(allKLines []types.KLine) {
-	if inc.HLC3.Length() == 0 {
+	if inc.TypicalPrice.Length() == 0 {
 		for _, k := range allKLines {
-			inc.Update(k.High.Float64(), k.Low.Float64(), k.Close.Float64())
+			inc.Update(k.High.Add(k.Low).Add(k.Close).Div(three).Float64())
 			inc.EmitUpdate(inc.Last())
 		}
 	} else {
 		k := allKLines[len(allKLines)-1]
-		inc.Update(k.High.Float64(), k.Low.Float64(), k.Close.Float64())
+		inc.Update(k.High.Add(k.Low).Add(k.Close).Div(three).Float64())
 		inc.EmitUpdate(inc.Last())
 	}
 }
