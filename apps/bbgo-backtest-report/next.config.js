9d0483087e7f2018fa28d24937224d3c0659e70d
c9s <yoanlin93@gmail.com>
2022-05-13 19:29:24 +0800

    add workaround for chart lib
    https://github.com/react-financial/react-financial-charts/issues/606

--- apps/bbgo-backtest-report/next.config.js
+++ apps/bbgo-backtest-report/next.config.js
@@ -1,6 +1,27 @@
+
+// workaround for react financial charts
+// https://github.com/react-financial/react-financial-charts/issues/606
+const withTM = require('next-transpile-modules')([
+    'd3-array',
+    'd3-format',
+    'd3-time',
+    'd3-time-format',
+    'react-financial-charts',
+    '@react-financial-charts/annotations',
+    '@react-financial-charts/axes',
+    '@react-financial-charts/coordinates',
+    '@react-financial-charts/core',
+    '@react-financial-charts/indicators',
+    '@react-financial-charts/interactive',
+    '@react-financial-charts/scales',
+    '@react-financial-charts/series',
+    '@react-financial-charts/tooltip',
+    '@react-financial-charts/utils',
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
