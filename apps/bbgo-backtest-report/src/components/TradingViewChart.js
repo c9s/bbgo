1ad0ca7d263db5afb1d4422ae0314db61c2967a8
c9s <yoanlin93@gmail.com>
2022-05-17 14:56:41 +0800

    add volume histogram

--- apps/bbgo-backtest-report/src/components/TradingViewChart.js
+++ apps/bbgo-backtest-report/src/components/TradingViewChart.js
@@ -138,8 +138,6 @@ const ordersToMarkets = (orders) => {
   return markers;
 };
 
-
-
 function fetchKLines(symbol, interval, setter) {
   return fetch(
     `/data/klines/${symbol}-${interval}.tsv`,
@@ -155,19 +153,37 @@ function fetchKLines(symbol, interval, setter) {
     });
 }
 
+const klinesToVolumeData = (klines) => {
+  const volumes = [];
+
+  for (let i = 0 ; i < klines.length ; i++) {
+    const kline = klines[i];
+    volumes.push({
+      time: (kline.startTime.getTime() / 1000),
+      value: kline.volume,
+    })
+  }
+
+  return volumes;
+}
+
 const positionAverageCostHistoryToLineData = (hs) => {
   const avgCosts = [];
   for (let i = 0; i < hs.length; i++) {
     let pos = hs[i];
 
+    if (i > 0 && pos.average_cost == hs[i-1].average_cost) {
+      continue;
+    }
+
     if (pos.base == 0) {
       avgCosts.push({
-        time: pos.time,
+        time: pos.time.getTime() / 1000,
         value: 0,
       });
     } else {
       avgCosts.push({
-        time: pos.time,
+        time: pos.time.getTime() / 1000,
         value: pos.average_cost,
       });
     }
@@ -234,18 +250,28 @@ const TradingViewChart = (props) => {
       wickDownColor: 'rgb(255,82,82)',
       borderVisible: false,
     });
-
-    /*
-    const data2 = [{ open: 10, high: 10.63, low: 9.49, close: 9.55, time: 1642427876 }, { open: 9.55, high: 10.30, low: 9.42, close: 9.94, time: 1642514276 }, { open: 9.94, high: 10.17, low: 9.92, close: 9.78, time: 1642600676 }, { open: 9.78, high: 10.59, low: 9.18, close: 9.51, time: 1642687076 }, { open: 9.51, high: 10.46, low: 9.10, close: 10.17, time: 1642773476 }, { open: 10.17, high: 10.96, low: 10.16, close: 10.47, time: 1642859876 }, { open: 10.47, high: 11.39, low: 10.40, close: 10.81, time: 1642946276 }, { open: 10.81, high: 11.60, low: 10.30, close: 10.75, time: 1643032676 }, { open: 10.75, high: 11.60, low: 10.49, close: 10.93, time: 1643119076 }, { open: 10.93, high: 11.53, low: 10.76, close: 10.96, time: 1643205476 }];
-    series.setData(data2);
-    */
-    // series.setData(data.slice(0, 95));
     series.setData(data);
     series.setMarkers(markers);
 
-    console.log(positionHistory);
     const lineSeries = chart.addLineSeries();
-    lineSeries.setData(positionAverageCostHistoryToLineData(positionHistory));
+    const costLine = positionAverageCostHistoryToLineData(positionHistory);
+    lineSeries.setData(costLine);
+
+    const volumeData = klinesToVolumeData(data);
+    const volumeSeries = chart.addHistogramSeries({
+      color: '#182233',
+      lineWidth: 2,
+      priceFormat: {
+        type: 'volume',
+      },
+      overlay: true,
+      scaleMargins: {
+        top: 0.8,
+        bottom: 0,
+      },
+    });
+    volumeSeries.setData(volumeData);
+
   }, [ref.current, data])
   return <div>
     <div ref={ref}>
