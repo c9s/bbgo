2577767e8a613474a6426871f2232c9d2edd5311
c9s <yoanlin93@gmail.com>
2022-05-26 20:32:25 +0800

    max: update market struct fields

--- pkg/exchange/max/maxapi/public.go
+++ pkg/exchange/max/maxapi/public.go
@@ -22,12 +22,14 @@ type PublicService struct {
 type Market struct {
 	ID                 string           `json:"id"`
 	Name               string           `json:"name"`
+	Status             string           `json:"market_status"` // active
 	BaseUnit           string           `json:"base_unit"`
 	BaseUnitPrecision  int              `json:"base_unit_precision"`
 	QuoteUnit          string           `json:"quote_unit"`
 	QuoteUnitPrecision int              `json:"quote_unit_precision"`
 	MinBaseAmount      fixedpoint.Value `json:"min_base_amount"`
 	MinQuoteAmount     fixedpoint.Value `json:"min_quote_amount"`
+	SupportMargin      bool             `json:"m_wallet_supported"`
 }
 
 type Ticker struct {
