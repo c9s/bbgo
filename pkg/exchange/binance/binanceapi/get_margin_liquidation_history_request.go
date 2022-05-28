e72f8bcd1556b1a994f0412367056ca681f0517a
c9s <yoanlin93@gmail.com>
2022-05-29 00:57:46 +0800

    binance: fix and rename margin liquidation history request

---
+++ pkg/exchange/binance/binanceapi/get_margin_liquidation_history_request.go
@@ -0,0 +1,38 @@
+package binanceapi
+
+import (
+	"time"
+
+	"github.com/c9s/requestgen"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+type MarginLiquidationRecord struct {
+	AvgPrice    fixedpoint.Value           `json:"avgPrice"`
+	ExecutedQty fixedpoint.Value           `json:"executedQty"`
+	OrderId     uint64                     `json:"orderId"`
+	Price       fixedpoint.Value           `json:"price"`
+	Qty         fixedpoint.Value           `json:"qty"`
+	Side        SideType                   `json:"side"`
+	Symbol      string                     `json:"symbol"`
+	TimeInForce string                     `json:"timeInForce"`
+	IsIsolated  bool                       `json:"isIsolated"`
+	UpdatedTime types.MillisecondTimestamp `json:"updatedTime"`
+}
+
+//go:generate requestgen -method GET -url "/sapi/v1/margin/forceLiquidationRec" -type GetMarginLiquidationHistoryRequest -responseType .RowsResponse -responseDataField Rows -responseDataType []MarginLiquidationRecord
+type GetMarginLiquidationHistoryRequest struct {
+	client requestgen.AuthenticatedAPIClient
+
+	isolatedSymbol *string    `param:"isolatedSymbol"`
+	startTime      *time.Time `param:"startTime,milliseconds"`
+	endTime        *time.Time `param:"endTime,milliseconds"`
+	size           *int       `param:"size"`
+	current        *int       `param:"current"`
+}
+
+func (c *RestClient) NewGetMarginLiquidationHistoryRequest() *GetMarginLiquidationHistoryRequest {
+	return &GetMarginLiquidationHistoryRequest{client: c}
+}
