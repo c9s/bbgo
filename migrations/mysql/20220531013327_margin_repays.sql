54090d87b9ed48169fb917f4d4007ab2f482bf51
c9s <yoanlin93@gmail.com>
2022-05-31 01:40:21 +0800

    add margin history migrations

---
+++ migrations/mysql/20220531013327_margin_repays.sql
@@ -0,0 +1,24 @@
+-- +up
+CREATE TABLE `margin_repays`
+(
+    `gid`             BIGINT UNSIGNED         NOT NULL AUTO_INCREMENT,
+
+    `transaction_id`  BIGINT UNSIGNED         NOT NULL,
+
+    `exchange`        VARCHAR(24)             NOT NULL DEFAULT '',
+
+    `asset`           VARCHAR(24)             NOT NULL DEFAULT '',
+
+    `isolated_symbol` VARCHAR(24)             NOT NULL DEFAULT '',
+
+    -- quantity is the quantity of the trade that makes profit
+    `principle`       DECIMAL(16, 8) UNSIGNED NOT NULL,
+
+    `time`            DATETIME(3)             NOT NULL,
+
+    PRIMARY KEY (`gid`),
+    UNIQUE KEY (`transaction_id`)
+);
+
+-- +down
+DROP TABLE IF EXISTS `margin_repays`;
