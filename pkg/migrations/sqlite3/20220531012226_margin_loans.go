7601f087869e6bca7ec42575d04d0270bf8e5459
c9s <yoanlin93@gmail.com>
2022-05-31 17:30:24 +0800

    compile and update migration package

---
+++ pkg/migrations/sqlite3/20220531012226_margin_loans.go
@@ -0,0 +1,34 @@
+package sqlite3
+
+import (
+	"context"
+
+	"github.com/c9s/rockhopper"
+)
+
+func init() {
+	AddMigration(upMarginLoans, downMarginLoans)
+
+}
+
+func upMarginLoans(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is applied.
+
+	_, err = tx.ExecContext(ctx, "CREATE TABLE `margin_loans`\n(\n    `gid`             INTEGER PRIMARY KEY AUTOINCREMENT,\n    `transaction_id`  INTEGER        NOT NULL,\n    `exchange`        VARCHAR(24)    NOT NULL DEFAULT '',\n    `asset`           VARCHAR(24)    NOT NULL DEFAULT '',\n    `isolated_symbol` VARCHAR(24)    NOT NULL DEFAULT '',\n    -- quantity is the quantity of the trade that makes profit\n    `principle`       DECIMAL(16, 8) NOT NULL,\n    `time`            DATETIME(3)    NOT NULL\n);")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func downMarginLoans(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is rolled back.
+
+	_, err = tx.ExecContext(ctx, "DROP TABLE IF EXISTS `margin_loans`;")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
