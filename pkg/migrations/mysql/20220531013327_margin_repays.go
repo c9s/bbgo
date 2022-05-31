bf0a93edd4dbdc974a3c8d6c813c08102d5128e4
c9s <yoanlin93@gmail.com>
2022-05-31 17:30:24 +0800

    compile and update migration package

---
+++ pkg/migrations/mysql/20220531013327_margin_repays.go
@@ -0,0 +1,34 @@
+package mysql
+
+import (
+	"context"
+
+	"github.com/c9s/rockhopper"
+)
+
+func init() {
+	AddMigration(upMarginRepays, downMarginRepays)
+
+}
+
+func upMarginRepays(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is applied.
+
+	_, err = tx.ExecContext(ctx, "CREATE TABLE `margin_repays`\n(\n    `gid`             BIGINT UNSIGNED         NOT NULL AUTO_INCREMENT,\n    `transaction_id`  BIGINT UNSIGNED         NOT NULL,\n    `exchange`        VARCHAR(24)             NOT NULL DEFAULT '',\n    `asset`           VARCHAR(24)             NOT NULL DEFAULT '',\n    `isolated_symbol` VARCHAR(24)             NOT NULL DEFAULT '',\n    -- quantity is the quantity of the trade that makes profit\n    `principle`       DECIMAL(16, 8) UNSIGNED NOT NULL,\n    `time`            DATETIME(3)             NOT NULL,\n    PRIMARY KEY (`gid`),\n    UNIQUE KEY (`transaction_id`)\n);")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func downMarginRepays(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is rolled back.
+
+	_, err = tx.ExecContext(ctx, "DROP TABLE IF EXISTS `margin_repays`;")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
---
+++ pkg/migrations/mysql/20220531013542_margin_interests.go
@@ -0,0 +1,34 @@
+package mysql
+
+import (
+	"context"
+
+	"github.com/c9s/rockhopper"
+)
+
+func init() {
+	AddMigration(upMarginInterests, downMarginInterests)
+
+}
+
+func upMarginInterests(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is applied.
+
+	_, err = tx.ExecContext(ctx, "CREATE TABLE `margin_interests`\n(\n    `gid`             BIGINT UNSIGNED          NOT NULL AUTO_INCREMENT,\n    `exchange`        VARCHAR(24)              NOT NULL DEFAULT '',\n    `asset`           VARCHAR(24)              NOT NULL DEFAULT '',\n    `isolated_symbol` VARCHAR(24)              NOT NULL DEFAULT '',\n    `principle`       DECIMAL(16, 8) UNSIGNED  NOT NULL,\n    `interest`        DECIMAL(20, 16) UNSIGNED NOT NULL,\n    `interest_rate`   DECIMAL(20, 16) UNSIGNED NOT NULL,\n    `time`            DATETIME(3)              NOT NULL,\n    PRIMARY KEY (`gid`)\n);")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func downMarginInterests(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is rolled back.
+
+	_, err = tx.ExecContext(ctx, "DROP TABLE IF EXISTS `margin_interests`;")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
