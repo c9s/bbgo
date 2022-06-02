a150c56f6d0b5b5bf62bdd0b0b4e11c39b4df564
c9s <yoanlin93@gmail.com>
2022-06-02 16:51:44 +0800

    batch: add remote query profiler

--- pkg/util/profile.go
+++ pkg/util/profile.go
@@ -1,14 +1,21 @@
 package util
 
-import "time"
+import (
+	"time"
+)
 
 type TimeProfile struct {
+	Name               string
 	StartTime, EndTime time.Time
 	Duration           time.Duration
 }
 
-func StartTimeProfile() TimeProfile {
-	return TimeProfile{StartTime: time.Now()}
+func StartTimeProfile(args ...string) TimeProfile {
+	name := ""
+	if len(args) > 0 {
+		name = args[0]
+	}
+	return TimeProfile{StartTime: time.Now(), Name: name}
 }
 
 func (p *TimeProfile) TilNow() time.Duration {
@@ -19,4 +26,17 @@ func (p *TimeProfile) Stop() time.Duration {
 	p.EndTime = time.Now()
 	p.Duration = p.EndTime.Sub(p.StartTime)
 	return p.Duration
+}
+
+type logFunction func(format string, args ...interface{})
+
+func (p *TimeProfile) StopAndLog(f logFunction) {
+	duration := p.Stop()
+	s := "[profile] "
+	if len(p.Name) > 0 {
+		s += p.Name
+	}
+
+	s += " " + duration.String()
+	f(s)
 }
