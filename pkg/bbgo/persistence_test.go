bbdd0198e169fe9cdaaf47ec29b2aa99077d2165
c9s <yoanlin93@gmail.com>
2022-06-03 02:49:16 +0800

    persistence: add store and load test case

--- pkg/bbgo/persistence_test.go
+++ pkg/bbgo/persistence_test.go
@@ -68,6 +68,23 @@ func Test_loadPersistenceFields(t *testing.T) {
 			err := loadPersistenceFields(b, "test-nil", ps)
 			assert.Equal(t, errCanNotIterateNilPointer, err)
 		})
+
+		t.Run(psName+"/pointer-field", func(t *testing.T) {
+			var a = &TestStruct{
+				Position: types.NewPosition("BTCUSDT", "BTC", "USDT"),
+			}
+			a.Position.Base = fixedpoint.NewFromFloat(10.0)
+			a.Position.AverageCost = fixedpoint.NewFromFloat(3343.0)
+			err := storePersistenceFields(a, "pointer-field-test", ps)
+			assert.NoError(t, err)
+
+			b := &TestStruct{}
+			err = loadPersistenceFields(b, "pointer-field-test", ps)
+			assert.NoError(t, err)
+
+			assert.Equal(t, "10", a.Position.Base.String())
+			assert.Equal(t, "3343", a.Position.AverageCost.String())
+		})
 	}
 }
 
