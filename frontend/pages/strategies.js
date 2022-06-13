46375eb4773281009f510193445eb30546b0cf4d
Jun Su <sujunjun19@gmail.com>
2022-06-13 16:48:57 +0800

    add strategies route

---
+++ frontend/pages/strategies.js
@@ -0,0 +1,12 @@
+import DashboardLayout from '../layouts/DashboardLayout';
+
+
+export default function Strategies(){
+
+  return (
+    <DashboardLayout>
+      Hello World   
+    </DashboardLayout>
+
+  );
+}
