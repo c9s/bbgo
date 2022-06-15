21ec4fdb307d7064cfc9e1c2ac5d32e1e980799f
Jun Su <sujunjun19@gmail.com>
2022-06-15 19:02:24 +0800

    run prettier

--- frontend/hooks/useInterval.ts
+++ frontend/hooks/useInterval.ts
@@ -1,20 +1,20 @@
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from 'react';
 
 export default function useInterval(cb: Function, delayMs: number | null) {
   const savedCallback = useRef<Function>();
-  
+
   useEffect(() => {
-    savedCallback.current = cb
-  }, [cb])
-  
+    savedCallback.current = cb;
+  }, [cb]);
+
   useEffect(() => {
     function tick() {
       savedCallback.current();
     }
-    
+
     if (delayMs !== null) {
-      let timerId = setInterval(tick, delayMs)
-      return () => clearInterval(timerId)
+      let timerId = setInterval(tick, delayMs);
+      return () => clearInterval(timerId);
     }
-  }, [delayMs])
+  }, [delayMs]);
 }
