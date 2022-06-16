4b14e7f7e5c49369e25af2d2968e9f3bb0a1e9c8
c9s <yoanlin93@gmail.com>
2022-06-16 15:22:36 +0800

    refactor maxapi files

--- pkg/exchange/max/maxapi/v3/create_wallet_order_request.go
+++ pkg/exchange/max/maxapi/v3/create_wallet_order_request.go
@@ -2,6 +2,10 @@ package v3
 
 import "github.com/c9s/requestgen"
 
+//go:generate -command GetRequest requestgen -method GET
+//go:generate -command PostRequest requestgen -method POST
+//go:generate -command DeleteRequest requestgen -method DELETE
+
 //go:generate PostRequest -url "/api/v3/wallet/:walletType/orders" -type CreateWalletOrderRequest -responseType .Order
 type CreateWalletOrderRequest struct {
 	client requestgen.AuthenticatedAPIClient
