aba6b0db20b07ba71e06877d991cb0fd312743e2
なるみ <narumi@maicoin.com>
2022-06-24 23:21:32 +0800

    glassnode: add glassnode table

---
+++ migrations/mysql/20220624213742_add_glassnode_table.sql
@@ -0,0 +1,21 @@
+-- +up
+-- +begin
+CREATE TABLE `glassnode`
+(
+    `gid`       BIGINT UNSIGNED     NOT NULL AUTO_INCREMENT,
+    `category`  VARCHAR(64)         NOT NULL,
+    `metric`    VARCHAR(64)         NOT NULL,
+    `asset`     VARCHAR(20)         NOT NULL,
+    `interval`  VARCHAR(6)          NOT NULL,
+    `time`      DATETIME(3)         NOT NULL,
+    `key`       VARCHAR(64)         NOT NULL,
+    `value`     DECIMAL(32, 8)      NOT NULL,
+
+    PRIMARY KEY (`gid`)
+);
+-- +end
+
+-- +down
+-- +begin
+DROP TABLE IF EXISTS `glassnode`;
+-- +end
