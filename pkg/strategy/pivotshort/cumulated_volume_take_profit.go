e9b87f6f1e07a81936267210ef500be2bdadc1a6
c9s <yoanlin93@gmail.com>
2022-06-26 16:31:48 +0800

    pivotshort: refactor exit methods
    Signed-off-by: c9s <yoanlin93@gmail.com>

---
+++ pkg/strategy/pivotshort/cumulated_volume_take_profit.go
@@ -0,0 +1,64 @@
+package pivotshort
+
+import (
+	"context"
+
+	"github.com/c9s/bbgo/pkg/bbgo"
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+type CumulatedVolumeTakeProfit struct {
+	types.IntervalWindow
+	Ratio          fixedpoint.Value `json:"ratio"`
+	MinQuoteVolume fixedpoint.Value `json:"minQuoteVolume"`
+
+	session       *bbgo.ExchangeSession
+	orderExecutor *bbgo.GeneralOrderExecutor
+}
+
+func (s *CumulatedVolumeTakeProfit) Bind(session *bbgo.ExchangeSession, orderExecutor *bbgo.GeneralOrderExecutor) {
+	s.session = session
+	s.orderExecutor = orderExecutor
+
+	position := orderExecutor.Position()
+
+	store, _ := session.MarketDataStore(position.Symbol)
+
+	session.MarketDataStream.OnKLineClosed(func(kline types.KLine) {
+		if kline.Symbol != position.Symbol || kline.Interval != types.Interval1m {
+			return
+		}
+
+		closePrice := kline.Close
+		if position.IsClosed() || position.IsDust(closePrice) {
+			return
+		}
+
+		roi := position.ROI(closePrice)
+		if roi.Sign() < 0 {
+			return
+		}
+
+		if klines, ok := store.KLinesOfInterval(s.Interval); ok {
+			var cbv = fixedpoint.Zero
+			var cqv = fixedpoint.Zero
+			for i := 0; i < s.Window; i++ {
+				last := (*klines)[len(*klines)-1-i]
+				cqv = cqv.Add(last.QuoteVolume)
+				cbv = cbv.Add(last.Volume)
+			}
+
+			if cqv.Compare(s.MinQuoteVolume) > 0 {
+				bbgo.Notify("%s TakeProfit triggered by cumulated volume (window: %d) %f > %f, price = %f",
+					position.Symbol,
+					s.Window,
+					cqv.Float64(),
+					s.MinQuoteVolume.Float64(), kline.Close.Float64())
+
+				_ = orderExecutor.ClosePosition(context.Background(), fixedpoint.One)
+				return
+			}
+		}
+	})
+}
