7d7351d22f0faae997f0487da9c899386a8e39f9
c9s <yoanlin93@gmail.com>
2022-06-27 18:20:26 +0800

    show order tag in the order list

--- apps/backtest-report/components/OrderListTable.tsx
+++ apps/backtest-report/components/OrderListTable.tsx
@@ -1,6 +1,7 @@
 import {Button, Checkbox, Group, Table} from "@mantine/core";
 import React, {useState} from "react";
 import {Order} from "../types";
+import moment from "moment";
 
 interface OrderListTableProps {
   orders: Order[];
@@ -12,7 +13,7 @@ const OrderListTable = (props: OrderListTableProps) => {
   let orders = props.orders;
 
   const [showCanceledOrders, setShowCanceledOrders] = useState(false);
-  const [limit, setLimit] = useState(props.limit || 5);
+  const [limit, setLimit] = useState(props.limit || 100);
 
   if (!showCanceledOrders) {
     orders = orders.filter((order: Order) => {
@@ -40,7 +41,8 @@ const OrderListTable = (props: OrderListTableProps) => {
       <td>{order.price}</td>
       <td>{order.quantity}</td>
       <td>{order.status}</td>
-      <td>{order.creation_time.toString()}</td>
+      <td>{formatDate(order.creation_time)}</td>
+      <td>{order.tag}</td>
     </tr>
   ));
 
@@ -63,11 +65,17 @@ const OrderListTable = (props: OrderListTableProps) => {
         <th>Quantity</th>
         <th>Status</th>
         <th>Creation Time</th>
+        <th>Tag</th>
       </tr>
       </thead>
       <tbody>{rows}</tbody>
     </Table>
   </div>
 }
+
+const formatDate = (d : Date) : string => {
+  return moment(d).format("MMM Do YY hh:mm:ss A Z");
+}
+
 
 export default OrderListTable;
