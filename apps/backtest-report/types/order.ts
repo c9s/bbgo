f5daf81b039b2dde1d2c3e6f13c1b9f7a55b00b0
c9s <yoanlin93@gmail.com>
2022-06-27 16:17:06 +0800

    backtest: pull out OrderListTable

---
+++ apps/backtest-report/types/order.ts
@@ -0,0 +1,14 @@
+
+export interface Order {
+  order_id: number;
+  order_type: string;
+  side: string;
+  symbol: string;
+  price: number;
+  quantity: number;
+  executed_quantity: number;
+  status: string;
+  update_time: Date;
+  creation_time: Date;
+  time?: Date;
+}
