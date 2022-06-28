4d112cde31d1bc00aea96787319442639ab0cceb
c9s <yoanlin93@gmail.com>
2022-06-29 01:31:56 +0800

    rename protectionStopLoss to protectiveStopLoss

--- pkg/strategy/pivotshort/exit.go
+++ pkg/strategy/pivotshort/exit.go
@@ -4,15 +4,15 @@ import "github.com/c9s/bbgo/pkg/bbgo"
 
 type ExitMethod struct {
 	RoiStopLoss               *RoiStopLoss               `json:"roiStopLoss"`
-	ProtectionStopLoss        *ProtectionStopLoss        `json:"protectionStopLoss"`
+	ProtectiveStopLoss        *ProtectiveStopLoss        `json:"protectiveStopLoss"`
 	RoiTakeProfit             *RoiTakeProfit             `json:"roiTakeProfit"`
 	LowerShadowTakeProfit     *LowerShadowTakeProfit     `json:"lowerShadowTakeProfit"`
 	CumulatedVolumeTakeProfit *CumulatedVolumeTakeProfit `json:"cumulatedVolumeTakeProfit"`
 }
 
 func (m *ExitMethod) Bind(session *bbgo.ExchangeSession, orderExecutor *bbgo.GeneralOrderExecutor) {
-	if m.ProtectionStopLoss != nil {
-		m.ProtectionStopLoss.Bind(session, orderExecutor)
+	if m.ProtectiveStopLoss != nil {
+		m.ProtectiveStopLoss.Bind(session, orderExecutor)
 	} else if m.RoiStopLoss != nil {
 		m.RoiStopLoss.Bind(session, orderExecutor)
 	} else if m.RoiTakeProfit != nil {
