cf0ca70d24e682f769e05defccc6f3c3a732b8f6
c9s <yoanlin93@gmail.com>
2022-06-29 18:51:22 +0800

    move and rename isSymbolBasedStrategy

--- pkg/dynamic/field.go
+++ pkg/dynamic/field.go
@@ -6,3 +6,21 @@ func HasField(rs reflect.Value, fieldName string) (field reflect.Value, ok bool)
 	field = rs.FieldByName(fieldName)
 	return field, field.IsValid()
 }
+
+func LookupSymbolField(rs reflect.Value) (string, bool) {
+	if rs.Kind() == reflect.Ptr {
+		rs = rs.Elem()
+	}
+
+	field := rs.FieldByName("Symbol")
+	if !field.IsValid() {
+		return "", false
+	}
+
+	if field.Kind() != reflect.String {
+		return "", false
+	}
+
+	return field.String(), true
+}
+
