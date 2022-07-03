cd98d96448b42801cce7d98b2a38138c67232e86
c9s <yoanlin93@gmail.com>
2022-07-04 02:21:14 +0800

    backtest: assign user data stream to backtest.Exchange before we call EmitStart

--- pkg/backtest/matching.go
+++ pkg/backtest/matching.go
@@ -99,7 +99,6 @@ func (m *SimplePriceMatching) CancelOrder(o types.Order) (types.Order, error) {
 		}
 		m.askOrders = orders
 		m.mu.Unlock()
-
 	}
 
 	if !found {
@@ -190,6 +189,8 @@ func (m *SimplePriceMatching) PlaceOrder(o types.SubmitOrder) (*types.Order, *ty
 		order2.Status = types.OrderStatusFilled
 		order2.ExecutedQuantity = order2.Quantity
 		order2.IsWorking = false
+
+		m.EmitOrderUpdate(order2)
 
 		// let the exchange emit the "FILLED" order update (we need the closed order)
 		// m.EmitOrderUpdate(order2)
