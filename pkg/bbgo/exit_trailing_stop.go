c052df5c1594a32d7ebb2a9f6e4f636dfc39bc53
c9s <yoanlin93@gmail.com>
2022-07-06 03:04:01 +0800

    add trailing stop and it's test cases with gomock
    Signed-off-by: c9s <yoanlin93@gmail.com>

--- pkg/bbgo/exit_trailing_stop.go
+++ pkg/bbgo/exit_trailing_stop.go
@@ -1,6 +1,9 @@
 package bbgo
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
 )
@@ -10,6 +13,8 @@ type TrailingStop2 struct {
 
 	// CallbackRate is the callback rate from the previous high price
 	CallbackRate fixedpoint.Value `json:"callbackRate,omitempty"`
+
+	ActivationRatio fixedpoint.Value `json:"activationRatio,omitempty"`
 
 	// ClosePosition is a percentage of the position to be closed
 	ClosePosition fixedpoint.Value `json:"closePosition,omitempty"`
@@ -22,6 +27,13 @@ type TrailingStop2 struct {
 	// KLine per Interval will be used for updating the stop order
 	Interval types.Interval `json:"interval,omitempty"`
 
+	Side types.SideType `json:"side,omitempty"`
+
+	latestHigh fixedpoint.Value
+
+	// activated: when the price reaches the min profit price, we set the activated to true to enable trailing stop
+	activated bool
+
 	// private fields
 	session       *ExchangeSession
 	orderExecutor *GeneralOrderExecutor
@@ -29,7 +41,7 @@ type TrailingStop2 struct {
 
 func (s *TrailingStop2) Subscribe(session *ExchangeSession) {
 	// use 1m kline to handle roi stop
-	session.Subscribe(types.KLineChannel, s.Symbol, types.SubscribeOptions{Interval: types.Interval1m})
+	session.Subscribe(types.KLineChannel, s.Symbol, types.SubscribeOptions{Interval: s.Interval})
 }
 
 func (s *TrailingStop2) Bind(session *ExchangeSession, orderExecutor *GeneralOrderExecutor) {
@@ -37,7 +49,7 @@ func (s *TrailingStop2) Bind(session *ExchangeSession, orderExecutor *GeneralOrd
 	s.orderExecutor = orderExecutor
 
 	position := orderExecutor.Position()
-	session.MarketDataStream.OnKLineClosed(types.KLineWith(s.Symbol, types.Interval1m, func(kline types.KLine) {
+	session.MarketDataStream.OnKLineClosed(types.KLineWith(s.Symbol, s.Interval, func(kline types.KLine) {
 		s.checkStopPrice(kline.Close, position)
 	}))
 
@@ -52,18 +64,82 @@ func (s *TrailingStop2) Bind(session *ExchangeSession, orderExecutor *GeneralOrd
 	}
 }
 
-func (s *TrailingStop2) checkStopPrice(closePrice fixedpoint.Value, position *types.Position) {
-	if position.IsClosed() || position.IsDust(closePrice) {
-		return
+func (s *TrailingStop2) getRatio(price fixedpoint.Value, position *types.Position) (fixedpoint.Value, error) {
+	switch s.Side {
+	case types.SideTypeBuy:
+		// for short position
+		return position.AverageCost.Sub(price).Div(price), nil
+	case types.SideTypeSell:
+		return price.Sub(position.AverageCost).Div(position.AverageCost), nil
 	}
 
-	/*
-	roi := position.ROI(closePrice)
-	if roi.Compare(s.CallbackRate.Neg()) < 0 {
-		// stop loss
-		Notify("[TrailingStop2] %s stop loss triggered by ROI %s/%s, price: %f", position.Symbol, roi.Percentage(), s.Percentage.Neg().Percentage(), closePrice.Float64())
-		_ = s.orderExecutor.ClosePosition(context.Background(), fixedpoint.One, "TrailingStop2")
-		return
+	return fixedpoint.Zero, fmt.Errorf("unexpected side type: %v", s.Side)
+}
+
+func (s *TrailingStop2) checkStopPrice(price fixedpoint.Value, position *types.Position) error {
+	if position.IsClosed() || position.IsDust(price) {
+		return nil
 	}
-	*/
+
+	if !s.MinProfit.IsZero() {
+		// check if we have the minimal profit
+		roi := position.ROI(price)
+		if roi.Compare(s.MinProfit) >= 0 {
+			Notify("[trailingStop] activated: ROI %f > minimal profit ratio %f", roi.Float64(), s.MinProfit.Float64())
+			s.activated = true
+		}
+	} else if !s.ActivationRatio.IsZero() {
+		ratio, err := s.getRatio(price, position)
+		if err != nil {
+			return err
+		}
+
+		if ratio.Compare(s.ActivationRatio) >= 0 {
+			s.activated = true
+		}
+	}
+
+	// update the latest high for the sell order, or the latest low for the buy order
+	if s.latestHigh.IsZero() {
+		s.latestHigh = price
+	} else {
+		switch s.Side {
+		case types.SideTypeBuy:
+			s.latestHigh = fixedpoint.Min(price, s.latestHigh)
+		case types.SideTypeSell:
+			s.latestHigh = fixedpoint.Max(price, s.latestHigh)
+		}
+	}
+
+	if !s.activated {
+		return nil
+	}
+
+	switch s.Side {
+	case types.SideTypeBuy:
+		s.latestHigh = fixedpoint.Min(price, s.latestHigh)
+
+		change := price.Sub(s.latestHigh).Div(s.latestHigh)
+		if change.Compare(s.CallbackRate) >= 0 {
+			// submit order
+			return s.triggerStop(price)
+		}
+
+	case types.SideTypeSell:
+		s.latestHigh = fixedpoint.Max(price, s.latestHigh)
+
+		change := price.Sub(s.latestHigh).Div(s.latestHigh)
+		if change.Compare(s.CallbackRate) >= 0 {
+			// submit order
+			return s.triggerStop(price)
+		}
+	}
+
+	return nil
+}
+
+func (s *TrailingStop2) triggerStop(price fixedpoint.Value) error {
+	Notify("[TrailingStop] %s stop loss triggered. price: %f callback rate: %f", s.Symbol, price.Float64(), s.CallbackRate.Float64())
+	ctx := context.Background()
+	return s.orderExecutor.ClosePosition(ctx, fixedpoint.One, "trailingStop")
 }
