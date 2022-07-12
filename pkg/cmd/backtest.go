7d232f86b8dd78a4421d6ac553f6c2efb508e243
c9s <yoanlin93@gmail.com>
2022-07-12 19:34:07 +0800

    remove duplicated dumper close

--- pkg/cmd/backtest.go
+++ pkg/cmd/backtest.go
@@ -341,9 +341,6 @@ var BacktestCmd = &cobra.Command{
 
 			dumper := backtest.NewKLineDumper(kLineDataDir)
 			defer func() {
-				_ = dumper.Close()
-			}()
-			defer func() {
 				if err := dumper.Close(); err != nil {
 					log.WithError(err).Errorf("kline dumper can not close files")
 				}
