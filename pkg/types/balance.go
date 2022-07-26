1ee0bb5eaab458d80fe0595fea408802eb0dd23d
c9s <yoanlin93@gmail.com>
2022-07-26 11:44:57 +0800

    types: call debt()

--- pkg/types/balance.go
+++ pkg/types/balance.go
@@ -42,7 +42,7 @@ func (b Balance) Total() fixedpoint.Value {
 // Net returns the net asset value (total - debt)
 func (b Balance) Net() fixedpoint.Value {
 	total := b.Total()
-	return total.Sub(b.Borrowed).Sub(b.Interest)
+	return total.Sub(b.Debt())
 }
 
 func (b Balance) Debt() fixedpoint.Value {
