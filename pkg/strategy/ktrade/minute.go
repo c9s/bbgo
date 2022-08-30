d27f13295f788527dd63f9a828399e42bf8a5935
austin362667 <austin362667@gmail.com>
2022-08-30 17:49:01 +0800

    ktrade: add second try

--- pkg/strategy/ktrade/minute.go
+++ pkg/strategy/ktrade/minute.go
@@ -28,13 +28,6 @@ type Minute struct {
 	bbgo.QuantityOrAmount
 }
 
-func (s *Minute) updateQuote(ctx context.Context, symbol string) {
-	//bestBid, bestAsk, _ := s.StreamBook.BestBidAndAsk()
-
-	//s.midPrice = bestBid.Price.Add(bestAsk.Price).Div(fixedpoint.NewFromInt(2))
-	//log.Info(s.midPrice)
-}
-
 func (s *Minute) Bind(session *bbgo.ExchangeSession, orderExecutor *bbgo.GeneralOrderExecutor) {
 	s.session = session
 	s.orderExecutor = orderExecutor
@@ -49,32 +42,42 @@ func (s *Minute) Bind(session *bbgo.ExchangeSession, orderExecutor *bbgo.General
 	session.MarketDataStream.OnMarketTrade(func(trade types.Trade) {
 
 		log.Infof("%s trade @ %f", trade.Side, trade.Price.Float64())
-		bestAsk, _ := s.StreamBook.BestAsk()
-		bestBid, _ := s.StreamBook.BestBid()
+		bestBid, bestAsk, hasPrice := s.StreamBook.BestBidAndAsk()
+		if !hasPrice {
+			return
+		}
 		s.midPrice = bestBid.Price.Add(bestAsk.Price).Div(fixedpoint.NewFromInt(2))
+		ctx := context.Background()
 
-		if trade.Side == types.SideTypeBuy && trade.Price.Compare(s.midPrice) > 0 {
-			_ = s.orderExecutor.GracefulCancel(context.Background())
-			// update ask price
-			newAskPrice := s.midPrice.Mul(fixedpoint.NewFromFloat(0.25)).Add(trade.Price.Mul(fixedpoint.NewFromFloat(0.75)))
-			log.Infof("short @ %f", newAskPrice.Float64())
-			if trade.Price.Compare(newAskPrice) > 0 {
-				s.placeOrder(context.Background(), types.SideTypeSell, s.Quantity, s.midPrice.Mul(fixedpoint.NewFromFloat(1.001)), symbol)
-			} else {
-				log.Infof("new")
-				s.placeOrder(context.Background(), types.SideTypeSell, s.Quantity, newAskPrice.Round(2, 1), symbol)
+		if trade.Side == types.SideTypeBuy && trade.Price.Div(bestAsk.Price).Compare(fixedpoint.NewFromFloat(1.00005)) > 0 {
+			canceled := s.orderExecutor.GracefulCancel(ctx)
+			if canceled != nil {
+				_ = s.orderExecutor.GracefulCancel(ctx)
 			}
-
-		} else if trade.Side == types.SideTypeSell && trade.Price.Compare(s.midPrice) < 0 {
-			_ = s.orderExecutor.GracefulCancel(context.Background())
+			// update ask price
+			//bestAsk, _ = s.StreamBook.BestAsk()
+			newAskPrice := bestAsk.Price.Mul(fixedpoint.NewFromFloat(1.001)).Mul(fixedpoint.NewFromFloat(0.25)).Add(trade.Price.Mul(fixedpoint.NewFromFloat(0.75)))
+			log.Infof("short @ %f", newAskPrice.Float64())
+			err := s.placeOrder(context.Background(), types.SideTypeSell, s.Quantity, newAskPrice.Round(2, 0), symbol)
+			if err != nil {
+				newAskPrice = bestAsk.Price.Mul(fixedpoint.NewFromFloat(1.002)).Mul(fixedpoint.NewFromFloat(0.25)).Add(trade.Price.Mul(fixedpoint.NewFromFloat(0.75)))
+				log.Infof("short again @ %f", newAskPrice.Float64())
+				_ = s.placeOrder(context.Background(), types.SideTypeSell, s.Quantity, newAskPrice.Round(2, 0), symbol)
+			}
+		} else if trade.Side == types.SideTypeSell && trade.Price.Div(bestBid.Price).Compare(fixedpoint.NewFromFloat(0.99995)) < 0 {
+			canceled := s.orderExecutor.GracefulCancel(ctx)
+			if canceled != nil {
+				_ = s.orderExecutor.GracefulCancel(ctx)
+			}
 			// update bid price
-			newBidPrice := s.midPrice.Mul(fixedpoint.NewFromFloat(0.25)).Add(trade.Price.Mul(fixedpoint.NewFromFloat(0.75)))
+			//bestBid, _ = s.StreamBook.BestBid()
+			newBidPrice := bestBid.Price.Mul(fixedpoint.NewFromFloat(0.999)).Mul(fixedpoint.NewFromFloat(0.25)).Add(trade.Price.Mul(fixedpoint.NewFromFloat(0.75)))
 			log.Infof("long @ %f", newBidPrice.Float64())
-			if trade.Price.Compare(newBidPrice) < 0 {
-				s.placeOrder(context.Background(), types.SideTypeBuy, s.Quantity, s.midPrice.Mul(fixedpoint.NewFromFloat(0.999)), symbol)
-			} else {
-				log.Infof("new")
-				s.placeOrder(context.Background(), types.SideTypeBuy, s.Quantity, newBidPrice.Round(2, 1), symbol)
+			err := s.placeOrder(context.Background(), types.SideTypeBuy, s.Quantity, newBidPrice.Round(2, 2), symbol)
+			if err != nil {
+				newBidPrice = bestBid.Price.Mul(fixedpoint.NewFromFloat(0.998)).Mul(fixedpoint.NewFromFloat(0.25)).Add(trade.Price.Mul(fixedpoint.NewFromFloat(0.75)))
+				log.Infof("long again @ %f", newBidPrice.Float64())
+				_ = s.placeOrder(context.Background(), types.SideTypeBuy, s.Quantity, newBidPrice.Round(2, 2), symbol)
 			}
 		}
 
@@ -86,25 +89,13 @@ func (s *Minute) Bind(session *bbgo.ExchangeSession, orderExecutor *bbgo.General
 
 	}))
 
-	//go func() {
-	//	quoteTicker := time.NewTicker(util.MillisecondsJitter(time.Millisecond*10, 200))
-	//	defer quoteTicker.Stop()
-	//
-	//	for {
-	//		select {
-	//		case <-quoteTicker.C:
-	//			s.updateQuote(context.Background(), symbol)
-	//		}
-	//	}
-	//}()
-
 	if !bbgo.IsBackTesting {
 		session.MarketDataStream.OnMarketTrade(func(trade types.Trade) {
 		})
 	}
 }
 
-func (s *Minute) placeOrder(ctx context.Context, side types.SideType, quantity fixedpoint.Value, price fixedpoint.Value, symbol string) {
+func (s *Minute) placeOrder(ctx context.Context, side types.SideType, quantity fixedpoint.Value, price fixedpoint.Value, symbol string) error {
 	market, _ := s.session.Market(symbol)
 	_, err := s.orderExecutor.SubmitOrders(ctx, types.SubmitOrder{
 		Symbol:   symbol,
@@ -119,4 +110,5 @@ func (s *Minute) placeOrder(ctx context.Context, side types.SideType, quantity f
 	if err != nil {
 		log.WithError(err).Errorf("can not place order")
 	}
+	return err
 }
