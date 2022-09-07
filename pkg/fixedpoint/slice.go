e161f4ec1a5072c266965145eef77c266da04660
c9s <yoanlin93@gmail.com>
2022-09-07 12:35:09 +0800

    fixedpoint: add sort interface support on fixedpoint

--- pkg/fixedpoint/slice.go
+++ pkg/fixedpoint/slice.go
@@ -5,3 +5,20 @@ type Slice []Value
 func (s Slice) Reduce(init Value, reducer Reducer) Value {
 	return Reduce(s, init, reducer)
 }
+
+// Defaults to ascending sort
+func (s Slice) Len() int           { return len(s) }
+func (s Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s Slice) Less(i, j int) bool { return s[i].Compare(s[j]) < 0 }
+
+type Ascending []Value
+
+func (s Ascending) Len() int           { return len(s) }
+func (s Ascending) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s Ascending) Less(i, j int) bool { return s[i].Compare(s[j]) < 0 }
+
+type Descending []Value
+
+func (s Descending) Len() int           { return len(s) }
+func (s Descending) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s Descending) Less(i, j int) bool { return s[i].Compare(s[j]) > 0 }
