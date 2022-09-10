0e02f90c19a959d2f00c0c0dd0b21daa1bface7a
c9s <yoanlin93@gmail.com>
2022-09-11 02:33:32 +0800

    bbgo: notify closing position

--- pkg/bbgo/order_executor_general.go
+++ pkg/bbgo/order_executor_general.go
@@ -257,8 +257,11 @@ func (e *GeneralOrderExecutor) ClosePosition(ctx context.Context, percentage fix
 		return nil
 	}
 
-	log.Infof("closing %s position with tags: %v", e.symbol, tags)
-	submitOrder.Tag = strings.Join(tags, ",")
+	tagStr := strings.Join(tags, ",")
+	submitOrder.Tag = tagStr
+
+	Notify("closing %s position %s with tags: %v", e.symbol, percentage.Percentage(), tagStr)
+	
 	_, err := e.SubmitOrders(ctx, *submitOrder)
 	return err
 }
