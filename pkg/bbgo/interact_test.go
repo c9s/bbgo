8961c940b26716f41675f2f098413dc6e6251156
c9s <yoanlin93@gmail.com>
2022-09-11 03:13:10 +0800

    bbgo: add two test cases for reflect

--- pkg/bbgo/interact_test.go
+++ pkg/bbgo/interact_test.go
@@ -6,18 +6,26 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
 )
 
 type myStrategy struct {
-	Symbol string `json:"symbol"`
+	Symbol   string `json:"symbol"`
+	Position *types.Position
 }
 
-func (m myStrategy) ID() string {
+func (m *myStrategy) ID() string {
 	return "mystrategy"
 }
 
-func (m myStrategy) InstanceID() string {
+func (m *myStrategy) InstanceID() string {
 	return fmt.Sprintf("%s:%s", m.ID(), m.Symbol)
+}
+
+func (m *myStrategy) ClosePosition(ctx context.Context, percentage fixedpoint.Value) error {
+	return nil
 }
 
 func (m *myStrategy) Run(ctx context.Context, orderExecutor OrderExecutor, session *ExchangeSession) error {
@@ -30,4 +38,21 @@ func Test_getStrategySignature(t *testing.T) {
 	})
 	assert.NoError(t, err)
 	assert.Equal(t, "mystrategy:BTCUSDT", signature)
+}
+
+func Test_hasTypeField(t *testing.T) {
+	s := &myStrategy{
+		Symbol: "BTCUSDT",
+	}
+	ok := hasTypeField(s, &types.Position{})
+	assert.True(t, ok)
+}
+
+func Test_testInterface(t *testing.T) {
+	s := &myStrategy{
+		Symbol: "BTCUSDT",
+	}
+
+	ok := testInterface(s, (*PositionCloser)(nil))
+	assert.True(t, ok)
 }
