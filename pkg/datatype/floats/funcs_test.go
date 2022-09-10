c484c1f1765f5e6cf195ce5a6561905686056dcd
c9s <yoanlin93@gmail.com>
2022-09-10 13:31:31 +0800

    reformat code

--- pkg/datatype/floats/funcs_test.go
+++ pkg/datatype/floats/funcs_test.go
@@ -11,7 +11,6 @@ func TestLower(t *testing.T) {
 	assert.Equal(t, []float64{10.0, 11.0}, out)
 }
 
-
 func TestHigher(t *testing.T) {
 	out := Higher([]float64{10.0, 11.0, 12.0, 13.0, 15.0}, 12.0)
 	assert.Equal(t, []float64{13.0, 15.0}, out)
