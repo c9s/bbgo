8bfd1f7f30f0eba78447109803702e5f0d73d664
c9s <yoanlin93@gmail.com>
2022-09-15 11:49:19 +0800

    indicator: refactor pivot function to floats

--- pkg/indicator/pivotlow.go
+++ pkg/indicator/pivotlow.go
@@ -63,43 +63,14 @@ func (inc *PivotLow) PushK(k types.KLine) {
 	inc.EmitUpdate(inc.Last())
 }
 
-func calculatePivotF(values floats.Slice, left, right int, f func(a, pivot float64) bool) (float64, bool) {
-	length := len(values)
-
-	if right == 0 {
-		right = left
-	}
-
-	if length == 0 || length < left+right+1 {
-		return 0.0, false
-	}
-
-	end := length - 1
-	index := end - right
-	val := values[index]
-
-	for i := index - left; i <= index+right; i++ {
-		if i == index {
-			continue
-		}
-
-		// return if we found lower value
-		if !f(values[i], val) {
-			return 0.0, false
-		}
-	}
-
-	return val, true
-}
-
 func calculatePivotHigh(highs floats.Slice, left, right int) (float64, bool) {
-	return calculatePivotF(highs, left, right, func(a, pivot float64) bool {
+	return floats.CalculatePivot(highs, left, right, func(a, pivot float64) bool {
 		return a < pivot
 	})
 }
 
 func calculatePivotLow(lows floats.Slice, left, right int) (float64, bool) {
-	return calculatePivotF(lows, left, right, func(a, pivot float64) bool {
+	return floats.CalculatePivot(lows, left, right, func(a, pivot float64) bool {
 		return a > pivot
 	})
 }
