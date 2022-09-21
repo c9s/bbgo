f9d46bd08b7bd91d7c5924a0a93264621bb672c5
zenix <zenixls2@gmail.com>
2022-09-21 15:14:33 +0900

    fix: extract split string by length as a function

--- pkg/util/string.go
+++ pkg/util/string.go
@@ -1,6 +1,9 @@
 package util
 
-import "strings"
+import (
+	"strings"
+	"unicode/utf8"
+)
 
 func StringSliceContains(slice []string, needle string) bool {
 	for _, s := range slice {
@@ -26,4 +29,18 @@ func MaskKey(key string) string {
 	maskKey += strings.Repeat("*", len(key)-h*2)
 	maskKey += key[len(key)-h:]
 	return maskKey
+}
+
+func StringSplitByLength(s string, length int) (result []string) {
+	var left, right int
+	for left, right = 0, length; right < len(s); left, right = right, right+length {
+		for !utf8.RuneStart(s[right]) {
+			right--
+		}
+		result = append(result, s[left:right])
+	}
+	if len(s)-left > 0 {
+		result = append(result, s[left:])
+	}
+	return result
 }
