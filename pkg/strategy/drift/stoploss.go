5c1d0f95e22f9ccb220d22da79f0d6af015269d1
zenix <zenixls2@gmail.com>
2022-10-03 14:00:18 +0900

    fix/drift_stoploss

--- pkg/strategy/drift/stoploss.go
+++ pkg/strategy/drift/stoploss.go
@@ -1,15 +1,15 @@
 package drift
 
 func (s *Strategy) CheckStopLoss() bool {
-	stoploss := s.StopLoss.Float64()
-	atr := s.atr.Last()
 	if s.UseStopLoss {
+		stoploss := s.StopLoss.Float64()
 		if s.sellPrice > 0 && s.sellPrice*(1.+stoploss) <= s.highestPrice ||
 			s.buyPrice > 0 && s.buyPrice*(1.-stoploss) >= s.lowestPrice {
 			return true
 		}
 	}
 	if s.UseAtr {
+		atr := s.atr.Last()
 		if s.sellPrice > 0 && s.sellPrice+atr <= s.highestPrice ||
 			s.buyPrice > 0 && s.buyPrice-atr >= s.lowestPrice {
 			return true
