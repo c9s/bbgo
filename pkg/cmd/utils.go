b74022e8ee4a7b5ba3bf829a26a29e71400581af
c9s <yoanlin93@gmail.com>
2022-11-24 16:46:40 +0800

    all: remove ftx

--- pkg/cmd/utils.go
+++ pkg/cmd/utils.go
@@ -1,14 +1,9 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/spf13/viper"
-
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	"github.com/c9s/bbgo/pkg/exchange/ftx"
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
 )
@@ -35,17 +30,4 @@ func inBaseAsset(balances types.BalanceMap, market types.Market, price fixedpoin
 	quote := balances[market.QuoteCurrency]
 	base := balances[market.BaseCurrency]
 	return quote.Total().Div(price).Add(base.Total())
-}
-
-func newExchange(session string) (types.Exchange, error) {
-	switch session {
-	case "ftx":
-		return ftx.NewExchange(
-			viper.GetString("ftx-api-key"),
-			viper.GetString("ftx-api-secret"),
-			viper.GetString("ftx-subaccount"),
-		), nil
-
-	}
-	return nil, fmt.Errorf("unsupported session %s", session)
 }
