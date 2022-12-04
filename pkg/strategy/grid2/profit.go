a8fe55c284ddd201db18a5782aec6c75e83ffa97
c9s <yoanlin93@gmail.com>
2022-12-04 15:24:13 +0800

    grid2: push profit into stats

---
+++ pkg/strategy/grid2/profit.go
@@ -0,0 +1,45 @@
+package grid2
+
+import (
+	"time"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+type GridProfit struct {
+	Currency string
+	Profit   fixedpoint.Value
+	Time     time.Time
+}
+
+type GridProfitStats struct {
+	Symbol           string           `json:"symbol"`
+	TotalBaseProfit  fixedpoint.Value `json:"totalBaseProfit"`
+	TotalQuoteProfit fixedpoint.Value `json:"totalQuoteProfit"`
+	FloatProfit      fixedpoint.Value `json:"floatProfit"`
+	GridProfit       fixedpoint.Value `json:"gridProfit"`
+	ArbitrageCount   int              `json:"arbitrageCount"`
+	TotalFee         fixedpoint.Value `json:"totalFee"`
+	Volume           fixedpoint.Value `json:"volume"`
+	Market           types.Market     `json:"market"`
+	ProfitEntries    []*GridProfit    `json:"profitEntries"`
+}
+
+func newGridProfitStats(market types.Market) *GridProfitStats {
+	return &GridProfitStats{
+		Symbol: market.Symbol,
+		Market: market,
+	}
+}
+
+func (s *GridProfitStats) AddProfit(profit *GridProfit) {
+	switch profit.Currency {
+	case s.Market.QuoteCurrency:
+		s.TotalQuoteProfit = s.TotalQuoteProfit.Add(profit.Profit)
+	case s.Market.BaseCurrency:
+		s.TotalBaseProfit = s.TotalBaseProfit.Add(profit.Profit)
+	}
+
+	s.ProfitEntries = append(s.ProfitEntries, profit)
+}
