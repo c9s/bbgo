75caa6565eee3c52bcce9ac0044b0b4bb1174a88
zenix <zenixls2@gmail.com>
2022-12-19 18:11:08 +0900

    feature: add sar indicator

--- pkg/indicator/psar.go
+++ pkg/indicator/psar.go
@@ -1,12 +1,14 @@
 package indicator
 
 import (
+	"math"
 	"time"
 
+	"github.com/c9s/bbgo/pkg/datatype/floats"
 	"github.com/c9s/bbgo/pkg/types"
 )
 
-// Parabolic SAR(Stop and Reverse)
+// Parabolic SAR(Stop and Reverse) / SAR
 // Refer: https://www.investopedia.com/terms/p/parabolicindicator.asp
 // The parabolic SAR indicator, developed by J. Wells Wilder, is used by traders to determine
 // trend direction and potential reversals in price. The indicator uses a trailing stop and
@@ -21,63 +23,92 @@ import (
 type PSAR struct {
 	types.SeriesBase
 	types.IntervalWindow
-	Input *types.Queue
-	Value *types.Queue // Stop and Reverse
-	AF    float64      // Acceleration Factor
+	High    *types.Queue
+	Low     *types.Queue
+	Values  floats.Slice // Stop and Reverse
+	AF      float64      // Acceleration Factor
+	EP      float64
+	Falling bool
 
 	EndTime         time.Time
 	UpdateCallbacks []func(value float64)
 }
 
 func (inc *PSAR) Last() float64 {
-	if inc.Value == nil {
+	if len(inc.Values) == 0 {
 		return 0
 	}
-	return inc.Value.Last()
+	return inc.Values.Last()
 }
 
 func (inc *PSAR) Length() int {
-	if inc.Value == nil {
-		return 0
-	}
-	return inc.Value.Length()
+	return len(inc.Values)
 }
 
-func (inc *PSAR) Update(value float64) {
-	if inc.Input == nil {
+func (inc *PSAR) falling() bool {
+	up := inc.High.Last() - inc.High.Index(1)
+	dn := inc.Low.Index(1) - inc.Low.Last()
+	return (dn > up) && (dn > 0)
+}
+
+func (inc *PSAR) Update(high, low float64) {
+	if inc.High == nil {
 		inc.SeriesBase.Series = inc
-		inc.Input = types.NewQueue(inc.Window)
-		inc.Value = types.NewQueue(inc.Window)
+		inc.High = types.NewQueue(inc.Window)
+		inc.Low = types.NewQueue(inc.Window)
+		inc.Values = floats.Slice{}
 		inc.AF = 0.02
+		inc.High.Update(high)
+		inc.Low.Update(low)
+		return
 	}
-	inc.Input.Update(value)
-	if inc.Input.Length() == inc.Window {
-		pprev := inc.Value.Index(1)
-		ppsar := inc.Value.Last()
-		if value > ppsar { // rising formula
-			high := inc.Input.Highest(inc.Window)
-			inc.Value.Update(ppsar + inc.AF*(high-ppsar))
-			if high == value {
-				inc.AF += 0.02
-				if inc.AF > 0.2 {
-					inc.AF = 0.2
+	isFirst := inc.High.Length() < inc.Window
+	inc.High.Update(high)
+	inc.Low.Update(low)
+	if !isFirst {
+		ppsar := inc.Values.Last()
+		if inc.Falling { // falling formula
+			psar := ppsar - inc.AF*(ppsar-inc.EP)
+			h := inc.High.Shift(1).Highest(2)
+			inc.Values.Push(math.Max(psar, h))
+			if low < inc.EP {
+				inc.EP = low
+				if inc.AF <= 0.18 {
+					inc.AF += 0.02
 				}
 			}
-			if pprev > ppsar { // reverse
+			if high > psar { // reverse
 				inc.AF = 0.02
+				inc.Values[len(inc.Values)-1] = inc.EP
+				inc.EP = high
+				inc.Falling = false
 			}
-		} else { // falling formula
-			low := inc.Input.Lowest(inc.Window)
-			inc.Value.Update(ppsar - inc.AF*(ppsar-low))
-			if low == value {
-				inc.AF += 0.02
-				if inc.AF > 0.2 {
-					inc.AF = 0.2
+		} else { // rising formula
+			psar := ppsar + inc.AF*(inc.EP-ppsar)
+			l := inc.Low.Shift(1).Lowest(2)
+			inc.Values.Push(math.Min(psar, l))
+			if high > inc.EP {
+				inc.EP = high
+				if inc.AF <= 0.18 {
+					inc.AF += 0.02
 				}
 			}
-			if pprev < ppsar { // reverse
+			if low < psar { // reverse
 				inc.AF = 0.02
+				inc.Values[len(inc.Values)-1] = inc.EP
+				inc.EP = low
+				inc.Falling = true
 			}
+
+		}
+	} else {
+		inc.Falling = inc.falling()
+		if inc.Falling {
+			inc.Values.Push(inc.High.Index(1))
+			inc.EP = inc.Low.Index(1)
+		} else {
+			inc.Values.Push(inc.Low.Index(1))
+			inc.EP = inc.High.Index(1)
 		}
 	}
 }
@@ -89,7 +120,7 @@ func (inc *PSAR) CalculateAndUpdate(kLines []types.KLine) {
 		if inc.EndTime != zeroTime && !k.EndTime.After(inc.EndTime) {
 			continue
 		}
-		inc.Update(k.Close.Float64())
+		inc.Update(k.High.Float64(), k.Low.Float64())
 	}
 
 	inc.EmitUpdate(inc.Last())
---
+++ pkg/indicator/psar_test.go
@@ -0,0 +1,41 @@
+package indicator
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+import pandas as pd
+import pandas_ta as ta
+
+data = pd.Series([0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9])
+out = ta.psar(data, data)
+print(out)
+*/
+
+func Test_PSAR(t *testing.T) {
+	var randomPrices = []byte(`[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]`)
+	var input []fixedpoint.Value
+	if err := json.Unmarshal(randomPrices, &input); err != nil {
+		panic(err)
+	}
+	psar := PSAR{
+		IntervalWindow: types.IntervalWindow{Window: 2},
+	}
+	var klines []types.KLine
+	for _, v := range input {
+		klines = append(klines, types.KLine{
+			High: v,
+			Low:  v,
+		})
+	}
+	psar.CalculateAndUpdate(klines)
+	assert.Equal(t, psar.Length(), 29)
+	assert.Equal(t, psar.AF, 0.04)
+	assert.Equal(t, psar.Last(), 0.16)
+}
