28beca18e177a8a8128493c70e1d36aa2dd9c821
c9s <yoanlin93@gmail.com>
2022-12-23 13:08:06 +0800

    bump version to v1.43.0

--- pkg/version/dev.go
+++ pkg/version/dev.go
@@ -3,6 +3,6 @@
 
 package version
 
-const Version = "v1.42.0-7204e255-dev"
+const Version = "v1.43.0-8bcfb78b-dev"
 
-const VersionGitRef = "7204e255"
+const VersionGitRef = "8bcfb78b"
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,6 +3,6 @@
 
 package version
 
-const Version = "v1.42.0-7204e255"
+const Version = "v1.43.0-8bcfb78b"
 
-const VersionGitRef = "7204e255"
+const VersionGitRef = "8bcfb78b"
