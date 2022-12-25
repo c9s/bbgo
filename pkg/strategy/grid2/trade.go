fb9806e12cb7cfcdbb06ee7631a37412745009ee
c9s <yoanlin93@gmail.com>
2022-12-26 01:04:17 +0800

    grid2: split more files

---
+++ pkg/strategy/grid2/trade.go
@@ -0,0 +1,27 @@
+package grid2
+
+import (
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+// collectTradeFee collects the fee from the given trade slice
+func collectTradeFee(trades []types.Trade) map[string]fixedpoint.Value {
+	fees := make(map[string]fixedpoint.Value)
+	for _, t := range trades {
+		if fee, ok := fees[t.FeeCurrency]; ok {
+			fees[t.FeeCurrency] = fee.Add(t.Fee)
+		} else {
+			fees[t.FeeCurrency] = t.Fee
+		}
+	}
+	return fees
+}
+
+func aggregateTradesQuantity(trades []types.Trade) fixedpoint.Value {
+	tq := fixedpoint.Zero
+	for _, t := range trades {
+		tq = tq.Add(t.Quantity)
+	}
+	return tq
+}
