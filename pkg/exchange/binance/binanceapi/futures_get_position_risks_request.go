b41f8a8355113681a59ac558ffc9b2a61e67c334
c9s <yoanlin93@gmail.com>
2023-03-24 22:35:22 +0800

    binance: add FuturesGetIncomeHistoryRequest api support

--- pkg/exchange/binance/binanceapi/futures_get_position_risks_request.go
+++ pkg/exchange/binance/binanceapi/futures_get_position_risks_request.go
@@ -2,10 +2,25 @@ package binanceapi
 
 import "github.com/c9s/requestgen"
 
-type FuturesPositionRisksResponse struct {
+type FuturesPositionRisk struct {
+	EntryPrice       string `json:"entryPrice"`
+	MarginType       string `json:"marginType"`
+	IsAutoAddMargin  string `json:"isAutoAddMargin"`
+	IsolatedMargin   string `json:"isolatedMargin"`
+	Leverage         string `json:"leverage"`
+	LiquidationPrice string `json:"liquidationPrice"`
+	MarkPrice        string `json:"markPrice"`
+	MaxNotionalValue string `json:"maxNotionalValue"`
+	PositionAmt      string `json:"positionAmt"`
+	Notional         string `json:"notional"`
+	IsolatedWallet   string `json:"isolatedWallet"`
+	Symbol           string `json:"symbol"`
+	UnRealizedProfit string `json:"unRealizedProfit"`
+	PositionSide     string `json:"positionSide"`
+	UpdateTime       int    `json:"updateTime"`
 }
 
-//go:generate requestgen -method GET -url "/fapi/v2/positionRisk" -type FuturesGetPositionRisksRequest -responseType .FuturesPositionRisksResponse
+//go:generate requestgen -method GET -url "/fapi/v2/positionRisk" -type FuturesGetPositionRisksRequest -responseType []FuturesPositionRisk
 type FuturesGetPositionRisksRequest struct {
 	client requestgen.AuthenticatedAPIClient
 
