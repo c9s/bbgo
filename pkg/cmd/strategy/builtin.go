545f138760e917233c05ea8e1696384f224a8a55
Austin Liu <austin362667@gmail.com>
2023-03-24 15:36:48 +0800

    strategy: add neutral maker

--- pkg/cmd/strategy/builtin.go
+++ pkg/cmd/strategy/builtin.go
@@ -23,6 +23,7 @@ import (
 	_ "github.com/c9s/bbgo/pkg/strategy/kline"
 	_ "github.com/c9s/bbgo/pkg/strategy/linregmaker"
 	_ "github.com/c9s/bbgo/pkg/strategy/marketcap"
+	_ "github.com/c9s/bbgo/pkg/strategy/neutralmaker"
 	_ "github.com/c9s/bbgo/pkg/strategy/pivotshort"
 	_ "github.com/c9s/bbgo/pkg/strategy/pricealert"
 	_ "github.com/c9s/bbgo/pkg/strategy/pricedrop"
