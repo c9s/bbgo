a6b47fda7290026fc988afcf1ed33ae5418204ec
c9s <yoanlin93@gmail.com>
2023-03-26 02:12:00 +0800

    xfunding: check funding fee and record txn id

--- pkg/strategy/xfunding/profitstats.go
+++ pkg/strategy/xfunding/profitstats.go
@@ -10,6 +10,7 @@ import (
 type FundingFee struct {
 	Asset  string           `json:"asset"`
 	Amount fixedpoint.Value `json:"amount"`
+	Txn    int64            `json:"txn"`
 }
 
 type ProfitStats struct {
@@ -18,15 +19,18 @@ type ProfitStats struct {
 	FundingFeeCurrency string           `json:"fundingFeeCurrency"`
 	TotalFundingFee    fixedpoint.Value `json:"totalFundingFee"`
 	FundingFeeRecords  []FundingFee     `json:"fundingFeeRecords"`
+	LastFundingFeeTxn  int64            `json:"lastFundingFeeTxn"`
 }
 
 func (s *ProfitStats) AddFundingFee(fee FundingFee) error {
-	s.FundingFeeRecords = append(s.FundingFeeRecords, fee)
-	s.TotalFundingFee = s.TotalFundingFee.Add(fee.Amount)
 	if s.FundingFeeCurrency == "" {
 		s.FundingFeeCurrency = fee.Asset
 	} else if s.FundingFeeCurrency != fee.Asset {
 		return fmt.Errorf("unexpected error, funding fee currency is not matched, given: %s, wanted: %s", fee.Asset, s.FundingFeeCurrency)
 	}
+
+	s.FundingFeeRecords = append(s.FundingFeeRecords, fee)
+	s.TotalFundingFee = s.TotalFundingFee.Add(fee.Amount)
+	s.LastFundingFeeTxn = fee.Txn
 	return nil
 }
