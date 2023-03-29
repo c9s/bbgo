aa6feed272ae543b751eeacf9ad2ebf2cf8de0e7
c9s <yoanlin93@gmail.com>
2023-03-29 17:08:34 +0800

    binance: add FuturesChangeInitialLeverageRequest api

---
+++ pkg/exchange/binance/binanceapi/futures_change_initial_leverage_request.go
@@ -0,0 +1,25 @@
+package binanceapi
+
+import (
+	"github.com/c9s/requestgen"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+)
+
+type FuturesChangeInitialLeverageResponse struct {
+	Leverage         int              `json:"leverage"`
+	MaxNotionalValue fixedpoint.Value `json:"maxNotionalValue"`
+	Symbol           string           `json:"symbol"`
+}
+
+//go:generate requestgen -method POST -url "/fapi/v1/multiAssetsMargin" -type FuturesChangeInitialLeverageRequest -responseType FuturesChangeInitialLeverageResponse
+type FuturesChangeInitialLeverageRequest struct {
+	client requestgen.AuthenticatedAPIClient
+
+	symbol   string `param:"symbol"`
+	leverage int    `param:"leverage"`
+}
+
+func (c *FuturesRestClient) NewFuturesChangeInitialLeverageRequest() *FuturesChangeInitialLeverageRequest {
+	return &FuturesChangeInitialLeverageRequest{client: c}
+}
