40f6295d9125a638605fc1d30ea24831e7604eaf
c9s <yoanlin93@gmail.com>
2023-05-04 17:18:42 +0800

    maxapi: move GetMarginInterestRatesRequest api to a file

--- pkg/exchange/max/maxapi/v3/margin.go
+++ pkg/exchange/max/maxapi/v3/margin.go
@@ -15,24 +15,8 @@ type MarginService struct {
 	Client *maxapi.RestClient
 }
 
-func (s *Client) NewGetMarginInterestRatesRequest() *GetMarginInterestRatesRequest {
-	return &GetMarginInterestRatesRequest{client: s.Client}
-}
-
 func (s *Client) NewGetMarginBorrowingLimitsRequest() *GetMarginBorrowingLimitsRequest {
 	return &GetMarginBorrowingLimitsRequest{client: s.Client}
-}
-
-type MarginInterestRate struct {
-	HourlyInterestRate     fixedpoint.Value `json:"hourly_interest_rate"`
-	NextHourlyInterestRate fixedpoint.Value `json:"next_hourly_interest_rate"`
-}
-
-type MarginInterestRateMap map[string]MarginInterestRate
-
-//go:generate GetRequest -url "/api/v3/wallet/m/interest_rates" -type GetMarginInterestRatesRequest -responseType .MarginInterestRateMap
-type GetMarginInterestRatesRequest struct {
-	client requestgen.APIClient
 }
 
 type MarginBorrowingLimitMap map[string]fixedpoint.Value
