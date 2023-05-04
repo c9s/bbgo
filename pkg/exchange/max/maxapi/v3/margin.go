db3bc04ac45a40740566c2f33175d5c0869c16ed
c9s <yoanlin93@gmail.com>
2023-05-04 16:38:20 +0800

    maxapi: fix margin interest history api

--- pkg/exchange/max/maxapi/v3/margin.go
+++ pkg/exchange/max/maxapi/v3/margin.go
@@ -5,13 +5,10 @@ package v3
 //go:generate -command DeleteRequest requestgen -method DELETE
 
 import (
-	"time"
-
 	"github.com/c9s/requestgen"
 
 	maxapi "github.com/c9s/bbgo/pkg/exchange/max/maxapi"
 	"github.com/c9s/bbgo/pkg/fixedpoint"
-	"github.com/c9s/bbgo/pkg/types"
 )
 
 type MarginService struct {
@@ -24,10 +21,6 @@ func (s *Client) NewGetMarginInterestRatesRequest() *GetMarginInterestRatesReque
 
 func (s *Client) NewGetMarginBorrowingLimitsRequest() *GetMarginBorrowingLimitsRequest {
 	return &GetMarginBorrowingLimitsRequest{client: s.Client}
-}
-
-func (s *Client) NewGetMarginInterestHistoryRequest(currency string) *GetMarginInterestHistoryRequest {
-	return &GetMarginInterestHistoryRequest{client: s.Client, currency: currency}
 }
 
 type MarginInterestRate struct {
@@ -47,21 +40,4 @@ type MarginBorrowingLimitMap map[string]fixedpoint.Value
 //go:generate GetRequest -url "/api/v3/wallet/m/limits" -type GetMarginBorrowingLimitsRequest -responseType .MarginBorrowingLimitMap
 type GetMarginBorrowingLimitsRequest struct {
 	client requestgen.APIClient
-}
-
-type MarginInterestRecord struct {
-	Currency     string                     `json:"currency"`
-	Amount       fixedpoint.Value           `json:"amount"`
-	InterestRate fixedpoint.Value           `json:"interest_rate"`
-	CreatedAt    types.MillisecondTimestamp `json:"created_at"`
-}
-
-//go:generate GetRequest -url "/api/v3/wallet/m/interests/history/:currency" -type GetMarginInterestHistoryRequest -responseType []MarginInterestRecord
-type GetMarginInterestHistoryRequest struct {
-	client requestgen.AuthenticatedAPIClient
-
-	currency  string     `param:"currency,slug,required"`
-	startTime *time.Time `param:"startTime,milliseconds"`
-	endTime   *time.Time `param:"endTime,milliseconds"`
-	limit     *int       `param:"limit"`
 }
