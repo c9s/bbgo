4996eb218c8e80dc1d0bb9c07684a324b7fd6086
c9s <yoanlin93@gmail.com>
2023-05-25 22:18:54 +0800

    indicator: move doc

--- pkg/indicator/macd2.go
+++ pkg/indicator/macd2.go
@@ -5,6 +5,24 @@ import (
 	"github.com/c9s/bbgo/pkg/types"
 )
 
+/*
+NEW INDICATOR DESIGN:
+
+klines := kLines(marketDataStream)
+closePrices := closePrices(klines)
+macd := MACD(klines, {Fast: 12, Slow: 10})
+
+equals to:
+
+klines := KLines(marketDataStream)
+closePrices := ClosePrice(klines)
+fastEMA := EMA(closePrices, 7)
+slowEMA := EMA(closePrices, 25)
+macd := Subtract(fastEMA, slowEMA)
+signal := EMA(macd, 16)
+histogram := Subtract(macd, signal)
+*/
+
 //go:generate callbackgen -type KLineStream
 type KLineStream struct {
 	updateCallbacks []func(k types.KLine)
