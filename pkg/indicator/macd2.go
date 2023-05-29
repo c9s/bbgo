68570e1eeb80694402ef53f5044f869ad55de42a
c9s <yoanlin93@gmail.com>
2023-05-29 21:42:22 +0800

    indicator: move EWMA2 to ewma2.go

--- pkg/indicator/macd2.go
+++ pkg/indicator/macd2.go
@@ -1,7 +1,6 @@
 package indicator
 
 import (
-	"github.com/c9s/bbgo/pkg/datatype/floats"
 	"github.com/c9s/bbgo/pkg/types"
 )
 
@@ -31,44 +30,4 @@ type Float64Source interface {
 type Float64Subscription interface {
 	types.Series
 	AddSubscriber(f func(v float64))
-}
-
-//go:generate callbackgen -type EWMAStream
-type EWMAStream struct {
-	Float64Updater
-	types.SeriesBase
-
-	slice floats.Slice
-
-	window     int
-	multiplier float64
-}
-
-func EWMA2(source Float64Source, window int) *EWMAStream {
-	s := &EWMAStream{
-		window:     window,
-		multiplier: 2.0 / float64(1+window),
-	}
-
-	s.SeriesBase.Series = s.slice
-
-	if sub, ok := source.(Float64Subscription); ok {
-		sub.AddSubscriber(s.calculateAndPush)
-	} else {
-		source.OnUpdate(s.calculateAndPush)
-	}
-
-	return s
-}
-
-func (s *EWMAStream) calculateAndPush(v float64) {
-	v2 := s.calculate(v)
-	s.slice.Push(v2)
-	s.EmitUpdate(v2)
-}
-
-func (s *EWMAStream) calculate(v float64) float64 {
-	last := s.slice.Last()
-	m := s.multiplier
-	return (1.0-m)*last + m*v
 }
