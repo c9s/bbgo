52420956d21bacd0d03b44aec9812ee25f49f111
c9s <yoanlin93@gmail.com>
2023-05-29 17:12:34 +0800

    floats: add floats LSM

--- pkg/datatype/floats/funcs_test.go
+++ pkg/datatype/floats/funcs_test.go
@@ -15,3 +15,9 @@ func TestHigher(t *testing.T) {
 	out := Higher([]float64{10.0, 11.0, 12.0, 13.0, 15.0}, 12.0)
 	assert.Equal(t, []float64{13.0, 15.0}, out)
 }
+
+func TestLSM(t *testing.T) {
+	slice := Slice{1., 2., 3., 4.}
+	slope := LSM(slice)
+	assert.Equal(t, 1.0, slope)
+}
--- pkg/datatype/floats/slice.go
+++ pkg/datatype/floats/slice.go
@@ -187,3 +187,30 @@ func (s Slice) Index(i int) float64 {
 func (s Slice) Length() int {
 	return len(s)
 }
+
+func (s Slice) LSM() float64 {
+	return LSM(s)
+}
+
+func LSM(values Slice) float64 {
+	var sumX, sumY, sumXSqr, sumXY = .0, .0, .0, .0
+
+	end := len(values) - 1
+	for i := end; i >= 0; i-- {
+		val := values[i]
+		per := float64(end - i + 1)
+		sumX += per
+		sumY += val
+		sumXSqr += per * per
+		sumXY += val * per
+	}
+
+	length := float64(len(values))
+	slope := (length*sumXY - sumX*sumY) / (length*sumXSqr - sumX*sumX)
+
+	average := sumY / length
+	tail := average - slope*sumX/length + slope
+	head := tail + slope*(length-1)
+	slope2 := (tail - head) / (length - 1)
+	return slope2
+}
