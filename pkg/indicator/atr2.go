ebf9c43cd589b923db1b7fa4f14411649229846e
c9s <yoanlin93@gmail.com>
2023-05-30 13:46:51 +0800

    indicator: separate TR + RMA and ATR = TR + RMA

--- pkg/indicator/atr2.go
+++ pkg/indicator/atr2.go
@@ -1,47 +1,13 @@
 package indicator
 
-import (
-	"math"
-
-	"github.com/c9s/bbgo/pkg/types"
-)
-
-// This ATRStream calculates the ATR first
 type ATRStream struct {
 	// embedded struct
-	Float64Series
-
-	// private states
-	previousClose float64
+	*RMAStream
 }
 
-func ATR2(source KLineSubscription) *ATRStream {
-	s := &ATRStream{
-		Float64Series: NewFloat64Series(),
-	}
-
-	source.AddSubscriber(func(k types.KLine) {
-		s.calculateAndPush(k.High.Float64(), k.Low.Float64(), k.Close.Float64())
-	})
+func ATR2(source KLineSubscription, window int) *ATRStream {
+	s := &ATRStream{}
+	tr := TR2(source)
+	s.RMAStream = RMA2(tr, window, true)
 	return s
-}
-
-func (s *ATRStream) calculateAndPush(high, low, cls float64) {
-	if s.previousClose == .0 {
-		s.previousClose = cls
-		return
-	}
-
-	trueRange := high - low
-	hc := math.Abs(high - s.previousClose)
-	lc := math.Abs(low - s.previousClose)
-	if trueRange < hc {
-		trueRange = hc
-	}
-	if trueRange < lc {
-		trueRange = lc
-	}
-
-	s.previousClose = cls
-	s.EmitUpdate(trueRange)
 }
