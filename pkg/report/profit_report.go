747f60b0d52b7ad0151803c19cfbbdef16826dd7
Andy Cheng <andy.cheng@richsoda.com>
2023-06-16 18:06:47 +0800

    feature/profitTracker: integrate profit report with profit tracker

--- pkg/report/profit_report.go
+++ pkg/report/profit_report.go
@@ -2,7 +2,6 @@ package report
 
 import (
 	"fmt"
-	"github.com/c9s/bbgo/pkg/bbgo"
 	"github.com/c9s/bbgo/pkg/data/tsv"
 	"github.com/c9s/bbgo/pkg/datatype/floats"
 	"github.com/c9s/bbgo/pkg/fixedpoint"
@@ -12,134 +11,104 @@ import (
 
 // AccumulatedProfitReport For accumulated profit report output
 type AccumulatedProfitReport struct {
-	// AccumulatedProfitMAWindow Accumulated profit SMA window, in number of trades
-	AccumulatedProfitMAWindow int `json:"accumulatedProfitMAWindow"`
+	// ProfitMAWindow Accumulated profit SMA window
+	ProfitMAWindow int `json:"ProfitMAWindow"`
 
-	// IntervalWindow interval window, in days
-	IntervalWindow int `json:"intervalWindow"`
-
-	// NumberOfInterval How many intervals to output to TSV
-	NumberOfInterval int `json:"NumberOfInterval"`
+	// ShortTermProfitWindow The window to sum up the short-term profit
+	ShortTermProfitWindow int `json:"shortTermProfitWindow"`
 
 	// TsvReportPath The path to output report to
 	TsvReportPath string `json:"tsvReportPath"`
 
-	// AccumulatedDailyProfitWindow The window to sum up the daily profit, in days
-	AccumulatedDailyProfitWindow int `json:"accumulatedDailyProfitWindow"`
+	symbol string
 
-	Symbol string
+	types.IntervalWindow
 
 	// Accumulated profit
-	accumulatedProfit         fixedpoint.Value
-	accumulatedProfitPerDay   floats.Slice
-	previousAccumulatedProfit fixedpoint.Value
+	accumulatedProfit            fixedpoint.Value
+	accumulatedProfitPerInterval floats.Slice
 
 	// Accumulated profit MA
-	accumulatedProfitMA       *indicator.SMA
-	accumulatedProfitMAPerDay floats.Slice
+	profitMA            *indicator.SMA
+	profitMAPerInterval floats.Slice
 
-	// Daily profit
-	dailyProfit floats.Slice
+	// Profit of each interval
+	ProfitPerInterval floats.Slice
 
 	// Accumulated fee
-	accumulatedFee       fixedpoint.Value
-	accumulatedFeePerDay floats.Slice
+	accumulatedFee            fixedpoint.Value
+	accumulatedFeePerInterval floats.Slice
 
 	// Win ratio
-	winRatioPerDay floats.Slice
+	winRatioPerInterval floats.Slice
 
 	// Profit factor
-	profitFactorPerDay floats.Slice
+	profitFactorPerInterval floats.Slice
 
 	// Trade number
-	dailyTrades               floats.Slice
-	accumulatedTrades         int
-	previousAccumulatedTrades int
+	accumulatedTrades            int
+	accumulatedTradesPerInterval floats.Slice
 
 	// Extra values
-	extraValues [][2]string
+	strategyParameters [][2]string
 }
 
-func (r *AccumulatedProfitReport) Initialize(Symbol string, session *bbgo.ExchangeSession, orderExecutor *bbgo.GeneralOrderExecutor, TradeStats *types.TradeStats) {
-	r.Symbol = Symbol
+func (r *AccumulatedProfitReport) Initialize(symbol string, interval types.Interval, window int) {
+	r.symbol = symbol
+	r.Interval = interval
+	r.Window = window
 
-	if r.AccumulatedProfitMAWindow <= 0 {
-		r.AccumulatedProfitMAWindow = 60
+	if r.ProfitMAWindow <= 0 {
+		r.ProfitMAWindow = 60
 	}
-	if r.IntervalWindow <= 0 {
-		r.IntervalWindow = 7
+
+	if r.Window <= 0 {
+		r.Window = 7
 	}
-	if r.AccumulatedDailyProfitWindow <= 0 {
-		r.AccumulatedDailyProfitWindow = 7
+
+	if r.ShortTermProfitWindow <= 0 {
+		r.ShortTermProfitWindow = 7
 	}
-	if r.NumberOfInterval <= 0 {
-		r.NumberOfInterval = 1
-	}
-	r.accumulatedProfitMA = &indicator.SMA{IntervalWindow: types.IntervalWindow{Interval: types.Interval1d, Window: r.AccumulatedProfitMAWindow}}
 
-	session.Subscribe(types.KLineChannel, r.Symbol, types.SubscribeOptions{Interval: types.Interval1d})
-
-	// Record profit
-	orderExecutor.TradeCollector().OnProfit(func(trade types.Trade, profit *types.Profit) {
-		if profit == nil {
-			return
-		}
-
-		r.RecordProfit(profit.Profit)
-	})
-
-	// Record trade
-	orderExecutor.TradeCollector().OnTrade(func(trade types.Trade, profit fixedpoint.Value, netProfit fixedpoint.Value) {
-		r.RecordTrade(trade.Fee)
-	})
-
-	// Record daily status
-	session.MarketDataStream.OnKLineClosed(types.KLineWith(r.Symbol, types.Interval1d, func(kline types.KLine) {
-		r.DailyUpdate(TradeStats)
-	}))
+	r.profitMA = &indicator.SMA{IntervalWindow: types.IntervalWindow{Interval: r.Interval, Window: r.ProfitMAWindow}}
 }
 
-func (r *AccumulatedProfitReport) AddExtraValue(valueAndTitle [2]string) {
-	r.extraValues = append(r.extraValues, valueAndTitle)
+func (r *AccumulatedProfitReport) AddStrategyParameter(title string, value string) {
+	r.strategyParameters = append(r.strategyParameters, [2]string{title, value})
 }
 
-func (r *AccumulatedProfitReport) RecordProfit(profit fixedpoint.Value) {
-	r.accumulatedProfit = r.accumulatedProfit.Add(profit)
-}
-
-func (r *AccumulatedProfitReport) RecordTrade(fee fixedpoint.Value) {
-	r.accumulatedFee = r.accumulatedFee.Add(fee)
+func (r *AccumulatedProfitReport) AddTrade(trade types.Trade) {
+	r.accumulatedFee = r.accumulatedFee.Add(trade.Fee)
 	r.accumulatedTrades += 1
 }
 
-func (r *AccumulatedProfitReport) DailyUpdate(tradeStats *types.TradeStats) {
-	// Daily profit
-	r.dailyProfit.Update(r.accumulatedProfit.Sub(r.previousAccumulatedProfit).Float64())
-	r.previousAccumulatedProfit = r.accumulatedProfit
-
+func (r *AccumulatedProfitReport) Rotate(ps *types.ProfitStats, ts *types.TradeStats) {
 	// Accumulated profit
-	r.accumulatedProfitPerDay.Update(r.accumulatedProfit.Float64())
+	r.accumulatedProfit.Add(ps.AccumulatedNetProfit)
+	r.accumulatedProfitPerInterval.Update(r.accumulatedProfit.Float64())
 
-	// Accumulated profit MA
-	r.accumulatedProfitMA.Update(r.accumulatedProfit.Float64())
-	r.accumulatedProfitMAPerDay.Update(r.accumulatedProfitMA.Last(0))
+	// Profit of each interval
+	r.ProfitPerInterval.Update(ps.AccumulatedNetProfit.Float64())
+
+	// Profit MA
+	r.profitMA.Update(r.accumulatedProfit.Float64())
+	r.profitMAPerInterval.Update(r.profitMA.Last(0))
 
 	// Accumulated Fee
-	r.accumulatedFeePerDay.Update(r.accumulatedFee.Float64())
+	r.accumulatedFeePerInterval.Update(r.accumulatedFee.Float64())
+
+	// Trades
+	r.accumulatedTradesPerInterval.Update(float64(r.accumulatedTrades))
 
 	// Win ratio
-	r.winRatioPerDay.Update(tradeStats.WinningRatio.Float64())
+	r.winRatioPerInterval.Update(ts.WinningRatio.Float64())
 
 	// Profit factor
-	r.profitFactorPerDay.Update(tradeStats.ProfitFactor.Float64())
-
-	// Daily trades
-	r.dailyTrades.Update(float64(r.accumulatedTrades - r.previousAccumulatedTrades))
-	r.previousAccumulatedTrades = r.accumulatedTrades
+	r.profitFactorPerInterval.Update(ts.ProfitFactor.Float64())
 }
 
 // Output Accumulated profit report to a TSV file
-func (r *AccumulatedProfitReport) Output(symbol string) {
+func (r *AccumulatedProfitReport) Output() {
 	if r.TsvReportPath != "" {
 		tsvwiter, err := tsv.AppendWriterFile(r.TsvReportPath)
 		if err != nil {
@@ -150,46 +119,34 @@ func (r *AccumulatedProfitReport) Output(symbol string) {
 		titles := []string{
 			"#",
 			"Symbol",
-			"accumulatedProfit",
-			"accumulatedProfitMA",
-			fmt.Sprintf("%dd profit", r.AccumulatedDailyProfitWindow),
+			"Total Net Profit",
+			fmt.Sprintf("Total Net Profit %sMA%d", r.Interval, r.Window),
+			fmt.Sprintf("%s %d Net Profit", r.Interval, r.ShortTermProfitWindow),
 			"accumulatedFee",
-			"accumulatedNetProfit",
 			"winRatio",
 			"profitFactor",
-			"60D trades",
+			fmt.Sprintf("%s %d Trades", r.Interval, r.Window),
 		}
-		for i := 0; i < len(r.extraValues); i++ {
-			titles = append(titles, r.extraValues[i][0])
+		for i := 0; i < len(r.strategyParameters); i++ {
+			titles = append(titles, r.strategyParameters[i][0])
 		}
 		_ = tsvwiter.Write(titles)
 
 		// Output data row
-		for i := 0; i <= r.NumberOfInterval-1; i++ {
-			accumulatedProfit := r.accumulatedProfitPerDay.Index(r.IntervalWindow * i)
-			accumulatedProfitStr := fmt.Sprintf("%f", accumulatedProfit)
-			accumulatedProfitMA := r.accumulatedProfitMAPerDay.Index(r.IntervalWindow * i)
-			accumulatedProfitMAStr := fmt.Sprintf("%f", accumulatedProfitMA)
-			intervalAccumulatedProfit := r.dailyProfit.Tail(r.AccumulatedDailyProfitWindow+r.IntervalWindow*i).Sum() - r.dailyProfit.Tail(r.IntervalWindow*i).Sum()
-			intervalAccumulatedProfitStr := fmt.Sprintf("%f", intervalAccumulatedProfit)
-			accumulatedFee := fmt.Sprintf("%f", r.accumulatedFeePerDay.Index(r.IntervalWindow*i))
-			accumulatedNetProfit := fmt.Sprintf("%f", accumulatedProfit-r.accumulatedFeePerDay.Index(r.IntervalWindow*i))
-			winRatio := fmt.Sprintf("%f", r.winRatioPerDay.Index(r.IntervalWindow*i))
-			profitFactor := fmt.Sprintf("%f", r.profitFactorPerDay.Index(r.IntervalWindow*i))
-			trades := r.dailyTrades.Tail(60+r.IntervalWindow*i).Sum() - r.dailyTrades.Tail(r.IntervalWindow*i).Sum()
-			tradesStr := fmt.Sprintf("%f", trades)
+		for i := 0; i <= r.Window-1; i++ {
 			values := []string{
 				fmt.Sprintf("%d", i+1),
-				symbol, accumulatedProfitStr,
-				accumulatedProfitMAStr,
-				intervalAccumulatedProfitStr,
-				accumulatedFee,
-				accumulatedNetProfit,
-				winRatio, profitFactor,
-				tradesStr,
+				r.symbol,
+				fmt.Sprintf("%f", r.accumulatedProfitPerInterval.Last(i)),
+				fmt.Sprintf("%f", r.profitMAPerInterval.Last(i)),
+				fmt.Sprintf("%f", r.accumulatedProfitPerInterval.Last(i)-r.accumulatedProfitPerInterval.Last(i+r.ShortTermProfitWindow)),
+				fmt.Sprintf("%f", r.accumulatedFeePerInterval.Last(i)),
+				fmt.Sprintf("%f", r.winRatioPerInterval.Last(i)),
+				fmt.Sprintf("%f", r.profitFactorPerInterval.Last(i)),
+				fmt.Sprintf("%f", r.accumulatedTradesPerInterval.Last(i)),
 			}
-			for j := 0; j < len(r.extraValues); j++ {
-				values = append(values, r.extraValues[j][1])
+			for j := 0; j < len(r.strategyParameters); j++ {
+				values = append(values, r.strategyParameters[j][1])
 			}
 			_ = tsvwiter.Write(values)
 		}
