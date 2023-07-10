3b6cff8dc7070befcac1c894ac11cd30dd180dcb
c9s <yoanlin93@gmail.com>
2023-07-10 15:24:07 +0800

    strategy: move risk control to common.Strategy

--- pkg/strategy/common/strategy.go
+++ pkg/strategy/common/strategy.go
@@ -3,7 +3,11 @@ package common
 import (
 	"context"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/c9s/bbgo/pkg/bbgo"
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/risk/riskcontrol"
 	"github.com/c9s/bbgo/pkg/types"
 )
 
@@ -18,6 +22,14 @@ type Strategy struct {
 	Environ       *bbgo.Environment
 	Session       *bbgo.ExchangeSession
 	OrderExecutor *bbgo.GeneralOrderExecutor
+
+	PositionHardLimit         fixedpoint.Value     `json:"positionHardLimit"`
+	MaxPositionQuantity       fixedpoint.Value     `json:"maxPositionQuantity"`
+	CircuitBreakLossThreshold fixedpoint.Value     `json:"circuitBreakLossThreshold"`
+	CircuitBreakEMA           types.IntervalWindow `json:"circuitBreakEMA"`
+
+	positionRiskControl     *riskcontrol.PositionRiskControl
+	circuitBreakRiskControl *riskcontrol.CircuitBreakRiskControl
 }
 
 func (s *Strategy) Initialize(ctx context.Context, environ *bbgo.Environment, session *bbgo.ExchangeSession, market types.Market, strategyID, instanceID string) {
@@ -55,4 +67,18 @@ func (s *Strategy) Initialize(ctx context.Context, environ *bbgo.Environment, se
 	s.OrderExecutor.TradeCollector().OnPositionUpdate(func(position *types.Position) {
 		// bbgo.Sync(ctx, s)
 	})
+
+	if !s.PositionHardLimit.IsZero() && !s.MaxPositionQuantity.IsZero() {
+		log.Infof("positionHardLimit and maxPositionQuantity are configured, setting up PositionRiskControl...")
+		s.positionRiskControl = riskcontrol.NewPositionRiskControl(s.OrderExecutor, s.PositionHardLimit, s.MaxPositionQuantity)
+	}
+
+	if !s.CircuitBreakLossThreshold.IsZero() {
+		log.Infof("circuitBreakLossThreshold is configured, setting up CircuitBreakRiskControl...")
+		s.circuitBreakRiskControl = riskcontrol.NewCircuitBreakRiskControl(
+			s.Position,
+			session.Indicators(market.Symbol).EWMA(s.CircuitBreakEMA),
+			s.CircuitBreakLossThreshold,
+			s.ProfitStats)
+	}
 }
