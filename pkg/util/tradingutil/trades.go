c605761c4f2bfa509d8f663f84aa00bae29ad6c3
c9s <yoanlin93@gmail.com>
2023-08-05 01:59:36 +0800

    add tradingutil package

---
+++ pkg/util/tradingutil/trades.go
@@ -0,0 +1,20 @@
+package tradingutil
+
+import (
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+// CollectTradeFee collects the fee from the given trade slice
+func CollectTradeFee(trades []types.Trade) map[string]fixedpoint.Value {
+	fees := make(map[string]fixedpoint.Value)
+	for _, t := range trades {
+		if fee, ok := fees[t.FeeCurrency]; ok {
+			fees[t.FeeCurrency] = fee.Add(t.Fee)
+		} else {
+			fees[t.FeeCurrency] = t.Fee
+		}
+	}
+
+	return fees
+}
