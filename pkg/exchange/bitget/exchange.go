2fda4477bd5eca33381f49cd7098d5d04a10b99e
c9s <yoanlin93@gmail.com>
2023-08-09 15:20:33 +0800

    bitget: implement QueryMarkets

--- pkg/exchange/bitget/exchange.go
+++ pkg/exchange/bitget/exchange.go
@@ -1,9 +1,13 @@
 package bitget
 
 import (
+	"context"
+	"math"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/c9s/bbgo/pkg/exchange/bitget/bitgetapi"
+	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
 )
 
@@ -42,4 +46,81 @@ func (e *Exchange) Name() types.ExchangeName {
 
 func (e *Exchange) PlatformFeeCurrency() string {
 	return PlatformToken
+}
+
+func (e *Exchange) NewStream() types.Stream {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) QueryMarkets(ctx context.Context) (types.MarketMap, error) {
+	// TODO implement me
+	req := e.client.NewGetSymbolsRequest()
+	symbols, err := req.Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	markets := types.MarketMap{}
+	for _, s := range symbols {
+		symbol := toGlobalSymbol(s.Symbol)
+		markets[symbol] = types.Market{
+			Symbol:          symbol,
+			LocalSymbol:     s.Symbol,
+			PricePrecision:  s.PriceScale,
+			VolumePrecision: s.QuantityScale,
+			QuoteCurrency:   s.QuoteCoin,
+			BaseCurrency:    s.BaseCoin,
+			MinNotional:     s.MinTradeUSDT,
+			MinAmount:       s.MinTradeUSDT,
+			MinQuantity:     s.MinTradeAmount,
+			MaxQuantity:     s.MaxTradeAmount,
+			StepSize:        fixedpoint.NewFromFloat(math.Pow10(-s.QuantityScale)),
+			TickSize:        fixedpoint.NewFromFloat(math.Pow10(-s.PriceScale)),
+			MinPrice:        0,
+			MaxPrice:        0,
+		}
+	}
+
+	return markets, nil
+}
+
+func (e *Exchange) QueryTicker(ctx context.Context, symbol string) (*types.Ticker, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) QueryTickers(ctx context.Context, symbol ...string) (map[string]types.Ticker, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) QueryKLines(ctx context.Context, symbol string, interval types.Interval, options types.KLineQueryOptions) ([]types.KLine, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) QueryAccount(ctx context.Context) (*types.Account, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) QueryAccountBalances(ctx context.Context) (types.BalanceMap, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) SubmitOrder(ctx context.Context, order types.SubmitOrder) (createdOrder *types.Order, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) QueryOpenOrders(ctx context.Context, symbol string) (orders []types.Order, err error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (e *Exchange) CancelOrders(ctx context.Context, orders ...types.Order) error {
+	// TODO implement me
+	panic("implement me")
 }
