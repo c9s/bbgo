affff325992ed972129511574cd0c27079fbf69c
Edwin <edwin@am.is>
2023-08-10 15:11:06 +0800

    pkg/exchange: get fee rate before connect

--- pkg/exchange/bybit/stream_test.go
+++ pkg/exchange/bybit/stream_test.go
@@ -8,8 +8,11 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/c9s/bbgo/pkg/exchange/bybit/bybitapi"
+	"github.com/c9s/bbgo/pkg/exchange/bybit/mocks"
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/testutil"
 	"github.com/c9s/bbgo/pkg/types"
@@ -26,7 +29,9 @@ func getTestClientOrSkip(t *testing.T) *Stream {
 		return nil
 	}
 
-	return NewStream(key, secret)
+	exchange, err := New(key, secret)
+	assert.NoError(t, err)
+	return NewStream(key, secret, exchange)
 }
 
 func TestStream(t *testing.T) {
@@ -310,5 +315,122 @@ func Test_convertSubscription(t *testing.T) {
 		})
 		assert.Equal(t, fmt.Errorf("unsupported stream channel: %s", "unsupported"), err)
 		assert.Equal(t, "", res)
+	})
+}
+
+func TestStream_getFeeRate(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	unknownErr := errors.New("unknown err")
+
+	t.Run("succeeds", func(t *testing.T) {
+		mockMarketProvider := mocks.NewMockMarketInfoProvider(mockCtrl)
+		s := &Stream{
+			marketProvider: mockMarketProvider,
+		}
+
+		ctx := context.Background()
+		feeRates := bybitapi.FeeRates{
+			List: []bybitapi.FeeRate{
+				{
+					Symbol:       "BTCUSDT",
+					TakerFeeRate: fixedpoint.NewFromFloat(0.001),
+					MakerFeeRate: fixedpoint.NewFromFloat(0.001),
+				},
+				{
+					Symbol:       "ETHUSDT",
+					TakerFeeRate: fixedpoint.NewFromFloat(0.001),
+					MakerFeeRate: fixedpoint.NewFromFloat(0.001),
+				},
+				{
+					Symbol:       "OPTIONCOIN",
+					TakerFeeRate: fixedpoint.NewFromFloat(0.001),
+					MakerFeeRate: fixedpoint.NewFromFloat(0.001),
+				},
+			},
+		}
+
+		mkts := types.MarketMap{
+			"BTCUSDT": types.Market{
+				Symbol:        "BTCUSDT",
+				QuoteCurrency: "USDT",
+				BaseCurrency:  "BTC",
+			},
+			"ETHUSDT": types.Market{
+				Symbol:        "ETHUSDT",
+				QuoteCurrency: "USDT",
+				BaseCurrency:  "ETH",
+			},
+		}
+
+		mockMarketProvider.EXPECT().GetFeeRates(ctx).Return(feeRates, nil).Times(1)
+		mockMarketProvider.EXPECT().QueryMarkets(ctx).Return(mkts, nil).Times(1)
+
+		expFeeRates := map[string]*symbolFeeDetail{
+			"BTCUSDT": {
+				FeeRate:   feeRates.List[0],
+				BaseCoin:  "BTC",
+				QuoteCoin: "USDT",
+			},
+			"ETHUSDT": {
+				FeeRate:   feeRates.List[1],
+				BaseCoin:  "ETH",
+				QuoteCoin: "USDT",
+			},
+		}
+		err := s.getFeeRate(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, expFeeRates, s.symbolFeeDetails)
+	})
+
+	t.Run("failed to query markets", func(t *testing.T) {
+		mockMarketProvider := mocks.NewMockMarketInfoProvider(mockCtrl)
+		s := &Stream{
+			marketProvider: mockMarketProvider,
+		}
+
+		ctx := context.Background()
+		feeRates := bybitapi.FeeRates{
+			List: []bybitapi.FeeRate{
+				{
+					Symbol:       "BTCUSDT",
+					TakerFeeRate: fixedpoint.NewFromFloat(0.001),
+					MakerFeeRate: fixedpoint.NewFromFloat(0.001),
+				},
+				{
+					Symbol:       "ETHUSDT",
+					TakerFeeRate: fixedpoint.NewFromFloat(0.001),
+					MakerFeeRate: fixedpoint.NewFromFloat(0.001),
+				},
+				{
+					Symbol:       "OPTIONCOIN",
+					TakerFeeRate: fixedpoint.NewFromFloat(0.001),
+					MakerFeeRate: fixedpoint.NewFromFloat(0.001),
+				},
+			},
+		}
+
+		mockMarketProvider.EXPECT().GetFeeRates(ctx).Return(feeRates, nil).Times(1)
+		mockMarketProvider.EXPECT().QueryMarkets(ctx).Return(nil, unknownErr).Times(1)
+
+		err := s.getFeeRate(ctx)
+		assert.Equal(t, fmt.Errorf("failed to get markets: %w", unknownErr), err)
+		assert.Equal(t, map[string]*symbolFeeDetail(nil), s.symbolFeeDetails)
+	})
+
+	t.Run("failed to get fee rates", func(t *testing.T) {
+		mockMarketProvider := mocks.NewMockMarketInfoProvider(mockCtrl)
+		s := &Stream{
+			marketProvider: mockMarketProvider,
+		}
+
+		ctx := context.Background()
+
+		mockMarketProvider.EXPECT().GetFeeRates(ctx).Return(bybitapi.FeeRates{}, unknownErr).Times(1)
+
+		err := s.getFeeRate(ctx)
+		assert.Equal(t, fmt.Errorf("failed to call get fee rates: %w", unknownErr), err)
+		assert.Equal(t, map[string]*symbolFeeDetail(nil), s.symbolFeeDetails)
 	})
 }
