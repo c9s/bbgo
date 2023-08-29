8dc9b958a64173701e9a51fb2070ab0e8a84cc7b
c9s <yoanlin93@gmail.com>
2023-08-29 16:12:31 +0800

    all: implement stats tracker
    - rename Add() to AddProfit() or AddTrade() so that we can apply
      interface here

--- pkg/types/trade_stats.go
+++ pkg/types/trade_stats.go
@@ -240,7 +240,7 @@ func (s *TradeStats) CsvRecords() [][]string {
 	}
 }
 
-func (s *TradeStats) Add(profit *Profit) {
+func (s *TradeStats) AddProfit(profit *Profit) {
 	if s.Symbol != "" && profit.Symbol != s.Symbol {
 		return
 	}
