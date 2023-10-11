142b857b210ec7bdd405359ab9e4b455c4ee127d
chiahung <s199180626@gmail.com>
2023-10-11 17:12:11 +0800

    also sync orders already in active orderbook if the open orders are expired

--- pkg/strategy/grid2/active_order_recover.go
+++ pkg/strategy/grid2/active_order_recover.go
@@ -6,6 +6,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/c9s/bbgo/pkg/bbgo"
 	"github.com/c9s/bbgo/pkg/exchange/retry"
 	"github.com/c9s/bbgo/pkg/types"
 	"github.com/c9s/bbgo/pkg/util"
@@ -15,8 +16,8 @@ func (s *Strategy) recoverActiveOrdersPeriodically(ctx context.Context) {
 	// every time we activeOrdersRecoverCh receive signal, do active orders recover
 	s.activeOrdersRecoverCh = make(chan struct{}, 1)
 
-	// make ticker's interval random in 40 min ~ 70 min
-	interval := util.MillisecondsJitter(40*time.Minute, 30*60*1000)
+	// make ticker's interval random in 25 min ~ 35 min
+	interval := util.MillisecondsJitter(25*time.Minute, 10*60*1000)
 	s.logger.Infof("[ActiveOrderRecover] interval: %s", interval)
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
@@ -55,11 +56,13 @@ func (s *Strategy) syncActiveOrders(ctx context.Context) error {
 	}
 
 	openOrders, err := retry.QueryOpenOrdersUntilSuccessful(ctx, s.session.Exchange, s.Symbol)
-
 	if err != nil {
 		s.logger.WithError(err).Error("[ActiveOrderRecover] failed to query open orders, skip this time")
 		return err
 	}
+
+	// open orders query time + 1 min means this open orders may be changed !
+	openOrdersExpiredTime := time.Now().Add(1 * time.Minute)
 
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -73,31 +76,48 @@ func (s *Strategy) syncActiveOrders(ctx context.Context) error {
 	}
 
 	// update active orders not in open orders
+	var openOrdersExpired bool = false
 	for _, activeOrder := range activeOrders {
 		if _, exist := openOrdersMap[activeOrder.OrderID]; exist {
+			if openOrdersExpired || time.Now().After(openOrdersExpiredTime) {
+				openOrdersExpired = true
+				s.logger.Infof("active order #%d is in the open orders but the update_at is over 1 min, updating...", activeOrder.OrderID)
+				if err := s.syncActiveOrder(ctx, activeOrderBook, activeOrder.OrderID); err != nil {
+					s.logger.WithError(err).Errorf("[ActiveOrderRecover] unable to query order #%d", activeOrder.OrderID)
+					continue
+				}
+			}
+
 			delete(openOrdersMap, activeOrder.OrderID)
 		} else {
 			s.logger.Infof("found active order #%d is not in the open orders, updating...", activeOrder.OrderID)
 
-			updatedOrder, err := retry.QueryOrderUntilSuccessful(ctx, s.orderQueryService, types.OrderQuery{
-				Symbol:  activeOrder.Symbol,
-				OrderID: strconv.FormatUint(activeOrder.OrderID, 10),
-			})
-
-			if err != nil {
+			if err := s.syncActiveOrder(ctx, activeOrderBook, activeOrder.OrderID); err != nil {
 				s.logger.WithError(err).Errorf("[ActiveOrderRecover] unable to query order #%d", activeOrder.OrderID)
 				continue
 			}
-
-			activeOrderBook.Update(*updatedOrder)
 		}
 	}
 
-	// TODO: should we add open orders back into active orderbook ?
 	// update open orders not in active orders
 	for _, openOrder := range openOrdersMap {
 		activeOrderBook.Update(openOrder)
 	}
+
+	return nil
+}
+
+func (s *Strategy) syncActiveOrder(ctx context.Context, activeOrderBook *bbgo.ActiveOrderBook, orderID uint64) error {
+	updatedOrder, err := retry.QueryOrderUntilSuccessful(ctx, s.orderQueryService, types.OrderQuery{
+		Symbol:  s.Symbol,
+		OrderID: strconv.FormatUint(orderID, 10),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	activeOrderBook.Update(*updatedOrder)
 
 	return nil
 }
