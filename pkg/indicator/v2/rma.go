22a7232e8b5a657f75cbd01a2b1cef49cad7b9eb
narumi <toucans-cutouts0f@icloud.com>
2023-10-24 16:37:44 +0800

    fix duplicate rma value

--- pkg/indicator/v2/rma.go
+++ pkg/indicator/v2/rma.go
@@ -34,23 +34,20 @@ func RMA2(source types.Float64Source, window int, adjust bool) *RMAStream {
 
 func (s *RMAStream) Calculate(x float64) float64 {
 	lambda := 1 / float64(s.window)
-	tmp := 0.0
 	if s.counter == 0 {
 		s.sum = 1
-		tmp = x
+		s.previous = x
 	} else {
 		if s.Adjust {
 			s.sum = s.sum*(1-lambda) + 1
-			tmp = s.previous + (x-s.previous)/s.sum
+			s.previous = s.previous + (x-s.previous)/s.sum
 		} else {
-			tmp = s.previous*(1-lambda) + x*lambda
+			s.previous = s.previous*(1-lambda) + x*lambda
 		}
 	}
 	s.counter++
 
-	s.Slice.Push(tmp)
-	s.previous = tmp
-	return tmp
+	return s.previous
 }
 
 func (s *RMAStream) Truncate() {
