cb5e305fedafbc9a9855011c9af8a09b43ce79a8
Edwin <edwin@am.is>
2023-11-06 11:51:16 +0800

    pkg/exchange: support submit order

---
+++ pkg/exchange/bitget/bitgetapi/v2/place_order_request.go
@@ -0,0 +1,29 @@
+package bitgetapi
+
+//go:generate -command GetRequest requestgen -method GET -responseType .APIResponse -responseDataField Data
+//go:generate -command PostRequest requestgen -method POST -responseType .APIResponse -responseDataField Data
+
+import (
+	"github.com/c9s/requestgen"
+)
+
+type PlaceOrderResponse struct {
+	OrderId       string `json:"orderId"`
+	ClientOrderId string `json:"clientOrderId"`
+}
+
+//go:generate PostRequest -url "/api/v2/spot/trade/place-order" -type PlaceOrderRequest -responseDataType .PlaceOrderResponse
+type PlaceOrderRequest struct {
+	client        requestgen.AuthenticatedAPIClient
+	symbol        string     `param:"symbol"`
+	orderType     OrderType  `param:"orderType"`
+	side          SideType   `param:"side"`
+	force         OrderForce `param:"force"`
+	price         *string    `param:"price"`
+	size          string     `param:"size"`
+	clientOrderId *string    `param:"clientOid"`
+}
+
+func (c *Client) NewPlaceOrderRequest() *PlaceOrderRequest {
+	return &PlaceOrderRequest{client: c.Client}
+}
