5e5b8e1388c40b05c8dddba1caed9064d625e715
Edwin <edwin@am.is>
2023-11-14 14:35:16 +0800

    pkg/exchange: use v2 symbols

---
+++ pkg/exchange/bitget/bitgetapi/v2/get_symbols_request.go
@@ -0,0 +1,47 @@
+package bitgetapi
+
+import (
+	"github.com/c9s/requestgen"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+)
+
+//go:generate -command GetRequest requestgen -method GET -responseType .APIResponse -responseDataField Data
+//go:generate -command PostRequest requestgen -method POST -responseType .APIResponse -responseDataField Data
+
+type SymbolStatus string
+
+const (
+	// SymbolOffline represent market is suspended, users cannot trade.
+	SymbolOffline SymbolStatus = "offline"
+	// SymbolGray represents market is online, but user trading is not available.
+	SymbolGray SymbolStatus = "gray"
+	// SymbolOnline trading begins, users can trade.
+	SymbolOnline SymbolStatus = "online"
+)
+
+type Symbol struct {
+	Symbol              string           `json:"symbol"`
+	BaseCoin            string           `json:"baseCoin"`
+	QuoteCoin           string           `json:"quoteCoin"`
+	MinTradeAmount      fixedpoint.Value `json:"minTradeAmount"`
+	MaxTradeAmount      fixedpoint.Value `json:"maxTradeAmount"`
+	TakerFeeRate        fixedpoint.Value `json:"takerFeeRate"`
+	MakerFeeRate        fixedpoint.Value `json:"makerFeeRate"`
+	PricePrecision      fixedpoint.Value `json:"pricePrecision"`
+	QuantityPrecision   fixedpoint.Value `json:"quantityPrecision"`
+	QuotePrecision      fixedpoint.Value `json:"quotePrecision"`
+	MinTradeUSDT        fixedpoint.Value `json:"minTradeUSDT"`
+	Status              SymbolStatus     `json:"status"`
+	BuyLimitPriceRatio  fixedpoint.Value `json:"buyLimitPriceRatio"`
+	SellLimitPriceRatio fixedpoint.Value `json:"sellLimitPriceRatio"`
+}
+
+//go:generate GetRequest -url "/api/v2/spot/public/symbols" -type GetSymbolsRequest -responseDataType []Symbol
+type GetSymbolsRequest struct {
+	client requestgen.APIClient
+}
+
+func (c *Client) NewGetSymbolsRequest() *GetSymbolsRequest {
+	return &GetSymbolsRequest{client: c.Client}
+}
