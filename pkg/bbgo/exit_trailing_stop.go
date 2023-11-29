f9169a2e464449cf662aeed3a7d1f8cc362afd68
root <root@LAPTOP-454T6FMK.localdomain>
2023-11-29 18:37:28 +0800

    FIX: format minimal profit to percent

--- pkg/bbgo/exit_trailing_stop.go
+++ pkg/bbgo/exit_trailing_stop.go
@@ -101,7 +101,7 @@ func (s *TrailingStop2) checkStopPrice(price fixedpoint.Value, position *types.P
 		// check if we have the minimal profit
 		roi := position.ROI(price)
 		if roi.Compare(s.MinProfit) >= 0 {
-			Notify("[trailingStop] activated: %s ROI %s > minimal profit ratio %f", s.Symbol, roi.Percentage(), s.MinProfit.Float64())
+			Notify("[trailingStop] activated: %s ROI %s > minimal profit ratio %s", s.Symbol, roi.Percentage(), s.MinProfit.Percentage())
 			s.activated = true
 		}
 	} else if !s.ActivationRatio.IsZero() {
