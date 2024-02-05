b6717f2fcf8d8846754c6052e02bfe4a4d78ea25
edwin <edwin@maicoin.com>
2024-02-05 11:15:01 +0800

    pkg/exchange: replace /sapi/v1/margin/transfer to /sapi/v1/asset/transfer

---
+++ pkg/exchange/binance/binanceapi/transfer_asset_request.go
@@ -0,0 +1,40 @@
+package binanceapi
+
+import (
+	"time"
+
+	"github.com/c9s/requestgen"
+)
+
+type TransferResponse struct {
+	TranId int `json:"tranId"`
+}
+
+type TransferAssetType string
+
+const (
+	TransferAssetTypeMainToMargin         TransferAssetType = "MAIN_MARGIN"
+	TransferAssetTypeMarginToMain         TransferAssetType = "MARGIN_MAIN"
+	TransferAssetTypeMainToIsolatedMargin TransferAssetType = "MAIN_ISOLATED_MARGIN"
+	TransferAssetTypeIsolatedMarginToMain TransferAssetType = "ISOLATED_MARGIN_MAIN"
+)
+
+//go:generate requestgen -method POST -url "/sapi/v1/asset/transfer" -type TransferAssetRequest -responseType .TransferResponse
+type TransferAssetRequest struct {
+	client requestgen.AuthenticatedAPIClient
+
+	asset string `param:"asset"`
+
+	transferType TransferAssetType `param:"type"`
+
+	amount string `param:"amount"`
+
+	timestamp time.Time `param:"timestamp,milliseconds,query"`
+
+	fromSymbol *string `param:"fromSymbol"`
+	toSymbol   *string `param:"toSymbol"`
+}
+
+func (c *RestClient) NewTransferAssetRequest() *TransferAssetRequest {
+	return &TransferAssetRequest{client: c}
+}
