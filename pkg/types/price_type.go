ddce472578a608e6a42e9a207691ae4d3f279d92
narumi <4680567+narumiruna@users.noreply.github.com>
2024-02-23 16:29:26 +0800

    unmarshal price type

--- pkg/types/price_type.go
+++ pkg/types/price_type.go
@@ -1,7 +1,11 @@
 package types
 
 import (
+	"encoding/json"
+	"strings"
+
 	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/pkg/errors"
 )
 
 type PriceType string
@@ -14,6 +18,44 @@ const (
 	PriceTypeMaker PriceType = "MAKER"
 	PriceTypeTaker PriceType = "TAKER"
 )
+
+var ErrInvalidPriceType = errors.New("invalid price type")
+
+func StrToPriceType(s string) (price PriceType, err error) {
+	switch strings.ToLower(s) {
+	case "last":
+		price = PriceTypeLast
+	case "buy":
+		price = PriceTypeBuy
+	case "sell":
+		price = PriceTypeSell
+	case "mid":
+		price = PriceTypeMid
+	case "maker":
+		price = PriceTypeMaker
+	case "taker":
+		price = PriceTypeTaker
+	default:
+		err = ErrInvalidPriceType
+	}
+	return price, err
+}
+
+func (p *PriceType) UnmarshalJSON(data []byte) error {
+	var s string
+
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	t, err := StrToPriceType(s)
+	if err != nil {
+		return err
+	}
+
+	*p = t
+	return nil
+}
 
 func (p PriceType) Map(ticker *Ticker, side SideType) fixedpoint.Value {
 	price := ticker.Last
