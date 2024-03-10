927b387b1c110b654bd55d02b0df09539a23047e
Sven Woldt <sven.woldt@shell.com>
2024-03-10 20:23:18 +0100

    fix test

--- pkg/datasource/csvsource/tick_downloader_test.go
+++ pkg/datasource/csvsource/tick_downloader_test.go
@@ -2,7 +2,6 @@ package csvsource
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -17,88 +16,92 @@ type DownloadTester struct {
 	Reader      MakeCSVTickReader
 	Market      MarketType
 	Granularity DataType
-	Symbol      string
+	Symbols     []string
 	Path        string
 }
 
 var (
 	expectedCandles = []int{864, 144, 72}
 	intervals       = []types.Interval{types.Interval5m, types.Interval30m, types.Interval1h}
+	symbols         = []string{"FXSUSDT", "BTCUSDT"}
 	since           = time.Date(2023, 11, 17, 0, 0, 0, 0, time.UTC)
 	until           = time.Date(2023, 11, 19, 0, 0, 0, 0, time.UTC)
 )
 
 func Test_CSV_Download(t *testing.T) {
-	if _, ok := os.LookupEnv("TEST_CSV_DOWNLOADER"); !ok {
-		t.Skip()
-	}
+	// if _, ok := os.LookupEnv("TEST_CSV_DOWNLOADER"); !ok {
+	// 	t.Skip()
+	// }
 	var tests = []DownloadTester{
 		{
 			Exchange:    types.ExchangeBinance,
 			Reader:      NewBinanceCSVTickReader,
 			Market:      SPOT,
 			Granularity: AGGTRADES,
-			Symbol:      "FXSUSDT",
-			Path:        "testdata/binance/FXSUSDT",
+			Symbols:     symbols,
+			Path:        "testdata/binance",
 		},
 		{
 			Exchange:    types.ExchangeBybit,
 			Reader:      NewBybitCSVTickReader,
 			Market:      FUTURES,
 			Granularity: AGGTRADES,
-			Symbol:      "FXSUSDT",
-			Path:        "testdata/bybit/FXSUSDT",
+			Symbols:     symbols,
+			Path:        "testdata/bybit",
 		},
 		{
 			Exchange:    types.ExchangeOKEx,
 			Reader:      NewOKExCSVTickReader,
 			Market:      SPOT,
 			Granularity: AGGTRADES,
-			Symbol:      "FXSUSDT",
-			Path:        "testdata/okex/FXSUSDT",
+			Symbols:     symbols,
+			Path:        "testdata/okex",
 		},
 	}
 
 	for _, tt := range tests {
-		err := Download(
-			tt.Path,
-			tt.Symbol,
-			tt.Exchange,
-			tt.Market,
-			tt.Granularity,
-			since,
-			until,
-		)
-		assert.NoError(t, err)
-
-		klineMap, err := ReadTicksFromCSVWithDecoder(
-			filepath.Join(tt.Path, string(tt.Granularity)),
-			tt.Symbol,
-			intervals,
-			MakeCSVTickReader(tt.Reader),
-		)
-		assert.NoError(t, err)
-
-		for i, interval := range intervals {
-			klines := klineMap[interval]
-
-			assert.Equal(
-				t,
-				expectedCandles[i],
-				len(klines),
-				fmt.Sprintf("%s: %s/%s should have %d kLines",
-					tt.Exchange.String(),
-					tt.Symbol,
-					interval.String(),
-					expectedCandles[i],
-				),
+		for _, symbol := range tt.Symbols {
+			path := filepath.Join(tt.Path, symbol)
+			err := Download(
+				path,
+				symbol,
+				tt.Exchange,
+				tt.Market,
+				tt.Granularity,
+				since,
+				until,
 			)
-
-			err = WriteKLines(tt.Path, tt.Symbol, klines)
 			assert.NoError(t, err)
+
+			klineMap, err := ReadTicksFromCSVWithDecoder(
+				filepath.Join(path, string(tt.Granularity)),
+				symbol,
+				intervals,
+				MakeCSVTickReader(tt.Reader),
+			)
+			assert.NoError(t, err)
+
+			for i, interval := range intervals {
+				klines := klineMap[interval]
+
+				assert.Equal(
+					t,
+					expectedCandles[i],
+					len(klines),
+					fmt.Sprintf("%s: %s/%s should have %d kLines",
+						tt.Exchange.String(),
+						symbol,
+						interval.String(),
+						expectedCandles[i],
+					),
+				)
+
+				err = WriteKLines(path, symbol, klines)
+				assert.NoError(t, err)
+			}
 		}
 
-		err = os.RemoveAll(tt.Path)
-		assert.NoError(t, err)
+		// err = os.RemoveAll(tt.Path)
+		// assert.NoError(t, err)
 	}
 }
