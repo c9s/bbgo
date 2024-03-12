465e7d8983d184b582eee49b0983df0e693db16d
zenix.huang <zenixls2@gmail.com>
2024-03-12 15:57:22 +0900

    add test for binance new function

--- pkg/exchange/binance/exchange_test.go
+++ pkg/exchange/binance/exchange_test.go
@@ -1,6 +1,7 @@
 package binance
 
 import (
+	"context"
 	"strings"
 	"testing"
 
@@ -14,4 +15,13 @@ func Test_newClientOrderID(t *testing.T) {
 
 	cID = newSpotClientOrderID("myid1")
 	assert.Equal(t, cID, "x-"+spotBrokerID+"myid1")
+}
+
+func Test_new(t *testing.T) {
+	ex := New("", "")
+	assert.NotEmpty(t, ex)
+	ctx := context.Background()
+	ticker, err := ex.QueryTicker(ctx, "btcusdt")
+	assert.NotEmpty(t, ticker)
+	assert.NoError(t, err)
 }
