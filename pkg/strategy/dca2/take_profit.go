70a10582fab51a5f90116f989fdf3d5809c07853
kbearXD <s199180626@gmail.com>
2024-04-15 16:25:34 +0800

    FEATURE: recollect position before placing the take-profit order

--- pkg/strategy/dca2/take_profit.go
+++ pkg/strategy/dca2/take_profit.go
@@ -2,14 +2,38 @@ package dca2
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
+	"github.com/pkg/errors"
 )
 
 func (s *Strategy) placeTakeProfitOrders(ctx context.Context) error {
 	s.logger.Info("start placing take profit orders")
-	order := generateTakeProfitOrder(s.Market, s.TakeProfitRatio, s.Position, s.OrderGroupID)
+	currentRound, err := s.roundCollector.CollectCurrentRound(ctx)
+	if currentRound.TakeProfitOrder.OrderID != 0 {
+		return fmt.Errorf("there is a take-profit order before placing the take-profit order, please check it")
+	}
+
+	trades, err := s.roundCollector.CollectRoundTrades(ctx, currentRound)
+	if err != nil {
+		return errors.Wrap(err, "failed to place the take-profit order when collecting round trades")
+	}
+
+	position := types.NewPositionFromMarket(s.Market)
+
+	for _, trade := range trades {
+		if trade.FeeProcessing {
+			return fmt.Errorf("failed to place the take-profit order because there is a trade's fee not ready")
+		}
+
+		position.AddTrade(trade)
+	}
+
+	s.logger.Infof("placeTakeProfitOrders: %s", position.String())
+
+	order := generateTakeProfitOrder(s.Market, s.TakeProfitRatio, position, s.OrderGroupID)
 	createdOrders, err := s.OrderExecutor.SubmitOrders(ctx, order)
 	if err != nil {
 		return err
