94c126dd833cfeb9912949d7d39f1e979f3a8d21
narumi <toucans-cutouts0f@icloud.com>
2024-04-17 15:27:46 +0800

    move logerr to util

--- pkg/strategy/atrpin/strategy.go
+++ pkg/strategy/atrpin/strategy.go
@@ -199,21 +199,3 @@ func (s *Strategy) Run(ctx context.Context, orderExecutor bbgo.OrderExecutor, se
 
 	return nil
 }
-
-func logErr(err error, msgAndArgs ...interface{}) bool {
-	if err == nil {
-		return false
-	}
-
-	if len(msgAndArgs) == 0 {
-		log.WithError(err).Error(err.Error())
-	} else if len(msgAndArgs) == 1 {
-		msg := msgAndArgs[0].(string)
-		log.WithError(err).Error(msg)
-	} else if len(msgAndArgs) > 1 {
-		msg := msgAndArgs[0].(string)
-		log.WithError(err).Errorf(msg, msgAndArgs[1:]...)
-	}
-
-	return true
-}
