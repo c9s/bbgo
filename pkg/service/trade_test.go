2c2e5afa453f0de6e5018cbc0887f704291f5107
Yu-Cheng <yucheng@maicoin.com>
2024-07-09 17:36:20 +0800

    trade: test gid parameter

--- pkg/service/trade_test.go
+++ pkg/service/trade_test.go
@@ -110,6 +110,10 @@ func TestTradeService_Query(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, "invalid order by column: invalid_column", err.Error())
 
+	mock.ExpectQuery("SELECT \\* FROM trades WHERE gid > \\? ORDER BY gid ASC").WithArgs(1234).WillReturnError(sql.ErrNoRows)
+	_, err = s.Query(QueryTradesOptions{LastGID: 1234, Ordering: "ASC", OrderByColumn: "gid"})
+	assert.Equal(t, sql.ErrNoRows, err)
+
 	mock.ExpectQuery("SELECT \\* FROM trades ORDER BY gid DESC").WillReturnError(sql.ErrNoRows)
 	_, err = s.Query(QueryTradesOptions{Ordering: "DESC", OrderByColumn: "gid"})
 	assert.Equal(t, sql.ErrNoRows, err)
