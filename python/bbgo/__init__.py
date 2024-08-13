5ef45e09308de703efa86262014343496a372fcc
narumi <toucans-cutouts0f@icloud.com>
2024-08-13 17:07:56 +0800

    delete python

--- python/bbgo/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import enums
-from . import handlers
-from . import utils
-from .services import MarketService
-from .services import TradingService
-from .services import UserDataService
-from .stream import Stream
