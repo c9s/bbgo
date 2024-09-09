77dfe213e5a3abc3126ce994d3ec6a66a0316483
c9s <yoanlin93@gmail.com>
2024-09-09 14:41:41 +0800

    xmaker: pull out getLayerPrice and add test against the method

--- pkg/strategy/xmaker/strategy_test.go
+++ pkg/strategy/xmaker/strategy_test.go
@@ -2,27 +2,88 @@ package xmaker
 
 import (
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
-	"github.com/stretchr/testify/assert"
+
+	. "github.com/c9s/bbgo/pkg/testing/testhelper"
 )
 
-func Test_aggregatePrice(t *testing.T) {
-	bids := types.PriceVolumeSlice{
-		{
-			Price:  fixedpoint.NewFromFloat(1000.0),
-			Volume: fixedpoint.NewFromFloat(1.0),
-		},
-		{
-			Price:  fixedpoint.NewFromFloat(1200.0),
-			Volume: fixedpoint.NewFromFloat(1.0),
-		},
-		{
-			Price:  fixedpoint.NewFromFloat(1400.0),
-			Volume: fixedpoint.NewFromFloat(1.0),
-		},
+func TestStrategy_getLayerPrice(t *testing.T) {
+	symbol := "BTCUSDT"
+	market := Market(symbol)
+
+	s := &Strategy{
+		UseDepthPrice: true,
+		DepthQuantity: Number(3.0),
+		makerMarket:   market,
 	}
+
+	sourceBook := types.NewStreamBook(symbol, types.ExchangeBinance)
+	sourceBook.Load(types.SliceOrderBook{
+		Symbol: symbol,
+		Bids: PriceVolumeSlice(
+			Number(1300.0), Number(1.0),
+			Number(1200.0), Number(2.0),
+			Number(1100.0), Number(3.0),
+		),
+		Asks: PriceVolumeSlice(
+			Number(1301.0), Number(1.0),
+			Number(1400.0), Number(2.0),
+			Number(1500.0), Number(3.0),
+		),
+		Time:         time.Time{},
+		LastUpdateId: 1,
+	})
+
+	quote := &Quote{
+		BestBidPrice: Number(1300.0),
+		BestAskPrice: Number(1301.0),
+		BidMargin:    Number(0.001),
+		AskMargin:    Number(0.001),
+		BidLayerPips: Number(100.0),
+		AskLayerPips: Number(100.0),
+	}
+
+	t.Run("depthPrice bid price at 0", func(t *testing.T) {
+		price := s.getLayerPrice(0, types.SideTypeBuy, sourceBook, quote, s.DepthQuantity)
+
+		// (1300 + 1200*2)/3 * (1 - 0.001)
+		assert.InDelta(t, 1232.10, price.Float64(), 0.01)
+	})
+
+	t.Run("depthPrice bid price at 1", func(t *testing.T) {
+		price := s.getLayerPrice(1, types.SideTypeBuy, sourceBook, quote, s.DepthQuantity)
+
+		// (1300 + 1200*2)/3 * (1 - 0.001) - 100 * 0.01
+		assert.InDelta(t, 1231.10, price.Float64(), 0.01)
+	})
+
+	t.Run("depthPrice ask price at 0", func(t *testing.T) {
+		price := s.getLayerPrice(0, types.SideTypeSell, sourceBook, quote, s.DepthQuantity)
+
+		// (1301 + 1400*2)/3 * (1 + 0.001)
+		assert.InDelta(t, 1368.367, price.Float64(), 0.01)
+	})
+
+	t.Run("depthPrice ask price at 1", func(t *testing.T) {
+		price := s.getLayerPrice(1, types.SideTypeSell, sourceBook, quote, s.DepthQuantity)
+
+		// (1301 + 1400*2)/3 * (1 + 0.001) + 100 * 0.01
+		assert.InDelta(t, 1369.367, price.Float64(), 0.01)
+	})
+
+}
+
+func Test_aggregatePrice(t *testing.T) {
+	bids := PriceVolumeSliceFromText(`
+	1000.0, 1.0
+	1200.0, 1.0
+	1400.0, 1.0
+`)
 
 	aggregatedPrice1 := aggregatePrice(bids, fixedpoint.NewFromFloat(0.5))
 	assert.Equal(t, fixedpoint.NewFromFloat(1000.0), aggregatedPrice1)
