92121a68adb0bcb11e8410b854fc348e626640c1
c9s <yoanlin93@gmail.com>
2024-09-18 13:30:56 +0800

    compile and update migration package

---
+++ pkg/migrations/mysql/main_20240918132534_add_position_index.go
@@ -0,0 +1,29 @@
+package mysql
+
+import (
+	"context"
+
+	"github.com/c9s/rockhopper/v2"
+)
+
+func init() {
+	AddMigration("main", up_main_addPositionIndex, down_main_addPositionIndex)
+}
+
+func up_main_addPositionIndex(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is applied.
+	_, err = tx.ExecContext(ctx, "CREATE INDEX positions_traded_at ON positions (traded_at, profit);")
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func down_main_addPositionIndex(ctx context.Context, tx rockhopper.SQLExecutor) (err error) {
+	// This code is executed when the migration is rolled back.
+	_, err = tx.ExecContext(ctx, "DROP INDEX positions_traded_at ON positions;")
+	if err != nil {
+		return err
+	}
+	return err
+}
