9194a9152c6348e630c4e70bb6058f93d2c235c3
c9s <yoanlin93@gmail.com>
2024-09-27 18:30:51 +0800

    extend ticker method

--- pkg/types/ticker.go
+++ pkg/types/ticker.go
@@ -18,6 +18,22 @@ type Ticker struct {
 	Sell   fixedpoint.Value // `sell` from Max, `askPrice` from binance
 }
 
+func (t *Ticker) GetValidPrice() fixedpoint.Value {
+	if !t.Last.IsZero() {
+		return t.Last
+	}
+
+	if !t.Buy.IsZero() {
+		return t.Buy
+	}
+
+	if !t.Sell.IsZero() {
+		return t.Sell
+	}
+
+	return t.Open
+}
+
 func (t *Ticker) String() string {
 	return fmt.Sprintf("O:%s H:%s L:%s LAST:%s BID/ASK:%s/%s TIME:%s", t.Open, t.High, t.Low, t.Last, t.Buy, t.Sell, t.Time.String())
 }
