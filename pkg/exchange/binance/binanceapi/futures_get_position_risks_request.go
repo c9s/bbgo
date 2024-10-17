4cfba63a44749ee2913c9324429d6ef1677099c0
anywhy <anyyang527@outlook.com>
2024-10-17 17:27:18 +0800

    update binance api positionRisk to v3

--- pkg/exchange/binance/binanceapi/futures_get_position_risks_request.go
+++ pkg/exchange/binance/binanceapi/futures_get_position_risks_request.go
@@ -8,24 +8,30 @@ import (
 )
 
 type FuturesPositionRisk struct {
-	EntryPrice       fixedpoint.Value           `json:"entryPrice"`
-	MarginType       string                     `json:"marginType"`
-	IsAutoAddMargin  string                     `json:"isAutoAddMargin"`
-	IsolatedMargin   string                     `json:"isolatedMargin"`
-	Leverage         fixedpoint.Value           `json:"leverage"`
-	LiquidationPrice fixedpoint.Value           `json:"liquidationPrice"`
-	MarkPrice        fixedpoint.Value           `json:"markPrice"`
-	MaxNotionalValue fixedpoint.Value           `json:"maxNotionalValue"`
-	PositionAmount   fixedpoint.Value           `json:"positionAmt"`
-	Notional         fixedpoint.Value           `json:"notional"`
-	IsolatedWallet   string                     `json:"isolatedWallet"`
-	Symbol           string                     `json:"symbol"`
-	UnRealizedProfit fixedpoint.Value           `json:"unRealizedProfit"`
-	PositionSide     string                     `json:"positionSide"`
-	UpdateTime       types.MillisecondTimestamp `json:"updateTime"`
+	Symbol                 string           `json:"symbol"`
+	PositionSide           string           `json:"positionSide"`
+	PositionAmt            fixedpoint.Value `json:"positionAmt"`
+	EntryPrice             fixedpoint.Value `json:"entryPrice"`
+	BreakEvenPrice         fixedpoint.Value `json:"breakEvenPrice"`
+	MarkPrice              fixedpoint.Value `json:"markPrice"`
+	UnRealizedProfit       fixedpoint.Value `json:"unRealizedProfit"`
+	LiquidationPrice       fixedpoint.Value `json:"liquidationPrice"`
+	IsolatedMargin         string           `json:"isolatedMargin"`
+	Notional               fixedpoint.Value `json:"notional"`
+	MarginAsset            fixedpoint.Value `json:"marginAsset"`
+	IsolatedWallet         string           `json:"isolatedWallet"`
+	InitialMargin          fixedpoint.Value `json:"initialMargin"`
+	MaintMargin            fixedpoint.Value `json:"maintMargin"`
+	PositionInitialMargin  fixedpoint.Value `json:"positionInitialMargin"`
+	OpenOrderInitialMargin fixedpoint.Value `json:"openOrderInitialMargin"`
+	Adl                    fixedpoint.Value `json:"adl"`
+	BidNotional            fixedpoint.Value `json:"bidNotional"`
+	AskNotional            fixedpoint.Value `json:"askNotional"`
+
+	UpdateTime types.MillisecondTimestamp `json:"updateTime"`
 }
 
-//go:generate requestgen -method GET -url "/fapi/v2/positionRisk" -type FuturesGetPositionRisksRequest -responseType []FuturesPositionRisk
+//go:generate requestgen -method GET -url "/fapi/v3/positionRisk" -type FuturesGetPositionRisksRequest -responseType []FuturesPositionRisk
 type FuturesGetPositionRisksRequest struct {
 	client requestgen.AuthenticatedAPIClient
 
