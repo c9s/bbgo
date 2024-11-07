30b574a7ea2d7c306149bd651e4cfa2c8736d963
c9s <yoanlin93@gmail.com>
2024-11-07 16:31:02 +0800

    dynamic: compareStruct and compareSimpleValue

--- pkg/dynamic/compare.go
+++ pkg/dynamic/compare.go
@@ -14,62 +14,24 @@ type Diff struct {
 	After  string `json:"after"`
 }
 
-func isSimpleType(kind reflect.Kind) bool {
-	switch kind {
-	case reflect.Bool, reflect.Int, reflect.Int32, reflect.Int64, reflect.Uint64, reflect.String, reflect.Float64:
-		return true
-	default:
-		return false
-	}
-}
-
-func compareSimpleValue(a, b reflect.Value) bool {
-	if a.Kind() != b.Kind() {
-		return false
-	}
-
-	switch a.Kind() {
-	case reflect.Bool:
-		if a.Bool() == b.Bool() {
-			return true
-		}
-
-	case reflect.Int, reflect.Int32, reflect.Int64, reflect.Uint64:
-		if a.Int() == b.Int() {
-			return true
-		}
-
-	case reflect.String:
-		if a.String() == b.String() {
-			return true
-		}
-
-	case reflect.Float64:
-		if a.Float() == b.Float() {
-			return true
-		}
-
-	case reflect.Slice:
-		// TODO: compare slice
-
-	default:
-		// unhandled case
-
-	}
-
-	return false
-}
-
 // a (after)
 // b (before)
 func Compare(a, b interface{}) ([]Diff, error) {
 	var diffs []Diff
 
 	ra := reflect.ValueOf(a)
+	if ra.Kind() == reflect.Ptr {
+		ra = ra.Elem()
+	}
+
 	raType := ra.Type()
 	raKind := ra.Kind()
 
 	rb := reflect.ValueOf(b)
+	if rb.Kind() == reflect.Ptr {
+		rb = rb.Elem()
+	}
+
 	rbType := rb.Type()
 	rbKind := rb.Kind() // bool, int, slice, string, struct
 
@@ -96,13 +58,131 @@ func Compare(a, b interface{}) ([]Diff, error) {
 		}
 	}
 
+	if raKind == reflect.Struct {
+
+	}
+
 	return diffs, nil
+}
+
+func compareStruct(a, b reflect.Value) ([]Diff, error) {
+	if a.Kind() == reflect.Ptr {
+		a = a.Elem()
+	}
+
+	if b.Kind() == reflect.Ptr {
+		b = b.Elem()
+	}
+
+	if a.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("value is not a struct")
+	}
+
+	if b.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("value is not a struct")
+	}
+
+	if a.Type() != b.Type() {
+		return nil, fmt.Errorf("type is not the same")
+	}
+
+	var diffs []Diff
+
+	numFields := a.NumField()
+	for i := 0; i < numFields; i++ {
+		fieldValueA := a.Field(i)
+		fieldValueB := b.Field(i)
+
+		fieldName := a.Type().Field(i).Name
+
+		if isSimpleType(fieldValueA.Kind()) {
+			if compareSimpleValue(fieldValueA, fieldValueB) {
+				continue
+			} else {
+				diffs = append(diffs, Diff{
+					Field:  fieldName,
+					Before: convertToStr(fieldValueB),
+					After:  convertToStr(fieldValueA),
+				})
+			}
+		} else if fieldValueA.Kind() == reflect.Struct && fieldValueB.Kind() == reflect.Struct {
+			subDiffs, err := compareStruct(fieldValueA, fieldValueB)
+			if err != nil {
+				return diffs, err
+			}
+
+			for _, subDiff := range subDiffs {
+				diffs = append(diffs, Diff{
+					Field:  fieldName + "." + subDiff.Field,
+					Before: subDiff.Before,
+					After:  subDiff.After,
+				})
+			}
+		}
+	}
+
+	return diffs, nil
+}
+
+func isSimpleType(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Bool, reflect.Int, reflect.Int32, reflect.Int64, reflect.Uint64, reflect.String, reflect.Float64:
+		return true
+	default:
+		return false
+	}
+}
+
+func compareSimpleValue(a, b reflect.Value) bool {
+	if a.Kind() != b.Kind() {
+		return false
+	}
+
+	switch a.Kind() {
+
+	case reflect.Bool:
+		if a.Bool() == b.Bool() {
+			return true
+		}
+
+	case reflect.Uint, reflect.Uint32, reflect.Uint64:
+		if a.Uint() == b.Uint() {
+			return true
+		}
+	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
+		if a.Int() == b.Int() {
+			return true
+		}
+
+	case reflect.String:
+		if a.String() == b.String() {
+			return true
+		}
+
+	case reflect.Float64:
+		if a.Float() == b.Float() {
+			return true
+		}
+
+	case reflect.Slice:
+		// TODO: compare slice
+
+	default:
+		// unhandled case
+
+	}
+
+	return false
 }
 
 func convertToStr(val reflect.Value) string {
 	if val.Type() == reflect.TypeOf(fixedpoint.Zero) {
 		fp := val.Interface().(fixedpoint.Value)
 		return fp.String()
+	}
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
 	}
 
 	switch val.Kind() {
@@ -116,11 +196,7 @@ func convertToStr(val reflect.Value) string {
 		return strconv.FormatUint(val.Uint(), 10)
 
 	case reflect.Bool:
-		if val.Bool() {
-			return "true"
-		} else {
-			return "false"
-		}
+		return strconv.FormatBool(val.Bool())
 	default:
 		strType := reflect.TypeOf("")
 		if val.CanConvert(strType) {
