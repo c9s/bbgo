9e736a72824104a89926093811db0a3064ade2c8
c9s <yoanlin93@gmail.com>
2024-11-08 13:12:23 +0800

    slacknotifier,livenote: fix slack user lookup and add one time mention

--- pkg/livenote/options.go
+++ pkg/livenote/options.go
@@ -2,11 +2,30 @@ package livenote
 
 type Option interface{}
 
-type Mention struct {
-	User string
+type OptionCompare struct {
+	Value bool
 }
 
-type Comment struct {
+func CompareObject(value bool) *OptionCompare {
+	return &OptionCompare{Value: value}
+}
+
+type OptionOneTimeMention struct {
+	Users []string
+}
+
+func OneTimeMention(users ...string) *OptionOneTimeMention {
+	return &OptionOneTimeMention{Users: users}
+}
+
+type OptionComment struct {
 	Text  string
 	Users []string
+}
+
+func Comment(text string, users ...string) *OptionComment {
+	return &OptionComment{
+		Text:  text,
+		Users: users,
+	}
 }
