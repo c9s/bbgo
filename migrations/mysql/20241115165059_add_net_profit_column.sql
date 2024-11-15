b574c25a56c11c85902e6ffe76efff26b64b4a79
c9s <yoanlin93@gmail.com>
2024-11-15 18:02:32 +0800

    migration: set net profit from profit

--- migrations/mysql/20241115165059_add_net_profit_column.sql
+++ migrations/mysql/20241115165059_add_net_profit_column.sql
@@ -4,7 +4,9 @@ ALTER TABLE `positions`
     ADD COLUMN `net_profit` DECIMAL(16, 8) DEFAULT 0.00000000 NOT NULL
 ;
 -- +end
-
+-- +begin
+UPDATE positions SET net_profit = profit WHERE net_profit = 0.0;
+-- +end
 
 -- +down
 
