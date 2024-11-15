90645bd5a7d034a34b7ece58d67e2b1261d7d264
c9s <yoanlin93@gmail.com>
2024-11-15 15:56:17 +0800

    style: fix PnLEmojiMargin

--- pkg/style/pnl.go
+++ pkg/style/pnl.go
@@ -1,6 +1,8 @@
 package style
 
 import (
+	"strings"
+
 	"github.com/c9s/bbgo/pkg/fixedpoint"
 )
 
@@ -27,40 +29,32 @@ func PnLSignString(pnl fixedpoint.Value) string {
 }
 
 func PnLEmojiSimple(pnl fixedpoint.Value) string {
+	if pnl.IsZero() {
+		return ""
+	}
+
 	if pnl.Sign() < 0 {
 		return LossEmoji
+	} else {
+		return ProfitEmoji
+	}
+}
+
+// PnLEmojiMargin returns the emoji representation of the PnL with margin and resolution
+func PnLEmojiMargin(pnl, margin, resolution fixedpoint.Value) string {
+	if margin.IsZero() {
+		return PnLEmojiSimple(pnl)
 	}
 
 	if pnl.IsZero() {
 		return ""
 	}
 
-	return ProfitEmoji
-}
-
-func PnLEmojiMargin(pnl, margin, resolution fixedpoint.Value) (out string) {
-	if margin.IsZero() {
-		return PnLEmojiSimple(pnl)
-	}
-
 	if pnl.Sign() < 0 {
-		out = LossEmoji
-		level := max((margin.Neg()).Div(resolution).Int(), MaxEmojiRepeat)
-		for i := 1; i < level; i++ {
-			out += LossEmoji
-		}
-		return out
+		level := min((margin.Neg()).Div(resolution).Int(), MaxEmojiRepeat)
+		return strings.Repeat(LossEmoji, level)
 	}
 
-	if pnl.IsZero() {
-		return out
-	}
-
-	out = ProfitEmoji
-	level := max(margin.Div(resolution).Int(), MaxEmojiRepeat)
-	for i := 1; i < level; i++ {
-		out += ProfitEmoji
-	}
-
-	return out
+	level := min(margin.Div(resolution).Int(), MaxEmojiRepeat)
+	return strings.Repeat(ProfitEmoji, level)
 }
