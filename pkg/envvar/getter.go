89f6fbb9f74ef63247d79e5beff772c28e282549
c9s <yoanlin93@gmail.com>
2024-11-29 14:57:25 +0800

    add envvar.Int64

--- pkg/envvar/getter.go
+++ pkg/envvar/getter.go
@@ -28,6 +28,27 @@ func Duration(n string, args ...time.Duration) (time.Duration, bool) {
 	return du, true
 }
 
+// Int64 returns the int64 value of the environment variable named n.
+func Int64(n string, args ...int64) (int64, bool) {
+	defaultValue := int64(0)
+	if len(args) > 0 {
+		defaultValue = args[0]
+	}
+
+	str, ok := os.LookupEnv(n)
+	if !ok {
+		return defaultValue, false
+	}
+
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		logrus.WithError(err).Errorf("can not parse env var %q as int, incorrect format", str)
+		return defaultValue, false
+	}
+
+	return num, true
+}
+
 func Int(n string, args ...int) (int, bool) {
 	defaultValue := 0
 	if len(args) > 0 {
