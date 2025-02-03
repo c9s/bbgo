70e290e71ef90f1e77cf98a739d22421be48a6ba
c9s <yoanlin93@gmail.com>
2025-02-03 13:12:00 +0800

    strint: rename StrInt64 to Int64

--- pkg/types/strint.go
+++ pkg/types/strint.go
@@ -2,4 +2,4 @@ package types
 
 import "github.com/c9s/bbgo/pkg/types/strint"
 
-type StrInt64 = strint.StrInt64
+type StrInt64 = strint.Int64
--- pkg/types/strint/int64.go
+++ pkg/types/strint/int64.go
@@ -6,22 +6,22 @@ import (
 	"strconv"
 )
 
-// StrInt64 is a string type for int64
-type StrInt64 int64
+// Int64 is a string type for int64
+type Int64 int64
 
-func NewStrInt64FromString(ss string) (StrInt64, error) {
+func NewStrInt64FromString(ss string) (Int64, error) {
 	i, err := strconv.ParseInt(ss, 10, 64)
 	if err != nil {
 		return 0, err
 	}
 
-	return StrInt64(i), nil
+	return Int64(i), nil
 }
 
-func (s *StrInt64) UnmarshalYAML(unmarshal func(a interface{}) error) (err error) {
+func (s *Int64) UnmarshalYAML(unmarshal func(a interface{}) error) (err error) {
 	var a int64
 	if err = unmarshal(&a); err == nil {
-		*s = StrInt64(a)
+		*s = Int64(a)
 		return
 	}
 
@@ -36,15 +36,15 @@ func (s *StrInt64) UnmarshalYAML(unmarshal func(a interface{}) error) (err error
 		return
 	}
 
-	return fmt.Errorf("StrInt64.UnmarshalYAML error: unsupported value type, not int64 or string: %w", err)
+	return fmt.Errorf("Int64.UnmarshalYAML error: unsupported value type, not int64 or string: %w", err)
 }
 
-func (s *StrInt64) MarshalJSON() ([]byte, error) {
+func (s *Int64) MarshalJSON() ([]byte, error) {
 	ss := strconv.FormatInt(int64(*s), 10)
 	return json.Marshal(ss)
 }
 
-func (s *StrInt64) UnmarshalJSON(body []byte) error {
+func (s *Int64) UnmarshalJSON(body []byte) error {
 	var arg interface{}
 	if err := json.Unmarshal(body, &arg); err != nil {
 		return err
@@ -57,29 +57,29 @@ func (s *StrInt64) UnmarshalJSON(body []byte) error {
 			return err
 		}
 
-		*s = StrInt64(s2)
+		*s = Int64(s2)
 
 	case int64:
-		*s = StrInt64(ta)
+		*s = Int64(ta)
 	case int32:
-		*s = StrInt64(ta)
+		*s = Int64(ta)
 	case int:
-		*s = StrInt64(ta)
+		*s = Int64(ta)
 
 	case float32:
-		*s = StrInt64(ta)
+		*s = Int64(ta)
 
 	case float64:
-		*s = StrInt64(ta)
+		*s = Int64(ta)
 
 	default:
-		return fmt.Errorf("StrInt64 error: unsupported value type %T", ta)
+		return fmt.Errorf("Int64 error: unsupported value type %T", ta)
 	}
 
 	return nil
 }
 
-func (s *StrInt64) String() string {
+func (s *Int64) String() string {
 	if s == nil {
 		return ""
 	}
