e7ba7369d088628a828870963f5c3e50f5a4178a
dboy <dboy@maicoin.com>
2025-02-14 18:32:46 +0800

    🔧 fix(coinbase): resolve review comments, fixed-point value type for numeric fields for precision

--- pkg/exchange/coinbase/api/v1/create_order_request.go
+++ pkg/exchange/coinbase/api/v1/create_order_request.go
@@ -1,38 +1,41 @@
 package coinbase
 
 import (
-	"time"
-
+	"github.com/c9s/bbgo/pkg/fixedpoint"
 	"github.com/c9s/bbgo/pkg/types"
 	"github.com/c9s/requestgen"
 )
 
 type CreateOrderResponse struct {
-	ID             string    `json:"id"`
-	Price          string    `json:"price"`
-	Size           string    `json:"size"`
-	ProductID      string    `json:"product_id"`
-	ProfileID      string    `json:"profile_id"`
-	Side           string    `json:"side"`
-	Funds          string    `json:"funds"`
-	SpecifiedFunds string    `json:"specified_funds"`
-	Type           string    `json:"type"`
-	TimeInForce    string    `json:"time_in_force"`
-	ExpireTime     time.Time `json:"expire_time"`
-	PostOnly       bool      `json:"post_only"`
-	CreatedAt      time.Time `json:"created_at"`
-	DoneAt         time.Time `json:"done_at"`
-	DoneReason     string    `json:"done_reason"`
-	RejectReason   string    `json:"reject_reason"`
-	FillFees       string    `json:"fill_fees"`
-	FilledSize     string    `json:"filled_size"`
-	ExecutedValue  string    `json:"executed_value"`
-	Status         string    `json:"status"`
-	Settled        bool      `json:"settled"`
-	Stop           string    `json:"stop"`
-	StopPrice      string    `json:"stop_price"`
-	FundingAmount  string    `json:"funding_amount"`
-	ClientOrderID  string    `json:"client_oid"`
+	ID               string           `json:"id"`
+	Price            fixedpoint.Value `json:"price,omitempty"`
+	Size             fixedpoint.Value `json:"size,omitempty"`
+	ProductID        string           `json:"product_id"`
+	ProfileID        string           `json:"profile_id,omitempty"`
+	Side             SideType         `json:"side"`
+	Funds            fixedpoint.Value `json:"funds,omitempty"`
+	SpecifiedFunds   fixedpoint.Value `json:"specified_funds,omitempty"`
+	Type             OrderType        `json:"type"`
+	TimeInForce      TimeInForceType  `json:"time_in_force,omitempty"`
+	ExpireTime       string           `json:"expire_time,omitempty"`
+	PostOnly         bool             `json:"post_only"`
+	CreatedAt        string           `json:"created_at"`
+	DoneAt           string           `json:"done_at,omitempty"`
+	DoneReason       string           `json:"done_reason,omitempty"`
+	RejectReason     string           `json:"reject_reason,omitempty"`
+	FillFees         fixedpoint.Value `json:"fill_fees"`
+	FilledSize       fixedpoint.Value `json:"filled_size"`
+	ExecutedValue    fixedpoint.Value `json:"executed_value,omitempty"`
+	Status           OrderStatus      `json:"status"`
+	Settled          bool             `json:"settled"`
+	Stop             OrderStopType    `json:"stop,omitempty"`
+	StopPrice        fixedpoint.Value `json:"stop_price,omitempty"`
+	FundingAmount    string           `json:"funding_amount,omitempty"`
+	ClientOrderID    string           `json:"client_oid,omitempty"`
+	MarketType       string           `json:"market_type,omitempty"`
+	MaxFloor         string           `json:"max_floor,omitempty"`
+	SecondaryOrderID string           `json:"secondary_order_id,omitempty"`
+	StopLimitPrice   fixedpoint.Value `json:"stop_limit_price,omitempty"`
 }
 
 //go:generate requestgen -method POST -url "/orders" -type CreateOrderRequest -responseType .CreateOrderResponse
