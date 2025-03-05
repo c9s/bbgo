beb74a6b90c21bc671b0a10d9af115e4a4fcea9f
dboyliao <qmalliao@gmail.com>
2025-03-05 21:27:53 +0800

    🔧 feat(coinbase): implement stream message handlers for various events and enhance connection logic

---
+++ pkg/exchange/coinbase/stream_handlers.go
@@ -0,0 +1,298 @@
+package coinbase
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+type channelType struct {
+	Name       string   `json:"name"`
+	ProductIDs []string `json:"product_ids,omitempty"`
+}
+
+type authMsg struct {
+	Signature  string `json:"signature,omitempty"`
+	Key        string `json:"key,omitempty"`
+	Passphrase string `json:"passphrase,omitempty"`
+	Timestamp  string `json:"timestamp,omitempty"`
+}
+
+type subscribeMsgType1 struct {
+	Type     string        `json:"type"`
+	Channels []channelType `json:"channels"`
+
+	authMsg
+}
+
+type subscribeMsgType2 struct {
+	Type       string   `json:"type"`
+	Channels   []string `json:"channels"`
+	ProductIDs []string `json:"product_ids,omitempty"`
+	AccountIDs []string `json:"account_ids,omitempty"` // for balance channel
+
+	authMsg
+}
+
+func (s *Stream) handleConnect() {
+	// subscribe to channels
+	if len(s.Subscriptions) == 0 {
+		return
+	}
+
+	subProductsMap := make(map[string][]string)
+	for _, sub := range s.Subscriptions {
+		strChannel := string(sub.Channel)
+		// rfqMatchChannel allow empty symbol
+		if sub.Channel != rfqMatchChannel && len(sub.Symbol) == 0 {
+			continue
+		}
+		subProductsMap[strChannel] = append(subProductsMap[strChannel], sub.Symbol)
+	}
+	var subCmds []any
+	signature, ts := s.generateSignature()
+	for channel, productIDs := range subProductsMap {
+		var subType string
+		switch channel {
+		case "rfq_matches":
+			subType = "subscriptions"
+		default:
+			subType = "subscribe"
+		}
+		var subCmd any
+		switch channel {
+		case "ticker", "ticker_batch", "level2", "level2_batch":
+			subCmd = subscribeMsgType2{
+				Type:       subType,
+				Channels:   []string{channel},
+				ProductIDs: productIDs,
+				authMsg: authMsg{
+					Signature:  signature,
+					Key:        s.apiKey,
+					Passphrase: s.passphrase,
+					Timestamp:  ts,
+				},
+			}
+		case "balance":
+			subCmd = subscribeMsgType2{
+				Type:       subType,
+				Channels:   []string{channel},
+				AccountIDs: productIDs,
+
+				authMsg: authMsg{
+					Signature:  signature,
+					Key:        s.apiKey,
+					Passphrase: s.passphrase,
+					Timestamp:  ts,
+				},
+			}
+		default:
+			subCmd = subscribeMsgType1{
+				Type: subType,
+				Channels: []channelType{
+					{
+						Name:       channel,
+						ProductIDs: productIDs,
+					},
+				},
+
+				authMsg: authMsg{
+					Signature:  signature,
+					Key:        s.apiKey,
+					Passphrase: s.passphrase,
+					Timestamp:  ts,
+				},
+			}
+		}
+		subCmds = append(subCmds, subCmd)
+	}
+	for _, subCmd := range subCmds {
+		err := s.Conn.WriteJSON(subCmd)
+		if err != nil {
+			log.WithError(err).Errorf("subscription error: %v", subCmd)
+		} else {
+			log.Infof("subscribed to %v", subCmd)
+		}
+	}
+
+	// emit balance snapshot on connection
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		// query account balances
+		balances, err := s.exchange.QueryAccountBalances(ctx)
+		if err != nil {
+			log.WithError(err).Warn("failed to query account balances, the balance snapshot is initialized with empty balances")
+			return
+		}
+		s.EmitBalanceSnapshot(balances)
+	}()
+}
+
+func (s *Stream) handleDisconnect() {
+	// clear sequence numbers
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.lastSequenceMsgMap = make(map[MessageType]SequenceNumberType)
+}
+
+// order book, trade
+func (s *Stream) handleTickerMessage(msg *TickerMessage) {
+	// ignore outdated messages
+	if !s.checkAndUpdateSequenceNumber(msg.Type, msg.Sequence) {
+		return
+	}
+	trade := msg.Trade()
+	s.EmitTradeUpdate(trade)
+}
+
+func (s *Stream) handleMatchMessage(msg *MatchMessage) {
+	// ignore outdated messages
+	if !s.checkAndUpdateSequenceNumber(msg.Type, msg.Sequence) {
+		return
+	}
+	trade := msg.Trade()
+	s.EmitTradeUpdate(trade)
+}
+
+func (s *Stream) handleOrderBookSnapshotMessage(msg *OrderBookSnapshotMessage) {
+	symbol := toGlobalSymbol(msg.ProductID)
+	var bids types.PriceVolumeSlice
+	for _, b := range msg.Bids {
+		if len(b) != 2 {
+			continue
+		}
+		bids = append(bids, types.PriceVolume{
+			Price:  b[0],
+			Volume: b[1],
+		})
+	}
+	var asks types.PriceVolumeSlice
+	for _, a := range msg.Asks {
+		if len(a) != 2 {
+			continue
+		}
+		asks = append(asks, types.PriceVolume{
+			Price:  a[0],
+			Volume: a[1],
+		})
+	}
+	book := types.SliceOrderBook{
+		Symbol: symbol,
+		Bids:   bids,
+		Asks:   asks,
+		Time:   time.Now(),
+	}
+	s.EmitBookSnapshot(book)
+}
+
+func (s *Stream) handleOrderbookUpdateMessage(msg *OrderBookUpdateMessage) {
+	var bids types.PriceVolumeSlice
+	var asks types.PriceVolumeSlice
+	for _, c := range msg.Changes {
+		if len(c) != 3 {
+			continue
+		}
+		side := c[0]
+		price := fixedpoint.MustNewFromString(c[1])
+		volume := fixedpoint.MustNewFromString(c[2])
+		if volume.IsZero() {
+			// 0 volume means the price can be removed.
+			continue
+		}
+		switch side {
+		case "buy":
+			bids = append(bids, types.PriceVolume{
+				Price:  price,
+				Volume: volume,
+			})
+		case "sell":
+			asks = append(asks, types.PriceVolume{
+				Price:  price,
+				Volume: volume,
+			})
+		default:
+			log.Warnf("unknown order book update side: %s", side)
+		}
+	}
+	if len(bids) > 0 || len(asks) > 0 {
+		book := types.SliceOrderBook{
+			Symbol: toGlobalSymbol(msg.ProductID),
+			Bids:   bids,
+			Asks:   asks,
+			Time:   msg.Time,
+		}
+		s.EmitBookUpdate(book)
+	}
+
+}
+
+// order update
+func (s *Stream) handleReceivedMessage(msg *ReceivedMessage) {
+	if !s.checkAndUpdateSequenceNumber(msg.Type, msg.Sequence) {
+		return
+	}
+
+	order := types.Order{
+		SubmitOrder: types.SubmitOrder{
+			Symbol:   toGlobalSymbol(msg.ProductID),
+			Side:     toGlobalSide(msg.Side),
+			Price:    msg.Price,
+			Quantity: msg.Size,
+			Type:     types.OrderType(strings.ToUpper(msg.OrderType)),
+		},
+		Exchange:   types.ExchangeCoinBase,
+		UpdateTime: types.Time(msg.Time),
+		IsWorking:  true,
+	}
+	s.EmitOrderUpdate(order)
+}
+
+func (s *Stream) handleOpenMessage(msg *OpenMessage) {
+	if !s.checkAndUpdateSequenceNumber(msg.Type, msg.Sequence) {
+		return
+	}
+	order := types.Order{
+		SubmitOrder: types.SubmitOrder{
+			Symbol:   toGlobalSymbol(msg.ProductID),
+			Side:     toGlobalSide(msg.Side),
+			Price:    msg.Price,
+			Quantity: msg.RemainingSize,
+		},
+		UUID:       msg.OrderID,
+		Exchange:   types.ExchangeCoinBase,
+		IsWorking:  true,
+		UpdateTime: types.Time(msg.Time),
+	}
+	s.EmitOrderUpdate(order)
+}
+
+// balance update
+func (s *Stream) handleBalanceMessage(msg *BalanceMessage) {
+	balanceUpdte := make(types.BalanceMap)
+	balanceUpdte[msg.Currency] = types.Balance{
+		Currency:  msg.Currency,
+		Available: msg.Available,
+		Locked:    msg.Holds,
+		NetAsset:  msg.Available.Add(msg.Holds),
+	}
+	s.EmitBalanceUpdate(balanceUpdte)
+}
+
+// helpers
+func (s *Stream) checkAndUpdateSequenceNumber(msgType MessageType, currentSeqNum SequenceNumberType) bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	latestSeq := s.lastSequenceMsgMap[msgType]
+	if latestSeq >= currentSeqNum {
+		return false
+	}
+	s.lastSequenceMsgMap[msgType] = currentSeqNum
+	return true
+}
