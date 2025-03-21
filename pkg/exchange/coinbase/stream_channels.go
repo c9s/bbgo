09f38b3ceec97d1c2295fbc45067839301c2c601
dboyliao <6830390+dboyliao@users.noreply.github.com>
2025-03-21 10:03:27 +0800

    🔧 fix(coinbase): add logging for channel subscriptions in stream handler

--- pkg/exchange/coinbase/stream_channels.go
+++ pkg/exchange/coinbase/stream_channels.go
@@ -6,18 +6,22 @@ func (s *Stream) Subscribe(channel types.Channel, symbol string, options types.S
 	localSymbol := toLocalSymbol(symbol)
 	switch channel {
 	case types.BookChannel:
+		logStream.Infof("bridge %s to level2 channel (%s)", channel, symbol)
 		s.StandardStream.Subscribe(types.Channel("level2"), localSymbol, options)
 	case types.BookTickerChannel:
+		logStream.Infof("bridge %s to ticker channel (%s)", channel, symbol)
 		s.StandardStream.Subscribe(types.Channel("ticker"), localSymbol, options)
 	case types.MarketTradeChannel:
 		if s.PublicOnly {
+			logStream.Infof("bridge %s to full channel (%s)", channel, symbol)
 			s.StandardStream.Subscribe(types.Channel("full"), localSymbol, options)
 		} else {
+			logStream.Infof("bridge %s to user channel (%s)", channel, symbol)
 			s.StandardStream.Subscribe(types.Channel("user"), localSymbol, options)
 		}
 	case types.KLineChannel, types.AggTradeChannel, types.ForceOrderChannel, types.MarkPriceChannel, types.LiquidationOrderChannel, types.ContractInfoChannel:
 		logStream.Warnf("coinbase stream does not support subscription to %s", channel)
 	default:
-		s.StandardStream.Subscribe(channel, localSymbol, options)
+		logStream.Warnf("do not support subscription to non-standard channel %s (use `.StandardStream.Subscribe` to bypass it)", channel)
 	}
 }
