dd5db7faf64f16a2172af43a343e8f348bea86e9
kbearXD <s199180626@gmail.com>
2025-04-14 17:46:14 +0800

    FEATURE: [dca2] add order into order store when syncing active orders and update the metrics

--- pkg/strategy/dca2/state.go
+++ pkg/strategy/dca2/state.go
@@ -47,14 +47,15 @@ func (s *Strategy) updateState(state State) {
 	s.state = state
 
 	s.logger.Infof("[state] update state to %d", state)
-	metricsState.With(baseLabels).Set(float64(s.state))
+
+	updateNumOfActiveOrdersMetrics(state, int64(len(s.OrderExecutor.ActiveMakerOrders().Orders())))
 }
 
 func (s *Strategy) emitNextState(nextState State) {
 	select {
 	case s.nextStateC <- nextState:
 	default:
-		s.logger.Info("[DCA] nextStateC is full or not initialized")
+		s.logger.Warn("[DCA] nextStateC is full or not initialized")
 	}
 }
 
