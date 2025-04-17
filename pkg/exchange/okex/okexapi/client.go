cec52b774a681e032826a1fab8adbeef1e1f06ae
dboyliao <6830390+dboyliao@users.noreply.github.com>
2025-04-17 13:40:10 +0800

    🔧 Fix RestBaseURL to use the correct endpoint for OKX API

--- pkg/exchange/okex/okexapi/client.go
+++ pkg/exchange/okex/okexapi/client.go
@@ -21,7 +21,7 @@ import (
 )
 
 const defaultHTTPTimeout = time.Second * 15
-const RestBaseURL = "https://aws.okx.com/"
+const RestBaseURL = "https://www.okx.com/"
 const PublicWebSocketURL = "wss://wsaws.okx.com:8443/ws/v5/public"
 const PrivateWebSocketURL = "wss://wsaws.okx.com:8443/ws/v5/private"
 const PublicBusinessWebSocketURL = "wss://wsaws.okx.com:8443/ws/v5/business"
