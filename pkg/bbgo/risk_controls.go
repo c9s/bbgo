8a20bfa1ae7754afb6fdd2d40e058cafd3eb2f77
c9s <yoanlin93@gmail.com>
2025-04-22 17:43:35 +0800

    drop SymbolBasedRiskController

--- pkg/bbgo/risk_controls.go
+++ pkg/bbgo/risk_controls.go
@@ -4,10 +4,6 @@ import (
 	"github.com/c9s/bbgo/pkg/types"
 )
 
-type SymbolBasedRiskController struct {
-	BasicRiskController *BasicRiskController `json:"basic,omitempty" yaml:"basic,omitempty"`
-}
-
 func groupSubmitOrdersBySymbol(orders []types.SubmitOrder) map[string][]types.SubmitOrder {
 	var symbolOrders = make(map[string][]types.SubmitOrder, len(orders))
 	for _, order := range orders {
