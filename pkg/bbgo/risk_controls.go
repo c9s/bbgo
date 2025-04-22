6bca6613b99ae644c84c9edb5722f48d36a64aa1
c9s <yoanlin93@gmail.com>
2025-04-22 17:43:25 +0800

    drop RiskControlOrderExecutor

--- pkg/bbgo/risk_controls.go
+++ pkg/bbgo/risk_controls.go
@@ -1,53 +1,11 @@
 package bbgo
 
 import (
-	"context"
-
-	"github.com/sirupsen/logrus"
-
 	"github.com/c9s/bbgo/pkg/types"
 )
 
 type SymbolBasedRiskController struct {
 	BasicRiskController *BasicRiskController `json:"basic,omitempty" yaml:"basic,omitempty"`
-}
-
-type RiskControlOrderExecutor struct {
-	*ExchangeOrderExecutor
-
-	// Symbol => Executor config
-	BySymbol map[string]*SymbolBasedRiskController `json:"bySymbol,omitempty" yaml:"bySymbol,omitempty"`
-}
-
-func (e *RiskControlOrderExecutor) SubmitOrders(
-	ctx context.Context, orders ...types.SubmitOrder,
-) (retOrders types.OrderSlice, err error) {
-	var symbolOrders = groupSubmitOrdersBySymbol(orders)
-	for symbol, orders := range symbolOrders {
-		if controller, ok := e.BySymbol[symbol]; ok && controller != nil {
-			var riskErrs []error
-
-			orders, riskErrs = controller.BasicRiskController.ProcessOrders(e.Session, orders...)
-			for _, riskErr := range riskErrs {
-				// use logger from ExchangeOrderExecutor
-				logrus.Warnf("RISK ERROR: %s", riskErr.Error())
-			}
-		}
-
-		formattedOrders, err := e.Session.FormatOrders(orders)
-		if err != nil {
-			return retOrders, err
-		}
-
-		retOrders2, err := e.ExchangeOrderExecutor.SubmitOrders(ctx, formattedOrders...)
-		if err != nil {
-			return retOrders, err
-		}
-
-		retOrders = append(retOrders, retOrders2...)
-	}
-
-	return
 }
 
 func groupSubmitOrdersBySymbol(orders []types.SubmitOrder) map[string][]types.SubmitOrder {
