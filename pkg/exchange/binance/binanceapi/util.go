37dca478d83c889e777a2fdaccc4e2ff75a8b301
dboyliao <6830390+dboyliao@users.noreply.github.com>
2025-04-29 14:25:46 +0800

    🔧 feat(binance): Add support for Ed25519 private key authentication in RESTful API requests

--- pkg/exchange/binance/binanceapi/util.go
+++ pkg/exchange/binance/binanceapi/util.go
@@ -6,8 +6,8 @@ import (
 )
 
 // GenerateSignatureEd25519 generates a signature for the given string with the provided private key.
-func GenerateSignatureEd25519(paramString string, privateKey ed25519.PrivateKey) string {
-	signatureBytes := ed25519.Sign(privateKey, []byte(paramString))
+func GenerateSignatureEd25519(content string, privateKey ed25519.PrivateKey) string {
+	signatureBytes := ed25519.Sign(privateKey, []byte(content))
 	signature := base64.StdEncoding.EncodeToString(signatureBytes)
 	return signature
 }
