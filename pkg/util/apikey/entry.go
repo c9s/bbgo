d71c2ac010ac5d1753177ab11272c90987318ea8
c9s <yoanlin93@gmail.com>
2025-05-16 19:01:14 +0800

    apikey: add stringer method on entry

--- pkg/util/apikey/entry.go
+++ pkg/util/apikey/entry.go
@@ -1,10 +1,20 @@
 package apikey
 
+import (
+	"fmt"
+
+	"github.com/c9s/bbgo/pkg/util"
+)
+
 type Entry struct {
 	Index  int
 	Key    string
 	Secret string
 	Fields map[string]string
+}
+
+func (e *Entry) String() string {
+	return fmt.Sprintf("%d) %s:%s %+v", e.Index, util.MaskKey(e.Key), util.MaskKey(e.Secret), e.Fields)
 }
 
 func (e *Entry) GetKeySecret() (key, secret string) {
