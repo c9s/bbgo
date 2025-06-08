233e7b852e530f92cabae1ecb7a8e6956872f9b9
dboyliao <6830390+dboyliao@users.noreply.github.com>
2025-06-08 10:26:56 +0800

    🔧 refactor(indicatorv2): simplify liquidity demand API for the indicator set

--- pkg/strategy/correlation_liquidity_demand/strategy.go
+++ pkg/strategy/correlation_liquidity_demand/strategy.go
@@ -9,7 +9,6 @@ import (
 	"sync"
 
 	"github.com/c9s/bbgo/pkg/bbgo"
-	indicatorv2 "github.com/c9s/bbgo/pkg/indicator/v2"
 	"github.com/c9s/bbgo/pkg/strategy/common"
 	"github.com/c9s/bbgo/pkg/types"
 	"github.com/sirupsen/logrus"
@@ -87,15 +86,11 @@ func (s *Strategy) Subscribe(session *bbgo.ExchangeSession) {
 func (s *Strategy) Run(ctx context.Context, _ bbgo.OrderExecutor, session *bbgo.ExchangeSession) error {
 	s.Strategy.Initialize(ctx, s.Environment, session, s.Market, ID, s.InstanceID())
 
-	iw := types.IntervalWindow{
-		Interval: s.Interval,
-		Window:   s.Window,
-	}
-
-	liqDemand := indicatorv2.LiquidityDemand(
-		session.Indicators(s.Symbol).KLines(s.Interval),
-		session.Indicators(s.Symbol).EMA(iw),
-		session.Indicators(s.Symbol).EMA(iw),
+	liqDemand := session.Indicators(s.Symbol).LiquidityDemand(
+		types.IntervalWindow{
+			Interval: s.Interval,
+			Window:   s.Window,
+		},
 	)
 	session.MarketDataStream.OnKLineClosed(types.KLineWith(s.Symbol, s.Interval, func(k types.KLine) {
 		s.kLineCloseBuffer.Push(k.Close.Float64())
@@ -121,35 +116,34 @@ func (s *Strategy) Run(ctx context.Context, _ bbgo.OrderExecutor, session *bbgo.
 	bbgo.OnShutdown(ctx, func(ctx context.Context, wg *sync.WaitGroup) {
 		defer wg.Done()
 		// dump records to a file in JSON format
-		if len(s.records) == 0 {
-			return
-		}
-		priceDiffs := types.NewFloat64Series()
-		ligDemands := types.NewFloat64Series()
-		for _, rec := range s.records {
-			priceDiffs.Push(rec.PriceDiff)
-			ligDemands.Push(rec.LiquidityDemand)
-		}
-		corr := priceDiffs.Correlation(ligDemands, priceDiffs.Length())
-		s.logger.Infof("correlation between price diff and liquidity demand: %f", corr)
-		s.logger.Infof("number of records: %d", len(s.records))
-		fileName := fmt.Sprintf("%s-%s-records.json", s.Symbol, s.Interval)
-		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-		if err != nil {
-			s.logger.WithError(err).Errorf("unable to open file %s for writing", fileName)
-			return
-		}
-		defer file.Close()
+		if len(s.records) > 0 {
+			priceDiffs := types.NewFloat64Series()
+			liqDemands := types.NewFloat64Series()
+			for _, rec := range s.records {
+				priceDiffs.Push(rec.PriceDiff)
+				liqDemands.Push(rec.LiquidityDemand)
+			}
+			corr := priceDiffs.Correlation(liqDemands, priceDiffs.Length())
+			s.logger.Infof("correlation between price diff and liquidity demand: %f", corr)
+			s.logger.Infof("number of records: %d", len(s.records))
+			fileName := fmt.Sprintf("%s-%s-records.json", s.Symbol, s.Interval)
+			file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+			if err != nil {
+				s.logger.WithError(err).Errorf("unable to open file %s for writing", fileName)
+				return
+			}
+			defer file.Close()
 
-		jsonData, err := json.MarshalIndent(s.records, "", "  ")
-		if err != nil {
-			s.logger.WithError(err).Errorf("unable to marshal records to JSON")
-			return
-		}
-		_, err = file.Write(jsonData)
-		if err != nil {
-			s.logger.WithError(err).Errorf("unable to write records to file %s", fileName)
-			return
+			jsonData, err := json.MarshalIndent(s.records, "", "  ")
+			if err != nil {
+				s.logger.WithError(err).Errorf("unable to marshal records to JSON")
+				return
+			}
+			_, err = file.Write(jsonData)
+			if err != nil {
+				s.logger.WithError(err).Errorf("unable to write records to file %s", fileName)
+				return
+			}
 		}
 	})
 	return nil
