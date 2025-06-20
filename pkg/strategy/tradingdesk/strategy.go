93490d56cfd6fc5a82841eae9e32e674d67f6c59
narumi <toucans-cutouts0f@icloud.com>
2025-06-20 18:56:57 +0800

    init commit of tradingdesk strategy

---
+++ pkg/strategy/tradingdesk/strategy.go
@@ -0,0 +1,61 @@
+package tradingdesk
+
+import (
+	"context"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/c9s/bbgo/pkg/bbgo"
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+const ID = "tradingdesk"
+
+var log = logrus.WithField("strategy", ID)
+
+func init() {
+	bbgo.RegisterStrategy(ID, &Strategy{})
+}
+
+type Strategy struct {
+	Session *bbgo.ExchangeSession
+}
+
+func (s *Strategy) ID() string {
+	return ID
+}
+
+func (s *Strategy) InstanceID() string {
+	return ID
+}
+
+func (s *Strategy) Initialize() error {
+	return nil
+}
+
+func (s *Strategy) Validate() error {
+	return nil
+}
+
+func (s *Strategy) Subscribe(session *bbgo.ExchangeSession) {}
+
+func (s *Strategy) Run(ctx context.Context, orderExecutor bbgo.OrderExecutor, session *bbgo.ExchangeSession) error {
+	s.Session = session
+
+	log.Infof("Available markets: %s", strings.Join(s.Symbols(), ", "))
+
+	session.MarketDataStream.OnKLineClosed(func(kline types.KLine) {
+		bbgo.Notify(kline)
+	})
+	return nil
+}
+
+func (s *Strategy) Symbols() []string {
+	markets := s.Session.Markets()
+	var symbols []string
+	for _, market := range markets {
+		symbols = append(symbols, market.Symbol)
+	}
+	return symbols
+}
