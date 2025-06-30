e7524c8701b48ea7e5e795032ccd84ad35911801
c9s <yoanlin93@gmail.com>
2025-06-30 16:40:13 +0800

    reduce logs

--- pkg/strategy/xmaker/positionexposure.go
+++ pkg/strategy/xmaker/positionexposure.go
@@ -69,16 +69,5 @@ func (m *PositionExposure) GetUncovered() fixedpoint.Value {
 	netPosition := m.net.Get()
 	coveredPosition := m.pending.Get()
 	uncoverPosition := netPosition.Sub(coveredPosition)
-
-	if !netPosition.IsZero() || !coveredPosition.IsZero() {
-		log.Infof(
-			"%s netPosition:%v coveredPosition: %v uncoverPosition: %v",
-			m.symbol,
-			netPosition,
-			coveredPosition,
-			uncoverPosition,
-		)
-	}
-
 	return uncoverPosition
 }
