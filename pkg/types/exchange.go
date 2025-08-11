8f66a57e806c12b394aa4d86d58815d749f9386b
c9s <yoanlin93@gmail.com>
2025-08-11 16:44:41 +0800

    bitfinex: implement convert function

--- pkg/types/exchange.go
+++ pkg/types/exchange.go
@@ -24,6 +24,7 @@ const (
 	ExchangeBacktest ExchangeName = "backtest"
 	ExchangeBybit    ExchangeName = "bybit"
 	ExchangeCoinBase ExchangeName = "coinbase"
+	ExchangeBitfinex ExchangeName = "bitfinex"
 )
 
 var SupportedExchanges = map[ExchangeName]struct{}{
