f156b1b8edd467ca343434631f81b0aa4a8c1a05
c9s <yoanlin93@gmail.com>
2025-08-11 16:00:46 +0800

    add funding api and update tests

---
+++ pkg/exchange/bitfinex/bfxapi/cancel_funding_offer_request.go
@@ -0,0 +1,39 @@
+package bfxapi
+
+import (
+	"github.com/c9s/requestgen"
+
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+// CancelFundingOfferRequest represents a request to cancel a funding offer.
+// API: https://docs.bitfinex.com/reference/rest-auth-cancel-funding-offer
+//
+//go:generate requestgen -type CancelFundingOfferRequest -method POST -url "/v2/auth/w/funding/offer/cancel" -responseType .CancelFundingOfferResponse
+type CancelFundingOfferRequest struct {
+	client requestgen.AuthenticatedAPIClient
+
+	id int64 `param:"id,required"` // funding offer ID to cancel
+}
+
+// NewCancelFundingOfferRequest creates a new CancelFundingOfferRequest instance.
+func (c *Client) NewCancelFundingOfferRequest() *CancelFundingOfferRequest {
+	return &CancelFundingOfferRequest{client: c}
+}
+
+// CancelFundingOfferResponse represents the response for canceling a funding offer.
+type CancelFundingOfferResponse struct {
+	Mts          types.MillisecondTimestamp // response timestamp
+	Type         string                     // response type
+	MessageID    *int64                     // message ID
+	_            any                        // placeholder
+	FundingOffer FundingOffer               // funding offer details
+	Code         *int64                     // response code
+	Status       string                     // response status
+	Text         *string                    // response text (nullable)
+}
+
+// UnmarshalJSON parses the Bitfinex array response into CancelFundingOfferResponse.
+func (r *CancelFundingOfferResponse) UnmarshalJSON(data []byte) error {
+	return parseJsonArray(data, r, 0)
+}
