0ec1a845880a0ff7b23657f3175b8b205366c3da
anywhy <anyyang527@outlook.com>
2025-09-01 23:03:16 +0800

    init hyperliquid exchange

---
+++ pkg/exchange/hyperliquid/futures.go
@@ -0,0 +1,13 @@
+package hyperliquid
+
+import (
+	"context"
+
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+func (e *Exchange) queryFuturesMarkets(ctx context.Context) (types.MarketMap, error) {
+	markets := types.MarketMap{}
+
+	return markets, nil
+}
---
+++ pkg/exchange/hyperliquid/hyperapi/futures_get_meta_request.go
@@ -0,0 +1,31 @@
+package hyperapi
+
+import (
+	"github.com/c9s/requestgen"
+)
+
+type FuturesGetMetaResponse struct {
+	Universe []PerpMeta `json:"universe"` // universe
+}
+
+type PerpMeta struct {
+	Name         string `json:"name"`
+	SzDecimals   int    `json:"szDecimals"`
+	MaxLeverage  int    `json:"maxLeverage"`
+	OnlyIsolated bool   `json:"onlyIsolated"`
+	IsDelisted   bool   `json:"isDelisted"`
+}
+
+//go:generate requestgen -method POST -url "/info" -type FuturesGetMetaRequest -responseType FuturesGetMetaResponse
+type FuturesGetMetaRequest struct {
+	client requestgen.APIClient
+
+	metaType InfoReqType `param:"type" default:"meta" validValues:"meta"`
+}
+
+func (c *Client) NewGetFuturesMetaRequest() *FuturesGetMetaRequest {
+	return &FuturesGetMetaRequest{
+		client:   c,
+		metaType: Meta,
+	}
+}
