90cdc1895e70cdd3b68826ac4c526b20df339401
dboyliao <6830390+dboyliao@users.noreply.github.com>
2025-11-26 16:20:01 +0800

    feat(metrics): switch to promauto and add unrealized PnL metrics

--- pkg/types/position_metrics.go
+++ pkg/types/position_metrics.go
@@ -1,29 +1,30 @@
 package types
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
 
-var positionAverageCostMetrics = prometheus.NewGaugeVec(
+var positionAverageCostMetrics = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "bbgo_position_avg_cost",
 		Help: "bbgo position average cost metrics",
 	}, []string{"strategy_id", "strategy_type", "symbol"})
 
-var positionBaseQuantityMetrics = prometheus.NewGaugeVec(
+var positionBaseQuantityMetrics = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "bbgo_position_base_qty",
 		Help: "bbgo position base quantity metrics",
 	}, []string{"strategy_id", "strategy_type", "symbol"})
 
-var positionQuoteQuantityMetrics = prometheus.NewGaugeVec(
+var positionQuoteQuantityMetrics = promauto.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "bbgo_position_quote_qty",
 		Help: "bbgo position quote quantity metrics",
 	}, []string{"strategy_id", "strategy_type", "symbol"})
 
-func init() {
-	prometheus.MustRegister(
-		positionAverageCostMetrics,
-		positionBaseQuantityMetrics,
-		positionQuoteQuantityMetrics,
-	)
-}
+var positionUnrealizedProfitMetrics = promauto.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "bbgo_position_unrealized_profit",
+		Help: "bbgo position unrealized profit metrics",
+	}, []string{"strategy_id", "strategy_type", "symbol"})
