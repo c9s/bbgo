4507d249f7c9bd328520fcb791f13edcc49c3ec1
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-01-02 10:15:35 +0800

    🔄 refactor(interact): default interation dispatcher support

--- pkg/interact/default.go
+++ pkg/interact/default.go
@@ -1,6 +1,9 @@
 package interact
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 var defaultInteraction = New()
 
@@ -22,4 +25,28 @@ func AddCustomInteraction(custom CustomInteraction) {
 
 func Start(ctx context.Context) error {
 	return defaultInteraction.Start(ctx)
+}
+
+// default interactive dispatcher
+var defaultDispatcher *InteractiveMessageDispatcher
+
+// GetDispatcher returns the default interactive message dispatcher
+// NOTE: the default dispatcher is configured only dispatching interation callbacks by the first Slack menssenger.
+// If you have multiple Slack messengers, you need to create your own dispatcher.
+func GetDispatcher() (*InteractiveMessageDispatcher, error) {
+	if defaultDispatcher != nil {
+		return defaultDispatcher, nil
+	}
+	var slackMessenger *Slack
+	for _, messenger := range Default().GetMessengers() {
+		if sm, ok := messenger.(*Slack); ok {
+			slackMessenger = sm
+			break
+		}
+	}
+	if slackMessenger == nil {
+		return nil, fmt.Errorf("slack interaction is not enabled")
+	}
+	defaultDispatcher = NewInteractiveMessageDispatcher(slackMessenger)
+	return defaultDispatcher, nil
 }
