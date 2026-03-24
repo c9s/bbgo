b05a81609d46413bd8a35044617d9a05228d8bc8
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-03-24 14:07:49 +0800

    ✨ feat(orderbook): add StreamBookHealthCheck for monitoring StreamOrderBook health

---
+++ pkg/types/component/orderbook.go
@@ -0,0 +1,38 @@
+package component
+
+import (
+	"context"
+	"time"
+
+	"github.com/c9s/bbgo/pkg/types"
+)
+
+func StreamBookHealthCheck(ctx context.Context, checkDuration, reconnectThreshold time.Duration) func(*types.StreamOrderBook) {
+	return func(book *types.StreamOrderBook) {
+		go func() {
+			ticker := time.NewTicker(checkDuration)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case _, ok := <-ticker.C:
+					if !ok {
+						return
+					}
+					lastUpdate := book.LastUpdateTime()
+					if lastUpdate.IsZero() {
+						continue
+					}
+					if book.Stream == nil {
+						continue
+					}
+					if time.Since(lastUpdate) >= reconnectThreshold {
+						book.Stream.Reconnect()
+					}
+				}
+			}
+		}()
+	}
+}
