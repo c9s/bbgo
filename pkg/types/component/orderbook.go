138a431549b5fcd0287f6c7aea83cc76293413c6
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-03-24 18:26:06 +0800

    ✨ feat(orderbook): add logging for StreamBookHealthCheck

--- pkg/types/component/orderbook.go
+++ pkg/types/component/orderbook.go
@@ -5,10 +5,15 @@ import (
 	"time"
 
 	"github.com/c9s/bbgo/pkg/types"
+	"github.com/sirupsen/logrus"
 )
 
 func StreamBookHealthCheck(ctx context.Context, checkInterval, reconnectThreshold time.Duration) func(*types.StreamOrderBook) {
 	return func(book *types.StreamOrderBook) {
+		logger := logrus.WithFields(logrus.Fields{
+			"component": "healthcheck",
+			"symbol":    book.Symbol,
+		})
 		go func() {
 			ticker := time.NewTicker(checkInterval)
 			defer ticker.Stop()
@@ -16,6 +21,7 @@ func StreamBookHealthCheck(ctx context.Context, checkInterval, reconnectThreshol
 			for {
 				select {
 				case <-ctx.Done():
+					logger.Info("stream book health check context done")
 					return
 				case _, ok := <-ticker.C:
 					if !ok {
@@ -28,7 +34,13 @@ func StreamBookHealthCheck(ctx context.Context, checkInterval, reconnectThreshol
 					if book.Stream == nil {
 						continue
 					}
-					if time.Since(lastUpdate) >= reconnectThreshold {
+					duration := time.Since(lastUpdate)
+					if duration >= reconnectThreshold {
+						logger.Warnf(
+							"last update time exceeds threshold, reconnecting stream: %s >= %s",
+							duration.String(),
+							reconnectThreshold.String(),
+						)
 						book.Stream.Reconnect()
 					}
 				}
