8034f466890f4935b7e11ed4067f4bef8274aa22
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-04-05 20:03:37 +0800

    🔧 fix(csvsource): implement CSV downloading and parsing for multiple exchanges with tests

--- pkg/datasource/csvsource/read_klines.go
+++ pkg/datasource/csvsource/read_klines.go
@@ -16,18 +16,13 @@ type KLineReader interface {
 	ReadAll(interval time.Duration) ([]types.KLine, error)
 }
 
-// ReadKLinesFromCSV reads all the .csv files in a given directory or a single file into a slice of KLines.
+// ReadAllCsv reads all the .csv files in a given directory or a single file into a slice of KLines.
 // Wraps a default CSVKLineReader with Binance decoder for convenience.
 // For finer grained memory management use the base kline reader.
-func ReadKLinesFromCSV(path string, interval time.Duration) ([]types.KLine, error) {
-	return ReadKLinesFromCSVWithDecoder(path, interval, MakeCSVKLineReader(NewBinanceCSVKLineReader))
-}
-
-// ReadKLinesFromCSVWithDecoder permits using a custom CSVKLineReader.
-func ReadKLinesFromCSVWithDecoder(path string, interval time.Duration, maker MakeCSVKLineReader) ([]types.KLine, error) {
+func ReadAllCsv(dir string, interval time.Duration) ([]types.KLine, error) {
 	var klines []types.KLine
 
-	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -43,7 +38,7 @@ func ReadKLinesFromCSVWithDecoder(path string, interval time.Duration, maker Mak
 		}
 		//nolint:errcheck // Read ops only so safe to ignore err return
 		defer file.Close()
-		reader := maker(csv.NewReader(file))
+		reader := NewCSVKLineReader(csv.NewReader(file))
 		newKlines, err := reader.ReadAll(interval)
 		if err != nil {
 			return err
