d03bffa2b9a57bc7f7b1123e0cc50dea4a997468
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-04-05 20:04:07 +0800

    🔄 refactor(csvsource): simplify CSVTickDecoder and CSVTickReader for improved readability and maintainability

--- pkg/datasource/csvsource/csv_tick_reader.go
+++ pkg/datasource/csvsource/csv_tick_reader.go
@@ -7,44 +7,28 @@ import (
 
 var _ TickReader = (*CSVTickReader)(nil)
 
-// CSVTickReader is a CSVTickReader that reads from a CSV file.
+// CSVTickReader reads CsvTick records from a CSV file.
 type CSVTickReader struct {
-	csv     *csv.Reader
-	decoder CSVTickDecoder
-	ticks   []*CsvTick
+	csv *csv.Reader
 }
 
-// MakeCSVTickReader is a factory method type that creates a new CSVTickReader.
-type MakeCSVTickReader func(csv *csv.Reader) *CSVTickReader
-
-// NewCSVKLineReader creates a new CSVKLineReader with the default Binance decoder.
+// NewCSVTickReader creates a new CSVTickReader.
 func NewCSVTickReader(csv *csv.Reader) *CSVTickReader {
 	return &CSVTickReader{
-		csv:     csv,
-		decoder: BinanceCSVTickDecoder,
+		csv: csv,
 	}
 }
 
-// NewCSVTickReaderWithDecoder creates a new CSVKLineReader with the given decoder.
-func NewCSVTickReaderWithDecoder(csv *csv.Reader, decoder CSVTickDecoder) *CSVTickReader {
-	return &CSVTickReader{
-		csv:     csv,
-		decoder: decoder,
-	}
-}
-
-// ReadAll reads all the KLines from the underlying CSV data.
+// ReadAll reads all CsvTicks from the underlying CSV data.
 func (r *CSVTickReader) ReadAll() (ticks []*CsvTick, err error) {
-	var i int
 	for {
-		tick, err := r.Read(i)
+		tick, err := r.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			return nil, err
 		}
-		i++ // used as jump logic inside decoder to skip csv headers in case
 		if tick == nil {
 			continue
 		}
@@ -55,12 +39,12 @@ func (r *CSVTickReader) ReadAll() (ticks []*CsvTick, err error) {
 	return ticks, nil
 }
 
-// Read reads the next KLine from the underlying CSV data.
-func (r *CSVTickReader) Read(i int) (*CsvTick, error) {
+// Read reads the next CsvTick from the underlying CSV data.
+func (r *CSVTickReader) Read() (*CsvTick, error) {
 	rec, err := r.csv.Read()
 	if err != nil {
 		return nil, err
 	}
 
-	return r.decoder(rec, i)
+	return CSVTickDecoder(rec)
 }
