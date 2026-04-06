a8aef0b0e3c6648edf10628a07d5888fa37b86d4
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-04-06 23:50:57 +0800

    🔧 fix(tests): resolve path issues in Test_ReadTicksFromCSV

--- pkg/datasource/csvsource/read_ticks_test.go
+++ pkg/datasource/csvsource/read_ticks_test.go
@@ -8,8 +8,9 @@ import (
 	"github.com/c9s/bbgo/pkg/types"
 )
 
-func TestReadTicksFromBinanceCSV(t *testing.T) {
-	path := "./testdata/binance/FXSUSDT-ticks-2023-10-29.csv"
+func Test_ReadTicksFromCSV(t *testing.T) {
+	// test on binance data is enough since the CSV format is normalized by the downloader
+	path := "./testdata/binance/FXSUSDT/aggTrades"
 	symbol := "FXSUSDT"
 	intervals := []types.Interval{types.Interval1h}
 	klineMap, err := ReadTicksFromCSV(
@@ -17,12 +18,12 @@ func TestReadTicksFromBinanceCSV(t *testing.T) {
 	)
 	klines := klineMap[types.Interval1h]
 	assert.NoError(t, err)
-	assert.Len(t, klines, 1)
-	assert.Equal(t, int64(1698620400), klines[0].StartTime.Unix(), "StartTime")
-	assert.Equal(t, int64(1698624000), klines[0].EndTime.Unix(), "EndTime")
-	assert.Equal(t, 6.0, klines[0].Open.Float64(), "Open")
-	assert.Equal(t, 6.0, klines[0].High.Float64(), "High")
-	assert.Equal(t, 6.0, klines[0].Low.Float64(), "Low")
-	assert.Equal(t, 6.0, klines[0].Close.Float64(), "Close")
-	assert.Equal(t, 111.0, klines[0].Volume.Float64(), "Volume")
+	assert.Len(t, klines, 4)
+	assert.Equal(t, int64(1700179200), klines[0].StartTime.Unix(), "StartTime")
+	assert.Equal(t, int64(1700182800), klines[0].EndTime.Unix(), "EndTime")
+	assert.Equal(t, 7.503, klines[0].Open.Float64(), "Open")
+	assert.Equal(t, 7.601, klines[0].High.Float64(), "High")
+	assert.Equal(t, 7.495, klines[0].Low.Float64(), "Low")
+	assert.Equal(t, 7.598, klines[0].Close.Float64(), "Close")
+	assert.Equal(t, 26786.3, klines[0].Volume.Float64(), "Volume")
 }
