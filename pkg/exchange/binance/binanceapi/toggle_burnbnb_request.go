0729f25835c3b266d07cf560ac995e05433862f1
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-07-03 09:18:47 +0800

    ✨ feat(binance): BNB burn status query method

--- pkg/exchange/binance/binanceapi/toggle_burnbnb_request.go
+++ pkg/exchange/binance/binanceapi/toggle_burnbnb_request.go
@@ -15,6 +15,22 @@ type ToggleBurnBnbRequest struct {
 	interestBnbBurn *bool `param:"interestBNBBurn"`
 }
 
+// NOTE: this api is not officially documented
+//
+//go:generate requestgen -method GET -url "/sapi/v1/bnbBurn" -type BnbBurnStatusRequest -responseType BnbBurnStatusResponse
+type BnbBurnStatusRequest struct {
+	client requestgen.AuthenticatedAPIClient
+}
+
+type BnbBurnStatusResponse struct {
+	SpotBNBBurn     bool `json:"spotBNBBurn"`
+	InterestBNBBurn bool `json:"interestBNBBurn"`
+}
+
 func (c *RestClient) NewToggleBurnBnbRequest() *ToggleBurnBnbRequest {
 	return &ToggleBurnBnbRequest{client: c}
+}
+
+func (c *RestClient) NewBnbBurnStatusRequest() *BnbBurnStatusRequest {
+	return &BnbBurnStatusRequest{client: c}
 }
