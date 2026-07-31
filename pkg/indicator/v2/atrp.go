119d20c35d2bb831dd8672727df9240b253879a9
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-07-31 16:50:51 +0800

    🔧 fix(indicatorv2): use AddSubscriber for backfill support for atrp stream

--- pkg/indicator/v2/atrp.go
+++ pkg/indicator/v2/atrp.go
@@ -12,7 +12,7 @@ func ATRP2(source KLineSubscription, window int) *ATRPStream {
 	}
 	tr := TR2(source)
 	atr := RMA2(tr, window, true)
-	atr.OnUpdate(func(x float64) {
+	atr.AddSubscriber(func(x float64) {
 		// x is the last rma
 		k := source.Last(0)
 		cloze := k.Close.Float64()
