67bd5187145a417614a0d4b6b51d6ecef59cd152
dboyliao <6830390+dboyliao@users.noreply.github.com>
2026-08-28 15:46:32 +0800

    ✨ update(binance): update transfer API

--- pkg/exchange/binance/binanceapi/futures_transfer_request.go
+++ pkg/exchange/binance/binanceapi/futures_transfer_request.go
@@ -2,28 +2,30 @@ package binanceapi
 
 import "github.com/c9s/requestgen"
 
-type FuturesTransferType int
+type FuturesTransferType string
 
 const (
-	FuturesTransferSpotToUsdtFutures FuturesTransferType = 1
-	FuturesTransferUsdtFuturesToSpot FuturesTransferType = 2
+	FuturesTransferSpotToUsdtFutures FuturesTransferType = "MAIN_UMFUTURE"
+	FuturesTransferUsdtFuturesToSpot FuturesTransferType = "UMFUTURE_MAIN"
 
-	FuturesTransferSpotToCoinFutures FuturesTransferType = 3
-	FuturesTransferCoinFuturesToSpot FuturesTransferType = 4
+	FuturesTransferSpotToCoinFutures FuturesTransferType = "MAIN_CMFUTURE"
+	FuturesTransferCoinFuturesToSpot FuturesTransferType = "CMFUTURE_MAIN"
 )
 
 type FuturesTransferResponse struct {
 	TranId int64 `json:"tranId"`
 }
 
-//go:generate requestgen -method POST -url "/sapi/v1/futures/transfer" -type FuturesTransferRequest -responseType .FuturesTransferResponse
+// doc: https://developers.binance.com/en/docs/catalog/core-trading-wallet/api/rest-api/asset#user-universal-transfer
+//
+//go:generate requestgen -method POST -url "/sapi/v1/asset/transfer" -type FuturesTransferRequest -responseType .FuturesTransferResponse
 type FuturesTransferRequest struct {
 	client requestgen.AuthenticatedAPIClient
 
-	asset string `param:"asset"`
+	asset string `param:"asset,required"`
 
 	// amount is a decimal in string format
-	amount string `param:"amount"`
+	amount string `param:"amount,required"`
 
 	transferType FuturesTransferType `param:"type"`
 }
